94f21a2d69e42f7e845893899f98a57c7fe158c6
Ben Weier <ben.weier@gmail.com>
2021-03-21 03:20:21 +0000

    twin.d.ts

---
+++ src/twin.d.ts
@@ -0,0 +1,26 @@
+import 'twin.macro'
+import styledImport, { CSSProp, css as cssImport } from 'styled-components'
+
+declare module 'twin.macro' {
+  const styled: typeof styledImport
+  const css: typeof cssImport
+}
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends DOMAttributes<T> {
+    css?: CSSProp
+  }
+
+  interface SVGProps<T> extends SVGProps<SVGSVGElement> {
+    css?: CSSProp
+  }
+}
+
+// The 'as' prop on styled components
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes<T> extends DOMAttributes<T> {
+      as?: string | Element
+    }
+  }
+}
