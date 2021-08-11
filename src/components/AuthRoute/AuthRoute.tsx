37d4c785cf19e3c242a03707b7b98277a876fdaa
Ben Weier <ben.weier@gmail.com>
2021-08-11 12:07:59 +0000

    add auth route component

---
+++ src/components/AuthRoute/AuthRoute.tsx
@@ -0,0 +1,19 @@
+import { useEffect, ReactNode } from 'react'
+import { useNavigate } from 'react-router'
+import { selectIsAuthenticated } from '../../store/auth'
+import { useAppSelector } from '../../store/hooks'
+
+export const AuthRoute = ({ children }: { children: ReactNode }) => {
+  const isAuthenticated = useAppSelector(selectIsAuthenticated)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/auth')
+    }
+  }, [isAuthenticated, navigate])
+
+  if (!isAuthenticated) return null
+
+  return <>{children}</>
+}
