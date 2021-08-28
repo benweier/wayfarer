a06e55abb68031c255e27b9cd1e2e5abea3fbde1
Ben Weier <ben.weier@gmail.com>
2021-08-28 05:38:36 +0000

    add helmet provider, update named imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import { StrictMode } from 'react'
 import { render } from 'react-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import { QueryClientProvider } from 'react-query'
-import App from './components/App'
-import AppStyles from './components/AppStyles'
-import queryClient from './utilities/query-client'
+import { App } from './components/App'
+import { AppStyles } from './components/AppStyles'
+import { queryClient } from './utilities/query-client'
 
 render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AppStyles />
-      <App />
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppStyles />
+        <App />
+      </QueryClientProvider>
+    </HelmetProvider>
   </StrictMode>,
   document.getElementById('root'),
 )
