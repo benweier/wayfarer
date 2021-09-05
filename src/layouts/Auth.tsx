e4f1b52c76b7211bc5746b779aa7fedea6f433bf
Ben Weier <ben.weier@gmail.com>
2021-09-05 13:40:06 +1000

    update auth layout size

--- src/layouts/Auth.tsx
+++ src/layouts/Auth.tsx
@@ -2,5 +2,5 @@ import { ReactNode } from 'react'
 import tw from 'twin.macro'
 
 export const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <div css={tw`h-screen w-full max-w-4xl mx-auto py-16 flex justify-center items-center`}>{children}</div>
+  return <div css={tw`h-screen max-w-lg mx-auto py-16 grid items-center`}>{children}</div>
 }
