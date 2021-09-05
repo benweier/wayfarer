d69a9ac378d903bfd96eb1511b0a29f64f807cf3
Ben Weier <ben.weier@gmail.com>
2021-09-05 13:47:30 +1000

    refactor login and register form fields, submit handling, and layout

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,16 +1,32 @@
-import { useCallback } from 'react'
+import { FocusEvent, useCallback } from 'react'
 import { useForm, SubmitHandler, FormProvider } from 'react-hook-form'
 import { useNavigate } from 'react-router'
 import tw from 'twin.macro'
-import { useClaimUserMutation } from '../../services/spacetraders/core'
-import { Button } from '../Button'
-import { Input } from '../Input'
+import { Button } from 'components/Button'
+import { Input } from 'components/Input'
+import { Link } from 'components/Link'
+import { ROUTES } from 'config/routes'
+import { useLocation } from 'hooks/useLocation'
+import { useClaimUserMutation } from 'services/spacetraders/core'
+
+interface LoginFormState {
+  user: string
+  token: string
+}
+
+const handleFocus = (node: FocusEvent<HTMLInputElement>) => node.target.select()
 
 export const Login = () => {
   const navigate = useNavigate()
-  const methods = useForm<{ user: string; token: string }>()
+  const location = useLocation<Partial<LoginFormState>>()
+  const methods = useForm<LoginFormState>({
+    defaultValues: {
+      user: location.state?.user ?? '',
+      token: location.state?.token ?? '',
+    },
+  })
   const [claimUserMutation] = useClaimUserMutation()
-  const onSubmit = useCallback<SubmitHandler<{ user: string; token: string }>>(
+  const onSubmit = useCallback<SubmitHandler<LoginFormState>>(
     (values) => {
       return claimUserMutation(values.user).then(() => {
         navigate('/')
@@ -21,12 +37,14 @@ export const Login = () => {
 
   return (
     <FormProvider {...methods}>
-      <form css={tw`contents`} onSubmit={methods.handleSubmit(onSubmit)}>
-        <div css={tw`grid grid-cols-1 gap-6`}>
-          <Input label="Access Token" type="password" name="token" onFocus={(node) => node.target.select()} />
-        </div>
-        <div css={tw`grid grid-flow-col gap-6`}>
+      <form css={tw`grid grid-cols-1 gap-6`} onSubmit={methods.handleSubmit(onSubmit)}>
+        <Input label="Username" type="text" name="user" />
+        <Input label="Access Token" type="password" name="token" onFocus={handleFocus} />
+        <div css={tw`grid grid-flow-row gap-4`}>
           <Button type="submit">Login</Button>
+          <div css={tw`text-sm text-center`}>
+            Don&apos;t have an access token? <Link to={ROUTES.REGISTER}>Register</Link>
+          </div>
         </div>
       </form>
     </FormProvider>
--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,33 +1,90 @@
-import { useCallback } from 'react'
+import { FocusEvent, useCallback } from 'react'
 import { useForm, SubmitHandler, FormProvider } from 'react-hook-form'
-import { FiHelpCircle } from 'react-icons/fi'
 import { useNavigate } from 'react-router'
 import tw from 'twin.macro'
-import { useClaimUserMutation } from '../../services/spacetraders/core'
-import { Button } from '../Button'
-import { Input } from '../Input'
+import { Button } from 'components/Button'
+import { Input } from 'components/Input'
+import { Link } from 'components/Link'
+import { ROUTES } from 'config/routes'
+import { useClaimUserMutation } from 'services/spacetraders/core'
+import { Copy } from './Copy'
+import { useCopy } from './useCopy'
+
+interface RegisterFormState {
+  user: string
+  token: string
+}
+
+const handleFocus = (node: FocusEvent<HTMLInputElement>) => node.target.select()
 
 export const Register = () => {
+  const { ref, isCopied, onCopy, reset } = useCopy()
   const navigate = useNavigate()
-  const methods = useForm<{ user: string; token: string }>()
-  const [claimUserMutation] = useClaimUserMutation()
-  const onSubmit = useCallback<SubmitHandler<{ user: string; token: string }>>(
-    (values) => {
-      return claimUserMutation(values.user).then(() => {
-        navigate('/')
-      })
+  const methods = useForm<RegisterFormState>({
+    defaultValues: {
+      token: '',
     },
-    [claimUserMutation, navigate],
+  })
+  const [claimUserMutation] = useClaimUserMutation()
+  const onSubmit = useCallback<SubmitHandler<RegisterFormState>>(
+    (values) => {
+      return claimUserMutation(values.user)
+        .unwrap()
+        .then((response) => {
+          reset()
+          methods.setValue('token', response.token)
+          ref.current?.focus()
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    },
+    [claimUserMutation, methods, reset, ref],
   )
+
+  const token = methods.watch('token')
 
   return (
     <FormProvider {...methods}>
-      <form css={tw`contents`} onSubmit={methods.handleSubmit(onSubmit)}>
-        <div css={tw`grid grid-cols-1 gap-6`}>
-          <Input type="text" name="user" label="Username" icon={<FiHelpCircle size={20} />} />
+      <form css={tw`grid grid-cols-1 gap-6`} onSubmit={methods.handleSubmit(onSubmit)}>
+        <Input type="text" name="user" label="Username" />
+        <div ref={ref}>
+          <Input
+            type="text"
+            name="token"
+            label="Access Token"
+            onFocus={handleFocus}
+            readOnly
+            icon={
+              <Copy
+                disabled={!token}
+                onClick={async () => {
+                  await onCopy(token)
+                }}
+              />
+            }
+          />
         </div>
-        <div css={tw`grid grid-flow-col gap-6`}>
-          <Button type="submit">Register</Button>
+
+        <div css={tw`grid grid-flow-row gap-4`}>
+          {!isCopied && !token && <Button type="submit">Register</Button>}
+          {!!token && (
+            <Button
+              disabled={!isCopied}
+              type="button"
+              onClick={() => {
+                navigate('/auth/login', { state: { token } })
+              }}
+            >
+              Let&apos;s go!
+            </Button>
+          )}
+          <div css={tw`text-sm text-center`}>
+            Already have an access token?{' '}
+            <Link to={ROUTES.LOGIN} state={{ token }}>
+              Login
+            </Link>
+          </div>
         </div>
       </form>
     </FormProvider>
