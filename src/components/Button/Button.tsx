3a8699c7d502929b2ae88d31680a33814cbc0e87
Ben Weier <ben.weier@gmail.com>
2021-09-05 13:39:36 +1000

    update button color

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import tw, { styled } from 'twin.macro'
 
 export const Button = styled.button(
-  tw`text-sm font-bold bg-blue-600 leading-none p-3 rounded-md outline-none focus:(outline-none ring ring-indigo-500)`,
+  tw`w-full text-sm font-bold bg-blue-500 leading-none p-3 rounded-md outline-none focus:(outline-none ring ring-indigo-500)`,
 )
