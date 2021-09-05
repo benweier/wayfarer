9408e7869ed246981684ad7992ea678fca78e5c7
Ben Weier <ben.weier@gmail.com>
2021-09-05 13:34:33 +1000

    create use copy hook

---
+++ src/components/Auth/useCopy.ts
@@ -0,0 +1,32 @@
+import { useState, useRef, useEffect, useCallback } from 'react'
+
+export const useCopy = () => {
+  const ref = useRef<HTMLDivElement>(null)
+  const [isCopied, setIsCopied] = useState(false)
+  const onCopy = useCallback(
+    async (value) => {
+      return await navigator.clipboard.writeText(value).then(() => {
+        setIsCopied(true)
+      })
+    },
+    [setIsCopied],
+  )
+  const reset = useCallback(() => {
+    setIsCopied(false)
+  }, [setIsCopied])
+
+  useEffect(() => {
+    const node = ref.current
+    const copyHandler = () => {
+      setIsCopied(true)
+    }
+
+    node?.addEventListener('copy', copyHandler)
+
+    return () => {
+      node?.removeEventListener('copy', copyHandler)
+    }
+  }, [])
+
+  return { ref, isCopied, onCopy, reset }
+}
