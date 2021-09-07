fcb6a70dcddd5f3170c6084720ad260847df79fb
Ben Weier <ben.weier@gmail.com>
2021-09-07 19:50:45 +1000

    navigate to root path after login

--- src/layouts/Auth.tsx
+++ src/layouts/Auth.tsx
@@ -1,6 +1,18 @@
-import { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
+import { useNavigate } from 'react-router'
 import tw from 'twin.macro'
+import { selectIsAuthenticated } from 'store/auth'
+import { useAppSelector } from 'store/hooks'
 
 export const AuthLayout = ({ children }: { children: ReactNode }) => {
+  const isAuthenticated = useAppSelector(selectIsAuthenticated)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/', { replace: true })
+    }
+  }, [isAuthenticated, navigate])
+
   return <div css={tw`h-screen max-w-lg mx-auto py-16 grid items-center`}>{children}</div>
 }
