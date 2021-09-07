3cee469de23190e3adb46abaa8875c1313097b32
Ben Weier <ben.weier@gmail.com>
2021-09-07 19:13:41 +1000

    update login to use my account query

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,13 +1,12 @@
 import { FocusEvent, useCallback } from 'react'
 import { useForm, SubmitHandler, FormProvider } from 'react-hook-form'
-import { useNavigate } from 'react-router'
 import tw from 'twin.macro'
 import { Button } from 'components/Button'
 import { Input } from 'components/Input'
 import { Link } from 'components/Link'
 import { ROUTES } from 'config/routes'
 import { useLocation } from 'hooks/useLocation'
-import { useClaimUserMutation } from 'services/spacetraders/core'
+import { useLazyMyAccountQuery } from 'services/spacetraders/core'
 
 interface LoginFormState {
   user: string
@@ -17,22 +16,20 @@ interface LoginFormState {
 const handleFocus = (node: FocusEvent<HTMLInputElement>) => node.target.select()
 
 export const Login = () => {
-  const navigate = useNavigate()
   const location = useLocation<Partial<LoginFormState>>()
+
   const methods = useForm<LoginFormState>({
     defaultValues: {
       user: location.state?.user ?? '',
       token: location.state?.token ?? '',
     },
   })
-  const [claimUserMutation] = useClaimUserMutation()
+  const [myAccountQuery, { isLoading }] = useLazyMyAccountQuery()
   const onSubmit = useCallback<SubmitHandler<LoginFormState>>(
     (values) => {
-      return claimUserMutation(values.user).then(() => {
-        navigate('/')
-      })
+      return myAccountQuery({ token: values.token })
     },
-    [claimUserMutation, navigate],
+    [myAccountQuery],
   )
 
   return (
@@ -41,9 +38,14 @@ export const Login = () => {
         <Input label="Username" type="text" name="user" />
         <Input label="Access Token" type="password" name="token" onFocus={handleFocus} />
         <div css={tw`grid grid-flow-row gap-4`}>
-          <Button type="submit">Login</Button>
+          <Button type="submit" disabled={isLoading}>
+            Login
+          </Button>
           <div css={tw`text-sm text-center`}>
-            Don&apos;t have an access token? <Link to={ROUTES.REGISTER}>Register</Link>
+            Don&apos;t have an access token?{' '}
+            <Link css={tw`font-bold`} to={ROUTES.REGISTER}>
+              Register
+            </Link>
           </div>
         </div>
       </form>
