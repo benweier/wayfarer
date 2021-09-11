67d19ccb76dc25ee95fe56c54ae37397e771f5ad
Ben Weier <ben.weier@gmail.com>
2021-09-11 13:20:20 +1000

    remove label prop, refactor input styles

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,10 +1,13 @@
 import { InputHTMLAttributes, ReactNode } from 'react'
 import { useFormContext } from 'react-hook-form'
 import tw from 'twin.macro'
-import { Label } from 'components/Label'
+
+const styles = {
+  DEFAULT: [tw`py-2 px-4 text-gray-300 bg-gray-600 focus:(ring ring-emerald-400 outline-none) block w-full rounded-md`],
+  disabled: [tw`disabled:(bg-gray-700)`],
+}
 
 export const Input = ({
-  label,
   name,
   icon,
   ...props
@@ -12,19 +15,9 @@ export const Input = ({
   const methods = useFormContext()
 
   return (
-    <div>
-      {label && <Label>{label}</Label>}
-      <div css={tw`relative rounded-md shadow-sm`}>
-        <input
-          {...methods.register(name)}
-          {...props}
-          css={[
-            tw`p-3 text-gray-300 bg-gray-600 focus:(ring ring-emerald-400 outline-none) block w-full sm:text-sm rounded-md`,
-            icon && tw`pr-10`,
-          ]}
-        />
-        {icon && <div css={tw`absolute inset-y-0 right-0 pr-3 flex items-center`}>{icon}</div>}
-      </div>
+    <div css={tw`relative rounded-md shadow-sm`}>
+      <input {...methods.register(name)} {...props} css={[styles.DEFAULT, styles.disabled, icon && tw`pr-10`]} />
+      {icon && <div css={tw`absolute inset-y-0 right-0 pr-2 flex items-center`}>{icon}</div>}
     </div>
   )
 }
