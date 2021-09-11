87be29f5ec027d35a06dbf25356e13763fd721b0
Ben Weier <ben.weier@gmail.com>
2021-09-11 13:32:05 +1000

    update auth component padding

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -5,7 +5,7 @@ import { Register } from './Register'
 
 export const Auth = () => {
   return (
-    <div css={tw`rounded-lg border-2 border-gray-600 p-6`}>
+    <div css={tw`rounded-lg border-2 border-gray-600 px-4 py-6`}>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
