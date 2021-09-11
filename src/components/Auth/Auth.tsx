fc48c30d38db4a287075ee3a67f3320ccaec5639
Ben Weier <ben.weier@gmail.com>
2021-09-11 13:44:40 +1000

    move navigate behaviour from layout to feature component

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,9 +1,21 @@
-import { Routes, Route } from 'react-router'
+import { useEffect } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import tw from 'twin.macro'
+import { selectIsAuthenticated } from 'store/auth'
+import { useAppSelector } from 'store/hooks'
 import { Login } from './Login'
 import { Register } from './Register'
 
 export const Auth = () => {
+  const isAuthenticated = useAppSelector(selectIsAuthenticated)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/', { replace: true })
+    }
+  }, [isAuthenticated, navigate])
+
   return (
     <div css={tw`rounded-lg border-2 border-gray-600 px-4 py-6`}>
       <Routes>
--- src/layouts/Auth.tsx
+++ src/layouts/Auth.tsx
@@ -1,18 +1,6 @@
-import { ReactNode, useEffect } from 'react'
-import { useNavigate } from 'react-router'
+import { ReactNode } from 'react'
 import tw from 'twin.macro'
-import { selectIsAuthenticated } from 'store/auth'
-import { useAppSelector } from 'store/hooks'
 
 export const AuthLayout = ({ children }: { children: ReactNode }) => {
-  const isAuthenticated = useAppSelector(selectIsAuthenticated)
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate('/', { replace: true })
-    }
-  }, [isAuthenticated, navigate])
-
   return <div css={tw`h-screen max-w-lg mx-auto px-4 py-4 grid items-center`}>{children}</div>
 }
