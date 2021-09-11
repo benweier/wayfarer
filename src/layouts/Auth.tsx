e4b3e92febda0b3145e216186868ed85e78e4d94
Ben Weier <ben.weier@gmail.com>
2021-09-11 17:18:59 +1000

    reduce auth layout container width, update grid layout

--- src/layouts/Auth.tsx
+++ src/layouts/Auth.tsx
@@ -2,5 +2,11 @@ import { ReactNode } from 'react'
 import tw from 'twin.macro'
 
 export const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <div css={tw`h-screen max-w-lg mx-auto px-4 py-4 grid items-center`}>{children}</div>
+  return (
+    <div css={tw`h-screen w-full px-4 py-4 grid items-center`}>
+      <div css={tw`h-8 w-full grid items-center`}></div>
+      <div css={tw`mx-auto max-w-lg w-full px-4 py-4`}>{children}</div>
+      <div css={tw`h-8 w-full grid items-center`}></div>
+    </div>
+  )
 }
