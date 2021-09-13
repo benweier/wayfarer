8a2f49538e5ebfa1e7254bd9908a9fb99a5bd661
Ben Weier <ben.weier@gmail.com>
2021-09-13 22:42:58 +1000

    refactor auth layout for consistent spacing, add title and link icons

--- src/layouts/Auth.tsx
+++ src/layouts/Auth.tsx
@@ -1,12 +1,29 @@
 import { ReactNode } from 'react'
-import tw from 'twin.macro'
+import { IconContext } from 'react-icons'
+import { VscRocket, VscGithub } from 'react-icons/vsc'
+import tw, { theme } from 'twin.macro'
+import { Wayfarer } from 'components/Wayfarer'
 
 export const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div css={tw`h-screen w-full px-4 py-4 grid items-center`}>
-      <div css={tw`h-8 w-full grid items-center`}></div>
-      <div css={tw`mx-auto max-w-lg w-full px-4 py-4`}>{children}</div>
-      <div css={tw`h-8 w-full grid items-center`}></div>
+    <div css={tw`min-h-screen w-full px-4 py-4 grid gap-6 auto-rows-min grid-template-rows[auto 1fr auto]`}>
+      <div css={tw`grid justify-center`}>
+        <Wayfarer css={tw`text-center text-7xl`} />
+        <div css={tw`text-center text-xl font-semibold text-gray-400`}>A SpaceTraders API Interface</div>
+      </div>
+      <div css={tw`mx-auto max-w-lg w-full grid items-center`}>{children}</div>
+      <div css={tw`grid grid-flow-col gap-8 py-8`}>
+        <div css={tw`grid grid-flow-col gap-8 items-center justify-center`}>
+          <IconContext.Provider value={{ size: '32', color: theme`colors.gray.400` }}>
+            <a href="https://spacetraders.io" title="SpaceTraders">
+              <VscRocket />
+            </a>
+            <a href="https://github.com/benweier/wayfarer" title="GitHub">
+              <VscGithub />
+            </a>
+          </IconContext.Provider>
+        </div>
+      </div>
     </div>
   )
 }
