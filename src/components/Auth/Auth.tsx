148bc09046c082e6977c3dd82f914c5741f6d177
Ben Weier <ben.weier@gmail.com>
2021-09-13 22:41:22 +1000

    increase auth wrapper padding

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -17,7 +17,7 @@ export const Auth = () => {
   }, [isAuthenticated, navigate])
 
   return (
-    <div css={tw`rounded-lg border border-gray-700 shadow-xl px-4 py-6`}>
+    <div css={tw`rounded-lg border border-gray-700 shadow-xl px-8 py-8`}>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
