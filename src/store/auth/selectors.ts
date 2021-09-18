2175066a73559fba24064c91e1219448cff85d6e
Ben Weier <ben.weier@gmail.com>
2021-09-18 14:37:14 +1000

    add select status, rename select user

--- src/store/auth/selectors.ts
+++ src/store/auth/selectors.ts
@@ -1,4 +1,5 @@
 import type { RootState } from 'store/types.d'
 
+export const selectStatus = (state: RootState) => state.auth.status
 export const selectIsAuthenticated = (state: RootState) => state.auth.isAuthenticated
-export const selectCurrentUser = (state: RootState) => state.auth.user
+export const selectUser = (state: RootState) => state.auth.user
