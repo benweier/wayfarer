e255cc6ed29e9ddd648ede4bfa5b969f82b88ad3
Ben Weier <ben.weier@gmail.com>
2021-09-18 14:40:55 +1000

    remove navigate effect

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,21 +1,9 @@
-import { useEffect } from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import tw from 'twin.macro'
-import { selectIsAuthenticated } from 'store/auth'
-import { useAppSelector } from 'store/hooks'
 import { Login } from './Login'
 import { Register } from './Register'
 
 export const Auth = () => {
-  const isAuthenticated = useAppSelector(selectIsAuthenticated)
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate('/', { replace: true })
-    }
-  }, [isAuthenticated, navigate])
-
   return (
     <div css={tw`rounded-lg border border-gray-700 shadow-xl px-8 py-8`}>
       <Routes>
