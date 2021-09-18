591396ec8b4e8ea0ccc07b61e8407ab651c4ca6e
Ben Weier <ben.weier@gmail.com>
2021-09-18 14:39:21 +1000

    navigate to dashboard if already authenticated

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Auth } from 'components/Auth'
+import { ROUTES } from 'config/routes'
 import { AuthLayout } from 'layouts/Auth'
+import { selectIsAuthenticated } from 'store/auth'
+import { useAppSelector } from 'store/hooks'
 
 export const AuthPage = () => {
+  const isAuthenticated = useAppSelector(selectIsAuthenticated)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate(ROUTES.DASHBOARD, { replace: true })
+    }
+  }, [isAuthenticated, navigate])
+
   return (
     <AuthLayout>
       <Auth />
