20d133917bb9083557336cc32cb98f9f7d3d9e4a
Ben Weier <ben.weier@gmail.com>
2021-09-18 14:40:02 +1000

    update wayfarer text colour

--- src/components/Wayfarer/Wayfarer.tsx
+++ src/components/Wayfarer/Wayfarer.tsx
@@ -1,5 +1,5 @@
 import tw, { styled } from 'twin.macro'
 
 export const Wayfarer = styled.div.attrs({ children: 'WAYFARER' })(() => [
-  tw`text-3xl leading-none font-black letter-spacing[-0.03em] font-feature-settings["kern"]`,
+  tw`text-3xl text-gray-200 leading-none font-black letter-spacing[-0.03em] font-feature-settings["kern"]`,
 ])
