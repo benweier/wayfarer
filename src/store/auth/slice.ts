fcdf8af83fae26344b333400d2466bf889501cb6
Ben Weier <ben.weier@gmail.com>
2021-09-18 14:37:03 +1000

    add status to auth slice, update user credits when balance changes

--- src/store/auth/slice.ts
+++ src/store/auth/slice.ts
@@ -2,11 +2,20 @@ import { createSlice } from '@reduxjs/toolkit'
 import { spacetradersAPI } from 'services/spacetraders/core'
 import { User } from 'types/spacetraders'
 
+type AuthState =
+  | { user: null; token: null; isAuthenticated: false }
+  | { user: User; token: string; isAuthenticated: true }
+
+type AccountStatus = {
+  status: 'IDLE' | 'PENDING' | 'ERROR'
+}
+
 const initialState = {
   user: null,
   token: null,
   isAuthenticated: false,
-} as { user: null | User; token: null | string; isAuthenticated: boolean }
+  status: 'IDLE',
+} as AuthState & AccountStatus
 
 const slice = createSlice({
   name: 'auth',
@@ -16,16 +25,27 @@ const slice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addMatcher(spacetradersAPI.endpoints.myAccount.matchPending, () => {
-        //
+      .addMatcher(spacetradersAPI.endpoints.myAccount.matchPending, (state) => {
+        state.status = 'PENDING'
       })
       .addMatcher(spacetradersAPI.endpoints.myAccount.matchFulfilled, (state, action) => {
+        state.status = 'IDLE'
         state.token = action.meta.arg.originalArgs.token
         state.user = action.payload.user
         state.isAuthenticated = true
       })
-      .addMatcher(spacetradersAPI.endpoints.myAccount.matchRejected, () => {
-        //
+      .addMatcher(spacetradersAPI.endpoints.myAccount.matchRejected, (state) => {
+        state.status = 'ERROR'
+      })
+      .addMatcher(spacetradersAPI.endpoints.takeOutLoan.matchFulfilled, (state, action) => {
+        if (state.isAuthenticated) {
+          state.user.credits = action.payload.credits
+        }
+      })
+      .addMatcher(spacetradersAPI.endpoints.purchaseShip.matchFulfilled, (state, action) => {
+        if (state.isAuthenticated) {
+          state.user.credits = action.payload.credits
+        }
       })
   },
 })
