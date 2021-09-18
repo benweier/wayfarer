131baa391838c6d209b55a6748ccd6f3c494b781
Ben Weier <ben.weier@gmail.com>
2021-09-18 14:46:28 +1000

    refactor dashboard route handling, add home page and wildcard redirects

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,16 @@
 import { Suspense, lazy } from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { AuthRoute } from 'components/AuthRoute'
+import { ProtectedRoute } from 'components/ProtectedRoute'
+import { Redirect } from 'components/Redirect'
+import { ROUTES } from 'config/routes'
+import { HomePage } from 'routes/Home'
 import { store } from 'store'
 
+const DashboardPage = lazy(() => import('routes/Dashboard').then((mod) => ({ default: mod.DashboardPage })))
 const AuthPage = lazy(() => import('routes/Auth').then((mod) => ({ default: mod.AuthPage })))
+const LoanPage = lazy(() => import('routes/Loans').then((mod) => ({ default: mod.LoanPage })))
+const ShipPage = lazy(() => import('routes/Ships').then((mod) => ({ default: mod.ShipPage })))
 
 export const App = () => {
   return (
@@ -12,15 +18,21 @@ export const App = () => {
       <Provider store={store}>
         <Suspense fallback={<></>}>
           <Routes>
+            <Route path={ROUTES.HOME} element={<HomePage />} />
             <Route
-              path="/"
+              path={ROUTES.DASHBOARD}
               element={
-                <AuthRoute>
-                  <></>
-                </AuthRoute>
+                <ProtectedRoute>
+                  <DashboardPage />
+                </ProtectedRoute>
               }
-            />
+            >
+              <Route path={ROUTES.SHIPS} element={<ShipPage />} />
+              <Route path={ROUTES.LOANS} element={<LoanPage />} />
+              <Route path="*" element={<Redirect to={ROUTES.DASHBOARD} />} />
+            </Route>
             <Route path="/auth/*" element={<AuthPage />} />
+            <Route path="*" element={<Redirect to={ROUTES.HOME} />} />
           </Routes>
         </Suspense>
       </Provider>
