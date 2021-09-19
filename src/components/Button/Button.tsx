7336faa3f85a9c6f6fe96d60b7a53a5132f66777
Ben Weier <ben.weier@gmail.com>
2021-09-19 17:08:03 +1000

    update button padding

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import tw, { styled } from 'twin.macro'
 
 const styles = {
   DEFAULT: [
-    tw`w-full text-sm font-bold bg-blue-500 leading-none p-3 rounded outline-none`,
+    tw`w-full text-sm font-bold bg-blue-500 leading-none px-5 py-3 rounded outline-none`,
     tw`focus:(outline-none ring ring-emerald-400 ring-offset-2 ring-offset-gray-800)`,
   ],
   disabled: tw`disabled:(opacity-50 cursor-default)`,
