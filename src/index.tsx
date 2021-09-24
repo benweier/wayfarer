d9483d6d75826c230e66090673d9957a13d46834
Ben Weier <ben.weier@gmail.com>
2021-09-24 22:29:06 +1000

    add sentry integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 import { render } from 'react-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import { QueryClientProvider } from 'react-query'
 import { App } from './components/App'
 import { AppStyles } from './components/AppStyles'
 import { queryClient } from './utilities/query-client'
+
+Sentry.init({
+  dsn: 'https://628afa9a45cd4952adf380cb9224ce2f@o121131.ingest.sentry.io/5977586',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.2,
+})
 
 render(
   <StrictMode>
