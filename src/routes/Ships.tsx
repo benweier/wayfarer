7290ed0fd4d36c3a2c402615c9b81ca8f25b32e7
Ben Weier <ben.weier@gmail.com>
2021-09-24 16:40:02 +1000

    add available ships to ship page

--- src/routes/Ships.tsx
+++ src/routes/Ships.tsx
@@ -1,5 +1,10 @@
 import tw from 'twin.macro'
+import { AvailableShips } from 'components/Ships/Available'
 
 export const ShipPage = () => {
-  return <div css={tw`container`}></div>
+  return (
+    <div css={tw`container`}>
+      <AvailableShips />
+    </div>
+  )
 }
