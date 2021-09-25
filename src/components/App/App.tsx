52188f7dedb15b264517ca3885c8ce1d40724771
Ben Weier <ben.weier@gmail.com>
2021-09-25 10:34:16 +1000

    refactor app components with sentry profiling

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,25 @@
-import { Suspense, lazy } from 'react'
+import { Suspense, lazy, StrictMode } from 'react'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
+import { HelmetProvider } from 'react-helmet-async'
+import { QueryClientProvider } from 'react-query'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AppStyles } from 'components/AppStyles'
 import { ProtectedRoute } from 'components/ProtectedRoute'
 import { Redirect } from 'components/Redirect'
 import { ROUTES } from 'config/routes'
 import { HomePage } from 'routes/Home'
 import { store } from 'store'
+import { queryClient } from 'utilities/query-client'
+
+Sentry.init({
+  dsn: 'https://628afa9a45cd4952adf380cb9224ce2f@o121131.ingest.sentry.io/5977586',
+  integrations: [new Integrations.BrowserTracing()],
+  enabled: process.env.NODE_ENV === 'production',
+  environment: process.env.NODE_ENV,
+  tracesSampleRate: 0.2,
+})
 
 const DashboardPage = lazy(() => import('routes/Dashboard').then((mod) => ({ default: mod.DashboardPage })))
 const AuthPage = lazy(() => import('routes/Auth').then((mod) => ({ default: mod.AuthPage })))
@@ -16,36 +30,43 @@ const LoanPage = lazy(() => import('routes/Loans').then((mod) => ({ default: mod
 const ShipPage = lazy(() => import('routes/Ships').then((mod) => ({ default: mod.ShipPage })))
 const LeaderboardPage = lazy(() => import('routes/Leaderboard').then((mod) => ({ default: mod.LeaderboardPage })))
 
-export const App = () => {
+export const App = Sentry.withProfiler(() => {
   return (
-    <BrowserRouter>
-      <Provider store={store}>
-        <Suspense fallback={<></>}>
-          <Routes>
-            <Route path={ROUTES.HOME} element={<HomePage />} />
-            <Route
-              path={ROUTES.DASHBOARD}
-              element={
-                <ProtectedRoute>
-                  <DashboardPage />
-                </ProtectedRoute>
-              }
-            >
-              <Route path={ROUTES.SYSTEMS} element={<SystemPage />} />
-              <Route path={ROUTES.SHIPS} element={<ShipPage />} />
-              <Route path={ROUTES.LOANS} element={<LoanPage />} />
-              <Route path={ROUTES.LEADERBOARD} element={<LeaderboardPage />} />
-              <Route path="*" element={<Redirect to={ROUTES.DASHBOARD} />} />
-            </Route>
-            <Route path={ROUTES.AUTH} element={<AuthPage />}>
-              <Route path="" element={<Redirect to={ROUTES.LOGIN} />} />
-              <Route path={ROUTES.LOGIN} element={<Login />} />
-              <Route path={ROUTES.REGISTER} element={<Register />} />
-            </Route>
-            <Route path="*" element={<Redirect to={ROUTES.HOME} />} />
-          </Routes>
-        </Suspense>
-      </Provider>
-    </BrowserRouter>
+    <StrictMode>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <AppStyles />
+          <BrowserRouter>
+            <Provider store={store}>
+              <Suspense fallback={<></>}>
+                <Routes>
+                  <Route path={ROUTES.HOME} element={<HomePage />} />
+                  <Route
+                    path={ROUTES.DASHBOARD}
+                    element={
+                      <ProtectedRoute>
+                        <DashboardPage />
+                      </ProtectedRoute>
+                    }
+                  >
+                    <Route path={ROUTES.SYSTEMS} element={<SystemPage />} />
+                    <Route path={ROUTES.SHIPS} element={<ShipPage />} />
+                    <Route path={ROUTES.LOANS} element={<LoanPage />} />
+                    <Route path={ROUTES.LEADERBOARD} element={<LeaderboardPage />} />
+                    <Route path="*" element={<Redirect to={ROUTES.DASHBOARD} />} />
+                  </Route>
+                  <Route path={ROUTES.AUTH} element={<AuthPage />}>
+                    <Route path="" element={<Redirect to={ROUTES.LOGIN} />} />
+                    <Route path={ROUTES.LOGIN} element={<Login />} />
+                    <Route path={ROUTES.REGISTER} element={<Register />} />
+                  </Route>
+                  <Route path="*" element={<Redirect to={ROUTES.HOME} />} />
+                </Routes>
+              </Suspense>
+            </Provider>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </HelmetProvider>
+    </StrictMode>
   )
-}
+})
--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,4 @@
-import { StrictMode } from 'react'
-import * as Sentry from '@sentry/react'
-import { Integrations } from '@sentry/tracing'
 import { render } from 'react-dom'
-import { HelmetProvider } from 'react-helmet-async'
-import { QueryClientProvider } from 'react-query'
 import { App } from './components/App'
-import { AppStyles } from './components/AppStyles'
-import { queryClient } from './utilities/query-client'
 
-Sentry.init({
-  dsn: 'https://628afa9a45cd4952adf380cb9224ce2f@o121131.ingest.sentry.io/5977586',
-  integrations: [new Integrations.BrowserTracing()],
-  enabled: process.env.NODE_ENV === 'production',
-  environment: process.env.NODE_ENV,
-  tracesSampleRate: 0.2,
-})
-
-render(
-  <StrictMode>
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <AppStyles />
-        <App />
-      </QueryClientProvider>
-    </HelmetProvider>
-  </StrictMode>,
-  document.getElementById('root'),
-)
+render(<App />, document.getElementById('root'))
