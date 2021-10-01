e8ca92e9e32c6dad620796ee238be84eb0b65bc0
Ben Weier <ben.weier@gmail.com>
2021-10-01 23:27:33 +1000

    use system select state hook

--- src/components/Ships/Available/Available.tsx
+++ src/components/Ships/Available/Available.tsx
@@ -1,11 +1,14 @@
 import tw from 'twin.macro'
-import { SystemSelect } from 'components/SystemSelect'
+import { SystemSelect } from 'components/Systems/Select'
+import { useSystemSelect } from 'components/Systems/Select/useSystemSelect'
 
 export const AvailableShips = () => {
+  const systemSelect = useSystemSelect()
+
   return (
     <div>
       <div css={tw`w-72`}>
-        <SystemSelect />
+        <SystemSelect {...systemSelect} />
       </div>
     </div>
   )
