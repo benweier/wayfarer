6854b0a063c67e4a1757984363fda01d83647b54
Ben Weier <ben.weier@gmail.com>
2021-10-02 14:39:46 +1000

    render owned and available ships features

--- src/routes/Ships.tsx
+++ src/routes/Ships.tsx
@@ -1,10 +1,14 @@
 import tw from 'twin.macro'
 import { AvailableShips } from 'components/Ships/Available'
+import { OwnedShips } from 'components/Ships/Owned'
 
 export const ShipPage = () => {
   return (
     <div css={tw`container`}>
-      <AvailableShips />
+      <div css={tw`grid grid-flow-row gap-10`}>
+        <OwnedShips />
+        <AvailableShips />
+      </div>
     </div>
   )
 }
