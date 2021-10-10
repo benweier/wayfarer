001ea4643d0b5f3693220c8d56d7f682105b22ab
Ben Weier <ben.weier@gmail.com>
2021-10-10 11:21:40 +1000

    refactor owned ships to display unique ship types with count

--- src/components/Ships/Owned/Owned.tsx
+++ src/components/Ships/Owned/Owned.tsx
@@ -1,39 +1,61 @@
+import { useMemo } from 'react'
 import tw from 'twin.macro'
 import { useMyShipsQuery } from 'services/spacetraders/core'
-import { YourShip } from 'types/spacetraders'
 
-const OwnedShipItem = ({ ship }: { ship: YourShip }) => {
+interface OwnedShip {
+  type: string
+  manufacturer: string
+  class: string
+  count: number
+}
+
+const OwnedShipItem = ({ ship }: { ship: OwnedShip }) => {
   return (
-    <div css={tw`my-2 shadow p-4 border border-gray-600 rounded-lg`}>
-      <div css={tw`grid grid-flow-col auto-cols-min justify-between gap-2`}>
-        <div>
-          <div css={tw`text-xs leading-none uppercase font-bold text-gray-400`}>{ship.type}</div>
-          <div css={tw`flex flex-row space-x-2 items-center py-2`}>
-            <span css={tw`text-2xl font-bold leading-10`}></span>
+    <div css={tw`shadow p-4 border border-gray-700 rounded-lg grid grid-flow-row gap-2`}>
+      <div css={tw`grid grid-flow-row gap-6`}>
+        <div css={tw`grid grid-flow-col justify-between items-center`}>
+          <div>
+            <div css={tw`text-xs leading-none uppercase font-bold text-gray-400`}>{ship.type}</div>
+            <div css={tw`text-lg font-bold`}>
+              {ship.manufacturer} {ship.class}
+            </div>
           </div>
+          <div css={tw`font-black text-2xl px-2`}>{ship.count}</div>
         </div>
-        {/* <div>
-
-        </div> */}
       </div>
-      <div css={tw`grid grid-flow-col gap-2`}></div>
     </div>
   )
 }
 
 const OwnedShipsList = () => {
-  const { data } = useMyShipsQuery()
+  const ownedShipsQuery = useMyShipsQuery()
+
+  const ownedShips = useMemo(() => {
+    const owned = ownedShipsQuery.data?.ships.reduce<{ ships: Record<string, OwnedShip> }>(
+      (obj, ship) => {
+        const count = obj.ships[ship.type]?.count ?? 0
+
+        obj.ships[ship.type] = {
+          type: ship.type,
+          manufacturer: ship.manufacturer,
+          class: ship.class,
+          count: count + 1,
+        }
+
+        return obj
+      },
+      { ships: {} },
+    ) ?? { ships: {} }
+
+    return { ships: Object.values(owned.ships).sort((a, b) => a.type.localeCompare(b.type)) }
+  }, [ownedShipsQuery.data])
 
   return (
-    <>
-      {!!data?.ships.length && (
-        <div css={tw`grid grid-cols-3 gap-4`}>
-          {data.ships.map((ship) => (
-            <OwnedShipItem key={ship.type} ship={ship} />
-          ))}
-        </div>
-      )}
-    </>
+    <div css={tw`grid grid-cols-3 gap-8`}>
+      {ownedShips.ships.map((ship) => (
+        <OwnedShipItem key={ship.type} ship={ship} />
+      ))}
+    </div>
   )
 }
 
