76b861bd03f9e3d32211453a19408f9d64be0e00
Ben Weier <ben.weier@gmail.com>
2021-10-16 11:39:40 +1000

    show marketplace listings for system and location

--- src/components/Marketplace/Marketplace.tsx
+++ src/components/Marketplace/Marketplace.tsx
@@ -1,6 +1,8 @@
 import tw from 'twin.macro'
+import { LocationSelect, useLocationSelect } from 'components/Locations/Select'
+import { SystemSelect, useSystemSelect } from 'components/Systems/Select'
 import { useMarketplaceQuery } from 'services/spacetraders/core'
-import { Marketplace } from 'types/spacetraders'
+import { Location, Marketplace } from 'types/spacetraders'
 
 const MarketplaceItem = ({ marketplace }: { marketplace: Marketplace }) => {
   return (
@@ -10,14 +12,39 @@ const MarketplaceItem = ({ marketplace }: { marketplace: Marketplace }) => {
   )
 }
 
-export const MarketplaceListings = () => {
-  const marketplaceQuery = useMarketplaceQuery({ location: 'OE-PM-TR' })
+const MarketplaceList = ({ location }: { location: Location }) => {
+  const marketplaceQuery = useMarketplaceQuery({ location: location.symbol })
+
+  if (!marketplaceQuery.data) return null
 
   return (
     <div css={tw`grid grid-flow-row gap-2`}>
-      {marketplaceQuery.data?.marketplace.map((marketplace) => {
+      {marketplaceQuery.data.marketplace.map((marketplace) => {
         return <MarketplaceItem key={marketplace.symbol} marketplace={marketplace} />
       })}
+    </div>
+  )
+}
+
+export const MarketplaceListings = () => {
+  const system = useSystemSelect()
+  const location = useLocationSelect({ system: system.selected })
+
+  return (
+    <div>
+      <div css={tw`text-xl font-bold my-4`}>MARKETPLACE</div>
+      <div css={tw`grid grid-flow-row gap-8`}>
+        <div css={tw`grid grid-flow-col auto-cols-min gap-4`}>
+          <div css={tw`w-72`}>
+            <SystemSelect {...system} />
+          </div>
+          <div css={tw`w-72`}>
+            <LocationSelect {...location} />
+          </div>
+        </div>
+      </div>
+
+      {location.selected && <MarketplaceList location={location.selected} />}
     </div>
   )
 }
