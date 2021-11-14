ce9eab4781251cbeebdef8a58a77a32259bd6463
Ben Weier <ben.weier@gmail.com>
2021-11-14 13:34:21 +1000

    define custom location interface because it was removed from history

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -1,5 +1,9 @@
-import { Location, State } from 'history'
+import { Path, State } from 'history'
 import { useLocation as useRouterLocation } from 'react-router'
+
+interface Location<T = any> extends Path {
+  state?: T
+}
 
 interface UseLocation {
   <T extends State = null>(): Location<T | null>
