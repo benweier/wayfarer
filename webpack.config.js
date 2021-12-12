d639ff186f918bf45039171201bba6ce05e5832a
Ben Weier <ben.weier@gmail.com>
2021-12-12 14:13:40 +1000

    update webpack to output esmodules

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 const dev = process.env.NODE_ENV !== 'production';
 
-const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+const HTMLPluginConfig = new HtmlWebpackPlugin({
   template: path.resolve(__dirname, 'public', 'index.html'),
   filename: 'index.html',
+  chunks: ['main'],
   inject: 'body',
+  scriptLoading: 'module',
   minify: {
     removeComments: true,
     collapseWhitespace: true,
@@ -17,18 +19,53 @@ const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
 
 const DefinePluginConfig = new webpack.DefinePlugin({
   'process.env.NODE_ENV': JSON.stringify('production'),
-});
+})
 
 module.exports = {
   entry: './src/index.tsx',
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     plugins: [new TsConfigPathsPlugin()],
-    fallback: { 'fs': false, 'path': false, 'os': false, 'module': false }
+    fallback: {
+      fs: false,
+      path: false,
+      os: false,
+      module: false,
+      util: false,
+    },
   },
   output: {
+    module: true,
+    asyncChunks: true,
+    chunkFormat: 'module',
+    chunkLoading: 'import',
+    clean: true,
     path: path.resolve(__dirname, 'build'),
-    filename: 'index.bundle.js'
+    publicPath: '/',
+    filename: '[name].[chunkhash].js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
   },
   devServer: {
     compress: true,
@@ -44,17 +81,19 @@ module.exports = {
       {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
       {
-          test: /\.(jpg|jpeg|png|gif|svg)$/,
-          use: ['file-loader'],
+        test: /\.(jpg|jpeg|png|gif|svg)$/,
+        use: ['file-loader'],
       },
-    ]
+    ],
   },
   plugins: dev
-    ? [HTMLWebpackPluginConfig, new webpack.HotModuleReplacementPlugin()]
-    : [HTMLWebpackPluginConfig, DefinePluginConfig],
+    ? [HTMLPluginConfig, new webpack.HotModuleReplacementPlugin()]
+    : [HTMLPluginConfig, DefinePluginConfig],
   mode: dev ? 'development' : 'production',
-  devtool: 'inline-source-map',
+  experiments: {
+    outputModule: true,
+  },
 }
