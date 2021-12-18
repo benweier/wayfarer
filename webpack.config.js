96d0824329b05040bba4b4f9812d1a69c3f988f3
Ben Weier <ben.weier@gmail.com>
2021-12-18 15:29:53 +1000

    add fast refresh, improve hot reloading behaviour

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
-
-const isProduction = process.env.NODE_ENV === 'production';
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const HTMLPluginConfig = new HtmlWebpackPlugin({
   template: path.resolve(__dirname, 'public', 'index.html'),
@@ -25,127 +24,130 @@ const HTMLPluginConfig = new HtmlWebpackPlugin({
 // })
 
 const DefinePluginConfig = new webpack.DefinePlugin({
-  'process.env.NODE_ENV': JSON.stringify('production'),
+  'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
 })
 
-module.exports = {
-  entry: './src/index.tsx',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    plugins: [new TsConfigPathsPlugin()],
-    fallback: {
-      fs: false,
-      path: false,
-      os: false,
-      module: false,
-      util: false,
+module.exports = (env) => {
+  const isProduction = env === 'production'
+
+  return {
+    entry: './src/index.tsx',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      plugins: [new TsConfigPathsPlugin()],
+      fallback: {
+        fs: false,
+        path: false,
+        os: false,
+        module: false,
+        util: false,
+      },
     },
-  },
-  output: {
-    module: true,
-    asyncChunks: true,
-    chunkFormat: 'module',
-    chunkLoading: 'import',
-    clean: true,
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
-    filename: '[name].[chunkhash].js',
-  },
-  optimization: {
-    moduleIds: 'deterministic',
-    runtimeChunk: false,
-    splitChunks: {
-      chunks: 'async',
-      minSize: 20000,
-      maxSize: 200000,
-      minRemainingSize: 0,
-      minChunks: 1,
-      maxAsyncSize: 200000,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      enforceSizeThreshold: 50000,
-      cacheGroups: {
-        vendors: {
-          test: /node_modules/,
-          priority: -10,
-          reuseExistingChunk: true,
-          usedExports: true
+    output: {
+      module: isProduction,
+      asyncChunks: true,
+      chunkFormat: isProduction ? 'module' : 'array-push',
+      chunkLoading: isProduction ? 'import' : 'jsonp',
+      clean: true,
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+      filename: '[name].[chunkhash].js',
+    },
+    optimization: {
+      moduleIds: 'deterministic',
+      runtimeChunk: false,
+      splitChunks: {
+        chunks: 'async',
+        minSize: 20000,
+        maxSize: 200000,
+        minRemainingSize: 0,
+        minChunks: 1,
+        maxAsyncSize: 200000,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        enforceSizeThreshold: 50000,
+        cacheGroups: {
+          vendors: {
+            test: /node_modules/,
+            priority: -10,
+            reuseExistingChunk: true,
+            usedExports: true,
+          },
+          // vendors: {
+          //   name: 'vendors',
+          //   chunks: 'all',
+          //   test: /node_modules/,
+          //   priority: -10,
+          //   reuseExistingChunk: true,
+          //   usedExports: true,
+          // },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+            usedExports: true,
+          },
         },
-        // vendors: {
-        //   name: 'vendors',
-        //   chunks: 'all',
-        //   test: /node_modules/,
-        //   priority: -10,
-        //   reuseExistingChunk: true,
-        //   usedExports: true,
+      },
+    },
+    devServer: {
+      compress: false,
+      port: 8080,
+      hot: true,
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      historyApiFallback: true,
+      open: true,
+      watchFiles: ['src/**/*.{ts|tsx}', 'public/**/*'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/i,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        // {
+        //   test: /\.css$/i,
+        //   use: [
+        //     'style-loader',
+        //     MiniCssExtractPlugin.loader,
+        //     {
+        //       loader: 'css-loader',
+        //       options: {
+        //         modules: true,
+        //         importLoaders: 1,
+        //       },
+        //     },
+        //     {
+        //       loader: 'postcss-loader',
+        //       options: {
+        //         postcssOptions: {
+        //           plugins: [
+        //             [
+        //               'postcss-preset-env',
+        //               {
+        //                 stage: 2,
+        //               }
+        //             ],
+        //           ],
+        //         },
+        //       },
+        //     },
+        //   ],
         // },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-          usedExports: true,
+        {
+          test: /\.(svg|ttf|woff|woff2|png|jpg|jpeg|gif|webm)$/i,
+          type: 'asset',
         },
-      },
+      ],
     },
-  },
-  devServer: {
-    compress: true,
-    port: 8080,
-    // http2: true,
-    hot: true,
-    static: './static',
-    historyApiFallback: true,
-    open: true,
-    liveReload: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/i,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      // {
-      //   test: /\.css$/i,
-      //   use: [
-      //     'style-loader',
-      //     MiniCssExtractPlugin.loader,
-      //     {
-      //       loader: 'css-loader',
-      //       options: {
-      //         modules: true,
-      //         importLoaders: 1,
-      //       },
-      //     },
-      //     {
-      //       loader: 'postcss-loader',
-      //       options: {
-      //         postcssOptions: {
-      //           plugins: [
-      //             [
-      //               'postcss-preset-env',
-      //               {
-      //                 stage: 2,
-      //               }
-      //             ],
-      //           ],
-      //         },
-      //       },
-      //     },
-      //   ],
-      // },
-      {
-        test: /\.(svg|ttf|woff|woff2|png|jpg|jpeg|gif|webm)$/i,
-        type: 'asset',
-      }
-    ],
-  },
-  plugins: isProduction
-    ? [HTMLPluginConfig, DefinePluginConfig]
-    : [HTMLPluginConfig, new webpack.HotModuleReplacementPlugin()],
-  mode: isProduction ? 'production' : 'development',
-  experiments: {
-    outputModule: true,
-    layers: true,
-  },
+    plugins: [HTMLPluginConfig, DefinePluginConfig, new ReactRefreshPlugin()],
+    mode: env,
+    experiments: {
+      outputModule: isProduction,
+      layers: true,
+    },
+  }
 }
