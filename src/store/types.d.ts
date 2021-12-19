c0145de67c2ab13695ca00626fee0b4996a142b0
Ben Weier <ben.weier@gmail.com>
2021-12-19 13:48:18 +1000

    use store state to infer root reducers

--- src/store/types.d.ts
+++ src/store/types.d.ts
@@ -1,7 +1,6 @@
 import { ThunkAction } from '@reduxjs/toolkit'
 import { store } from './index'
-import { reducer } from './reducers'
 
-export type RootState = ReturnType<typeof reducer>
+export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>
