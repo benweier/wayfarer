26f3ee18b4bbf0285ec79be2b5e247dd195031d9
Ben Weier <ben.weier@gmail.com>
2021-12-19 15:21:36 +1000

    create pixi stage context bridge

---
+++ src/components/Pixi/Stage.tsx
@@ -0,0 +1,23 @@
+import { ComponentProps, FC, ReactNode } from 'react'
+import { Stage as PixiStage } from '@inlet/react-pixi'
+import { ReactReduxContext } from 'react-redux'
+
+const ContextBridge: FC<{ Context: typeof ReactReduxContext; render: (children: ReactNode) => ReactNode }> = ({
+  children,
+  Context,
+  render,
+}) => {
+  return (
+    <Context.Consumer>
+      {(value) => render(<Context.Provider value={value}>{children}</Context.Provider>)}
+    </Context.Consumer>
+  )
+}
+
+export const Stage: FC<ComponentProps<typeof PixiStage>> = ({ children, ...props }) => {
+  return (
+    <ContextBridge Context={ReactReduxContext} render={(children) => <PixiStage {...props}>{children}</PixiStage>}>
+      {children}
+    </ContextBridge>
+  )
+}
