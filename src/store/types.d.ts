4e7ea2354e43f1f630142d4b10c05073ea70f6e7
Ben Weier <ben.weier@gmail.com>
2021-12-21 12:40:50 +1000

    use combined reducers return type

--- src/store/types.d.ts
+++ src/store/types.d.ts
@@ -1,6 +1,7 @@
 import { ThunkAction } from '@reduxjs/toolkit'
 import { store } from './index'
+import { reducer } from './reducers'
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof reducer>
 export type AppDispatch = typeof store.dispatch
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>
