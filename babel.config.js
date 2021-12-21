cbdde051b2a460ad012fa8ff2cf598635367daa4
Ben Weier <ben.weier@gmail.com>
2021-12-21 19:11:34 +1000

    move macros config into babel file

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,38 @@
 module.exports = (api) => {
   const BABEL_ENV = api.env()
 
-  const plugins = ['babel-plugin-twin', 'babel-plugin-styled-components', 'babel-plugin-macros']
+  const styledComponentsConfig = {
+    namespace: 'sc-',
+    pure: true,
+    ssr: false,
+    displayName: BABEL_ENV !== 'production',
+    fileName: BABEL_ENV !== 'production',
+  }
+
+  const plugins = [
+    'babel-plugin-twin',
+    [
+      'babel-plugin-styled-components',
+      styledComponentsConfig,
+    ],
+    [
+      'babel-plugin-macros',
+      {
+        twin: {
+          preset: 'styled-components',
+          styled: {
+            import: 'default',
+            from: 'styled-components/macro',
+          },
+          css: {
+            import: 'css',
+            from: 'styled-components/macro',
+          },
+        },
+        styledComponents: styledComponentsConfig,
+      },
+    ],
+  ]
 
   if (BABEL_ENV === 'development') {
     plugins.push('react-refresh/babel')
