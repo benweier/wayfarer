9f922133fdc2be903c561e0f451df9b610aacf15
Ben Weier <ben.weier@gmail.com>
2021-12-23 09:42:05 +1000

    add ts-ignore rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'import/resolver': 'typescript',
   },
   rules: {
+    '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/unbound-method': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
