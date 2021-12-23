c481b4fdf3918b8975f70690c878892d6bf98aaf
Ben Weier <ben.weier@gmail.com>
2021-12-23 10:34:27 +1000

    add use unmount hook

---
+++ src/hooks/useUnmount.ts
@@ -0,0 +1,9 @@
+import { useEffect, useRef } from 'react'
+
+export const useUnmount = (fn: () => void): void => {
+  const ref = useRef(fn)
+
+  ref.current = fn
+
+  useEffect(() => () => ref.current(), [])
+}
