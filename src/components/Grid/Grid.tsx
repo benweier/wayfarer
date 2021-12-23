889616cfffe27828d509d26c554199192a1a19a9
Ben Weier <ben.weier@gmail.com>
2021-12-24 08:38:00 +1000

    add grid component

---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,17 @@
+import tw, { styled } from 'twin.macro'
+import { alignStyles, columnStyles, justifyStyles, rowStyles, spacingStyles } from './grid.styles'
+
+export const Grid = styled.div<{
+  cols?: keyof typeof columnStyles
+  rows?: keyof typeof rowStyles
+  justify?: keyof typeof justifyStyles
+  align?: keyof typeof alignStyles
+  gap?: keyof typeof spacingStyles
+}>(({ cols, rows, justify, align, gap }) => [
+  tw`grid`,
+  cols && columnStyles[cols],
+  rows && rowStyles[rows],
+  justify && justifyStyles[justify],
+  align && alignStyles[align],
+  gap && spacingStyles[gap],
+])
