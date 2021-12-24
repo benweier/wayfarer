fd5f11fa91db033ecd93e9c21e132b3a9d3ca323
Ben Weier <ben.weier@gmail.com>
2021-12-24 10:55:20 +1000

    add variant option to use flex mode

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -2,13 +2,15 @@ import tw, { styled } from 'twin.macro'
 import { alignStyles, columnStyles, justifyStyles, rowStyles, spacingStyles } from './grid.styles'
 
 export const Grid = styled.div<{
+  variant?: 'grid' | 'flex'
   cols?: keyof typeof columnStyles
   rows?: keyof typeof rowStyles
   justify?: keyof typeof justifyStyles
   align?: keyof typeof alignStyles
   gap?: keyof typeof spacingStyles
-}>(({ cols, rows, justify, align, gap }) => [
-  tw`grid`,
+}>(({ variant = 'grid', cols, rows, justify, align, gap }) => [
+  variant === 'grid' && tw`grid`,
+  variant === 'flex' && tw`flex`,
   cols && columnStyles[cols],
   rows && rowStyles[rows],
   justify && justifyStyles[justify],
