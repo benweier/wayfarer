4f054da53adfef97dc5f8a0c28b83ec20bbbe1dc
Ben Weier <ben.weier@gmail.com>
2021-12-24 10:29:14 +1000

    move ring colours out of focus state

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,8 @@ import tw, { styled } from 'twin.macro'
 const styles = {
   DEFAULT: [
     tw`w-full text-sm font-bold bg-blue-500 leading-none px-5 py-3 rounded outline-none`,
-    tw`focus:(outline-none ring ring-emerald-400 ring-offset-2 ring-offset-gray-800)`,
+    tw`ring-emerald-400 ring-offset-2 ring-offset-gray-800`,
+    tw`focus:(outline-none ring)`,
   ],
   disabled: tw`disabled:(opacity-50 cursor-default)`,
 }
