3a1518e154b13a49e15e1eaa3fdb21d011788a12
Ben Weier <ben.weier@gmail.com>
2021-12-27 09:40:10 +1000

    update typography import

--- src/components/Caption/Caption.tsx
+++ src/components/Caption/Caption.tsx
@@ -1,5 +1,5 @@
 import tw, { styled } from 'twin.macro'
-import { Typography } from '../Typography'
+import { Typography } from '@/components/Typography'
 
 export const Caption = styled(Typography).attrs({ size: 'xs', weight: 'bold', leading: 'none' })(() => [
   tw`uppercase text-gray-400`,
