15f4b5d158a270a0b712fe58c0de6b0c7c845cfd
Ben Weier <ben.weier@gmail.com>
2022-01-03 16:07:53 +1000

    allow input to accept a forwarded ref

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,7 @@
-import { InputHTMLAttributes, ReactNode } from 'react'
+import { ForwardedRef, forwardRef, InputHTMLAttributes, ReactNode } from 'react'
 import { useFormContext } from 'react-hook-form'
 import tw from 'twin.macro'
+import { isFunction } from '@/utilities/is-function'
 
 const styles = {
   DEFAULT: [
@@ -10,17 +11,30 @@ const styles = {
   disabled: [tw`disabled:(bg-gray-700 border-gray-600 opacity-80)`],
 }
 
-export const Input = ({
-  name,
-  icon,
-  ...props
-}: InputHTMLAttributes<HTMLInputElement> & { label?: string; name: string; icon?: ReactNode }) => {
-  const methods = useFormContext()
+export const InternalInput = (
+  { name, icon, ...props }: InputHTMLAttributes<HTMLInputElement> & { label?: string; name: string; icon?: ReactNode },
+  ref: ForwardedRef<HTMLInputElement | null> | null,
+) => {
+  const { register } = useFormContext()
+  const input = register(name)
 
   return (
     <div css={tw`relative rounded-md shadow-sm`}>
-      <input {...methods.register(name)} {...props} css={[styles.DEFAULT, styles.disabled, icon && tw`pr-10`]} />
+      <input
+        {...input}
+        ref={(node) => {
+          input.ref(node)
+
+          if (ref) {
+            isFunction(ref) ? ref(node) : (ref.current = node)
+          }
+        }}
+        {...props}
+        css={[styles.DEFAULT, styles.disabled, icon && tw`pr-10`]}
+      />
       {icon && <div css={tw`absolute inset-y-0 right-0 pr-1 flex items-center`}>{icon}</div>}
     </div>
   )
 }
+
+export const Input = forwardRef(InternalInput)
