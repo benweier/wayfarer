c6412cd677f2ad04eafae13b2189292cda0700df
Ben Weier <ben.weier@gmail.com>
2022-01-03 16:05:52 +1000

    update modal dialog styles

--- src/components/Modal/Dialog.tsx
+++ src/components/Modal/Dialog.tsx
@@ -2,23 +2,25 @@ import { ReactNode } from 'react'
 import tw from 'twin.macro'
 import { ModalProps } from './types.d'
 
-export const Dialog = ({ open, children, render = (children: ReactNode) => <>{children}</> }: ModalProps) => {
+const rndr = (children: ReactNode) => <>{children}</>
+
+export const Dialog = ({ open, children, render = rndr }: ModalProps) => {
   if (!open) return null
 
   return (
     <div
-      css={tw`fixed h-screen w-screen max-h-screen max-w-full bg-gray-800 bg-opacity-75 top-0 z-50 flex justify-center items-center`}
+      css={tw`fixed h-screen w-screen max-h-screen max-w-full bg-gray-800 bg-opacity-75 inset-0 z-50 flex justify-center items-center`}
     >
       <div
         tabIndex={0}
         css={[
-          tw`fixed w-full h-full max-w-full bg-gray-800 sm:(h-auto rounded-lg maxHeight[600px] max-w-3xl border border-gray-900 shadow-xl)`,
+          tw`fixed w-full h-full max-w-full bg-gray-800 sm:(h-auto rounded-lg maxHeight[80vh] max-w-3xl border border-gray-900 shadow-xl)`,
         ]}
         role="dialog"
         aria-modal="true"
       >
         {render(
-          <div css={[tw`flex flex-col items-stretch`, tw`maxHeight[80vh]`]}>
+          <div css={[tw`flex flex-col items-stretch`]}>
             <div css={[tw`flex-auto min-h-0`]}>{children}</div>
           </div>,
         )}
