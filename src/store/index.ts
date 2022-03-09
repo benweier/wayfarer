14343f92387533d51347d5d69ce3faff88a35aaf
Ben Weier <ben.weier@gmail.com>
2022-03-09 21:43:22 +1000

    update hmr logic for store reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { setupListeners } from '@reduxjs/toolkit/query'
-import { middleware } from './middleware'
+import { spacetradersAPI } from '@/services/spacetraders/core'
 import { reducer } from './reducers'
 
 export const store = configureStore({
   reducer,
-  middleware,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(spacetradersAPI.middleware),
 })
 
-if (process.env.NODE_ENV !== 'production' && module.hot) {
-  module.hot.accept('./reducers', () => store.replaceReducer(reducer))
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./reducers', () => {
+    import('./reducers')
+      .then((mod) => {
+        store.replaceReducer(mod.reducer)
+      })
+      .catch(() => {
+        window.location.reload()
+      })
+  })
 }
-
-setupListeners(store.dispatch)
