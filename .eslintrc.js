f576d0aedb39a8032f4d6dd7c5b6adf7d6b3c8ae
Ben Weier <ben.weier@gmail.com>
2022-11-27 18:07:33 +1000

    update eslint rules for cypress, storybook, and node files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,39 @@
 module.exports = {
   plugins: ['@typescript-eslint', 'prettier', 'import'],
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
-    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
   settings: {
     'import/parsers': {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
     'import/resolver': 'typescript',
   },
+  parserOptions: {
+    project: ['tsconfig.json'],
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
   rules: {
-    '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-misused-promises': 'off',
-    '@typescript-eslint/unbound-method': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'prettier/prettier': 'error',
     'import/order': [
       'error',
       {
-        groups: ['builtin', 'external', 'internal'],
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+        pathGroups: [
+          {
+            pattern: '@/',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
         'newlines-between': 'never',
         alphabetize: {
           order: 'asc',
@@ -42,6 +42,7 @@ module.exports = {
       },
     ],
     'import/prefer-default-export': 'off',
+    'import/namespace': ['error', { allowComputed: true }],
     'sort-imports': [
       'error',
       {
@@ -49,17 +50,42 @@ module.exports = {
         ignoreDeclarationSort: true,
         ignoreMemberSort: false,
         memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        allowSeparatedGroups: false,
       },
     ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
         argsIgnorePattern: '^_',
       },
     ],
+    '@typescript-eslint/unbound-method': 'off',
   },
   overrides: [
+    {
+      files: ['.eslintrc.js', 'jest.config.js', 'tailwind.config.js'],
+      plugins: ['node'],
+      extends: ['plugin:node/recommended'],
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.node.json'],
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      env: {
+        node: true,
+      },
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
     {
       files: ['src/**/*'],
       plugins: ['react'],
@@ -67,44 +93,56 @@ module.exports = {
       env: {
         browser: true,
       },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       settings: {
         react: {
           version: 'detect',
         },
       },
-      parserOptions: {
-        project: './tsconfig.json',
-        ecmaFeatures: {
-          jsx: true,
-        },
-        ecmaVersion: 2018,
-        sourceType: 'module',
-      },
       globals: { React: 'writable' },
       rules: {
-        'prettier/prettier': 'error',
-        'jsx-a11y/anchor-is-valid': 'off',
         'react/prop-types': 'off',
         'react/require-default-props': 'off',
-        'import/order': [
-          'error',
-          {
-            groups: ['builtin', 'external', 'internal'],
-            pathGroups: [
-              {
-                pattern: 'react',
-                group: 'external',
-                position: 'before',
-              },
-            ],
-            pathGroupsExcludedImportTypes: ['react'],
-            'newlines-between': 'never',
-            alphabetize: {
-              order: 'asc',
-              caseInsensitive: true,
-            },
-          },
-        ],
+      },
+    },
+    {
+      files: ['cypress/**/*'],
+      plugins: ['cypress', 'testing-library'],
+      extends: ['plugin:cypress/recommended', 'plugin:testing-library/dom'],
+      env: {
+        'cypress/globals': true,
+      },
+      parserOptions: {
+        project: ['tsconfig.json', 'cypress/tsconfig.json'],
+      },
+    },
+    {
+      files: ['src/**/*.stories.tsx'],
+      extends: ['plugin:storybook/recommended'],
+    },
+    {
+      files: ['src/**/*.spec.tsx', 'src/**/*.test.tsx', 'test/**/*'],
+      plugins: ['jest', 'testing-library', 'jest-dom'],
+      extends: [
+        'plugin:jest/recommended',
+        'plugin:jest-dom/recommended',
+        'plugin:testing-library/dom',
+        'plugin:testing-library/react',
+      ],
+      env: {
+        'jest/globals': true,
+      },
+      settings: {
+        jest: {
+          version: 'detect',
+        },
+      },
+      rules: {
+        'testing-library/prefer-screen-queries': 'off',
       },
     },
   ],
