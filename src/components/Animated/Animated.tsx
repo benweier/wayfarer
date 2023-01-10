13c8e5efbf522c319c6c63dc1b7306fa86f9dcac
Ben Weier <ben.weier@gmail.com>
2023-01-10 23:50:45 +1000

    remove old components

--- src/components/Animated/Animated.tsx
+++
@@ -1,4 +0,0 @@
-import { animated } from '@react-spring/web'
-import { styled } from 'twin.macro'
-
-export const Animated = styled(animated.div)<any>``
--- src/components/Caption/Caption.tsx
+++
@@ -1,6 +0,0 @@
-import tw, { styled } from 'twin.macro'
-import { Typography } from '@/components/Typography'
-
-export const Caption = styled(Typography).attrs({ size: 'xs', weight: 'bold', leading: 'none' })(() => [
-  tw`uppercase text-gray-400`,
-])
