e570d652151c8418077d5f4ecec586e673d106ab
Ben Weier <ben.weier@gmail.com>
2023-01-11 23:26:59 +1000

    update twin declaration config, add custom global generics

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,13 @@
 import 'react'
 
+declare global {
+  type WithChildren<T = Record<string, unknown>> = React.PropsWithChildren<T>
+
+  type WithClassName<T = Record<string, unknown>> = T & {
+    className?: string
+  }
+}
+
 declare module 'react' {
   function forwardRef<T, P = Record<string, never>>(
     render: (props: P, ref: React.Ref<T>) => React.ReactElement | null,
