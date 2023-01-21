1316c300c273bc9aeee8c749078784fe2fca5e1a
Ben Weier <ben.weier@gmail.com>
2023-01-21 12:16:25 +1000

    remove unnecessary components

--- src/components/Link/Link.tsx
+++
@@ -1,6 +0,0 @@
-import { LinkProps, Link as RouterLink } from 'react-router-dom'
-import { cx } from '@/utilities/cx'
-
-export const Link = ({ className, ...props }: LinkProps) => {
-  return <RouterLink {...props} className={cx('link', className)} />
-}
