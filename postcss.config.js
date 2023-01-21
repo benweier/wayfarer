b50709ef00a586121c1d8c5b78e26e441c70f584
Ben Weier <ben.weier@gmail.com>
2023-01-21 12:15:01 +1000

    use tailwind-apply plugins

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-preset-env'),
     require('postcss-import'),
-    require('postcss-nested'),
+    require('tailwind-apply/postcss'),
     require('tailwindcss/nesting'),
     require('tailwindcss'),
     require('autoprefixer'),
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   darkMode: ['class'],
   content: ['./src/**/*.{js,ts,jsx,tsx}', './public/index.html'],
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'), require('tailwind-apply')],
 }
