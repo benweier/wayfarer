3557d979d869ceeb9a55827088c2aa8d7246ba4c
Ben Weier <ben.weier@gmail.com>
2023-01-22 16:51:56 +1000

    add ship queries and mutations

--- src/services/api/spacetraders/ships.ts
+++ src/services/api/spacetraders/ships.ts
@@ -1,4 +1,10 @@
-import { YourShip } from '@/types/spacetraders'
+import { AvailableShipsResponse, YourShip } from '@/types/spacetraders'
 import * as st from './core'
 
 export const myShipsQuery = () => st.get<{ ships: YourShip[] }>('/my/ships')
+
+export const shipListingsQuery = (args: { class?: string; system: string }) =>
+  st.get<AvailableShipsResponse>(`/systems/${args.system}/ship-listings`, args)
+
+export const purchaseShipMutation = (payload: { location: string; type: string }) =>
+  st.post<{ credits: number; ship: YourShip }, { location: string; type: string }>('/my/ships', payload)
