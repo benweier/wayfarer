f469f7988e63f14dc3c273b521ad3ba281db9859
Ben Weier <ben.weier@gmail.com>
2023-01-22 16:53:38 +1000

    create confirm action hook

---
+++ src/hooks/useConfirmAction.ts
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+export const useConfirmAction = (callback: () => void) => {
+  const [confirm, setConfirm] = useState(false)
+
+  const onClick = () => {
+    if (!confirm) {
+      setConfirm(true)
+      return
+    }
+
+    setConfirm(false)
+    callback()
+  }
+
+  const onReset = () => setConfirm(false)
+
+  return { confirm, onClick, onReset }
+}
