5347fc048ccfbaff3d4065838008d475c7f73f8c
Ben Weier <ben.weier@gmail.com>
2023-01-22 16:59:26 +1000

    add params argument to spacetraders fetch methods

--- src/services/api/spacetraders/core.ts
+++ src/services/api/spacetraders/core.ts
@@ -1,10 +1,16 @@
 import * as f from '@/services/fetch'
 import { getState } from '@/services/store/auth'
 
-export const get = <T>(path: string) => {
+export const get = <T>(path: string, params?: f.URLParams) => {
   const { isAuthenticated, token } = getState()
 
   const url = new URL(path, 'https://api.spacetraders.io')
+
+  if (params) {
+    Object.entries(params).forEach(([key, value]) => {
+      if (value !== undefined && value !== null) url.searchParams.append(key, String(value))
+    })
+  }
 
   if (isAuthenticated) {
     return f.get<T>(url, { headers: { Authorization: `Bearer ${token}` } })
@@ -13,14 +19,20 @@ export const get = <T>(path: string) => {
   return f.get<T>(url)
 }
 
-export const post = <T, P extends RequestInit['body']>(path: string, payload: P) => {
+export const post = <T, P extends f.RequestPayload = unknown>(path: string, payload: P, params?: f.URLParams) => {
   const { isAuthenticated, token } = getState()
 
   const url = new URL(path, 'https://api.spacetraders.io')
+
+  if (params) {
+    Object.entries(params).forEach(([key, value]) => {
+      if (value !== undefined && value !== null) url.searchParams.append(key, String(value))
+    })
+  }
 
   if (isAuthenticated) {
     return f.post<T, P>(url, payload, { headers: { Authorization: `Bearer ${token}` } })
   }
 
-  return f.post<T, P>(url)
+  return f.post<T, P>(url, payload)
 }
