5c021dc9f7e62a7fd2e79dec6d8296fd9cc92636
Ben Weier <ben.weier@gmail.com>
2023-01-26 13:10:18 +1000

    add indefinite animation styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}', './public/index.html'],
   theme: {
     container: { center: true },
+    extend: {
+      animation: {
+        indefinite: 'indefinite 2s linear infinite',
+      },
+      keyframes: {
+        indefinite: {
+          '0%': { left: 0, transform: 'translateX(-100%)' },
+          '100%': { left: '100%', transform: 'translateX(0%)' },
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'), require('tailwind-apply')],
 }
