43647148b214ee83d62f400f0954a5a73ecbc320
Ben Weier <ben.weier@gmail.com>
2023-01-29 14:58:29 +1000

    add barrel file for api query methods

---
+++ src/services/api/spacetraders/index.ts
@@ -0,0 +1,4 @@
+export { leaderboardQuery } from './leaderboard'
+export { availableLoansQuery, myLoansQuery, payLoanMutation, takeOutLoanMutation } from './loans'
+export { myShipsQuery, purchaseShipMutation, shipListingsQuery } from './ships'
+export { availableSystemsQuery } from './systems'
