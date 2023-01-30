9b1238c3c747506a7d0d7def6c5f62ee9edcd91a
Ben Weier <ben.weier@gmail.com>
2023-01-30 15:02:13 +1000

    remove eslint plugin

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@
 const baseSSL = require('@vitejs/plugin-basic-ssl')
 const react = require('@vitejs/plugin-react-swc')
 const { defineConfig } = require('vite')
-const eslint = require('vite-plugin-eslint')
 const tsconfigPaths = require('vite-tsconfig-paths')
 
 module.exports = defineConfig(({ mode }) => {
   const plugins = []
 
   if (mode === 'development') {
-    plugins.push(baseSSL(), eslint.default())
+    plugins.push(baseSSL())
   }
 
   plugins.push(tsconfigPaths.default())
