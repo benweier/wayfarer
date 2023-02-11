850c803dd2ec558a5972b1b8507a39818fe87deb
Ben Weier <ben.weier@gmail.com>
2023-02-11 18:16:30 +1000

    remove old spacetraders api methods

--- src/services/api/spacetraders/leaderboard.ts
+++
@@ -1,4 +0,0 @@
-import { LeaderboardResponse } from '@/types/spacetraders'
-import * as st from './core'
-
-export const leaderboardQuery = () => st.get<LeaderboardResponse>(`/game/leaderboard/net-worth`)
