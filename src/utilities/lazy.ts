e69cff03d96b3a99677499da6ccdcb9655ca6df0
Ben Weier <ben.weier@gmail.com>
2023-02-11 13:49:05 +1000

    refactor lazy utility to accept array of exported names

--- src/utilities/lazy.ts
+++ src/utilities/lazy.ts
@@ -3,9 +3,13 @@ import * as React from 'react'
 // https://github.com/facebook/react/issues/14603#issuecomment-726551598
 export function lazy<T extends React.ComponentType<any>, I extends { [K in N]: T }, N extends keyof I>(
   factory: () => Promise<I>,
-  name: N,
+  name: N[],
 ): I {
-  return Object.create({
-    [name]: React.lazy(() => factory().then((module) => ({ default: module[name] }))),
-  })
+  return Object.create(
+    name.reduce<{ [key: string]: React.LazyExoticComponent<I[N]> }>((obj, n) => {
+      obj[n as string] = React.lazy(() => factory().then((module) => ({ default: module[n] })))
+
+      return obj
+    }, {}),
+  )
 }
