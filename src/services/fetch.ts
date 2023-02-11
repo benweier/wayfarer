6d0e7dab1b3d0cf71b2bcac06a563d6dc0bb4eb2
Ben Weier <ben.weier@gmail.com>
2023-02-11 13:53:48 +1000

    be consistent with function assignments

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -3,16 +3,16 @@ import { http } from './http'
 export type QueryParams = Record<string, any>
 export type RequestPayload = Record<string, any> | null | undefined | unknown
 
-export async function get<T>(url: URL | RequestInfo, args: RequestInit = {}) {
-  return await http<T>(url, { ...args, method: 'GET', credentials: 'same-origin' })
+export const get = <T>(url: URL | RequestInfo, args: RequestInit = {}) => {
+  return http<T>(url, { ...args, method: 'GET', credentials: 'same-origin' })
 }
 
-export async function post<T = unknown, P extends RequestPayload = unknown>(
+export const post = <T = unknown, P extends RequestPayload = unknown>(
   url: URL | RequestInfo,
   payload?: P,
   args: RequestInit = {},
-) {
-  return await http<T>(url, {
+) => {
+  return http<T>(url, {
     ...args,
     method: 'POST',
     credentials: 'same-origin',
@@ -20,12 +20,12 @@ export async function post<T = unknown, P extends RequestPayload = unknown>(
   })
 }
 
-export async function patch<T = unknown, P extends RequestPayload = unknown>(
+export const patch = <T = unknown, P extends RequestPayload = unknown>(
   url: URL | RequestInfo,
   payload?: P,
   args: RequestInit = {},
-) {
-  return await http<T>(url, {
+) => {
+  return http<T>(url, {
     ...args,
     method: 'PATCH',
     credentials: 'same-origin',
@@ -33,16 +33,16 @@ export async function patch<T = unknown, P extends RequestPayload = unknown>(
   })
 }
 
-export async function put<T>(url: URL | RequestInfo, payload: BodyInit, args: RequestInit = {}) {
-  return await http<T>(url, { ...args, method: 'POST', credentials: 'same-origin', body: payload })
+export const put = <T>(url: URL | RequestInfo, payload: BodyInit, args: RequestInit = {}) => {
+  return http<T>(url, { ...args, method: 'POST', credentials: 'same-origin', body: payload })
 }
 
-export async function remove<T = unknown, P extends RequestPayload = unknown>(
+export const remove = <T = unknown, P extends RequestPayload = unknown>(
   url: URL | RequestInfo,
   payload: P,
   args: RequestInit = {},
-) {
-  return await http<T>(url, {
+) => {
+  return http<T>(url, {
     ...args,
     method: 'DELETE',
     credentials: 'same-origin',
--- src/services/http.ts
+++ src/services/http.ts
@@ -1,6 +1,6 @@
 export type HttpError = Response
 
-export async function http<T = unknown>(url: URL | RequestInfo, args: RequestInit): Promise<T> {
+export const http = async <T = unknown>(url: URL | RequestInfo, args: RequestInit): Promise<T> => {
   const response = await fetch(url, args)
 
   if (!response.ok) {
@@ -15,7 +15,7 @@ export async function http<T = unknown>(url: URL | RequestInfo, args: RequestIni
   }
 }
 
-export function isHttpError(err: any): err is HttpError {
+export const isHttpError = (err: any): err is HttpError => {
   if (!err) return false
 
   return typeof err === 'object' && (err.ok === false || err.status >= 400)
