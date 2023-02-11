6d71c0aef811b8ae109f26c0c78280b62fe055af
Ben Weier <ben.weier@gmail.com>
2023-02-11 16:44:15 +1000

    rename vanilla zustand store creator

--- src/services/store/createVanillaStore.ts
+++ src/services/store/createVanillaStore.ts
@@ -3,11 +3,12 @@ import { StateCreator, createStore } from 'zustand/vanilla'
 export const createVanillaStore = <T>(args: StateCreator<T, [], [], T>) => {
   const store = createStore<T>(args)
 
-  const { getState, setState, subscribe } = store
+  const { getState, setState, subscribe, destroy } = store
 
   return {
     getState,
     setState,
     subscribe,
+    destroy,
   }
 }
