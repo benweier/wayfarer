a74f92897313f2f96056841c130bd0848c5196dc
Ben Weier <ben.weier@gmail.com>
2023-02-11 16:12:13 +1000

    add new response type interfaces

--- src/types/spacetraders.d.ts
+++ src/types/spacetraders.d.ts
@@ -44,31 +44,33 @@ export type FactionResponse = {
   }>
 }
 
+export type NavigationResponse = {
+  systemSymbol: string
+  waypointSymbol: string
+  route: {
+    departure: {
+      symbol: string
+      type: string
+      systemSymbol: string
+      x: number
+      y: number
+    }
+    destination: {
+      symbol: string
+      type: string
+      systemSymbol: string
+      x: number
+      y: number
+    }
+    arrival: string
+  }
+  status: string
+  flightMode: string
+}
+
 export type ShipResponse = {
   symbol: string
-  nav: {
-    systemSymbol: string
-    waypointSymbol: string
-    route: {
-      departure: {
-        symbol: string
-        type: string
-        systemSymbol: string
-        x: number
-        y: number
-      }
-      destination: {
-        symbol: string
-        type: string
-        systemSymbol: string
-        x: number
-        y: number
-      }
-      arrival: string
-    }
-    status: string
-    flightMode: string
-  }
+  nav: NavigationResponse
   crew: {
     current: number
     capacity: number
@@ -179,6 +181,88 @@ export type ShipResponse = {
       units: number
     }>
   }
+}
+
+export type SystemsResponse = {
+  symbol: string
+  sectorSymbol: string
+  type: string
+  x: number
+  y: number
+  waypoints: [
+    {
+      symbol: string
+      type: string
+      x: number
+      y: number
+    },
+  ]
+  factions: [
+    {
+      symbol: string
+    },
+  ]
+}
+
+export type ChartResponse = {
+  waypointSymbol: string
+  submittedBy: string
+  submittedOn: string
+}
+
+export type WaypointResponse = {
+  symbol: string
+  type: string
+  systemSymbol: string
+  x: number
+  y: number
+  orbitals: Array<{
+    symbol: string
+  }>
+  faction: {
+    symbol: string
+  }
+  traits: Array<{
+    symbol: string
+    name: string
+    description: string
+  }>
+  chart: ChartResponse
+}
+
+export type MarketResponse = {
+  symbol: string
+  exports: Array<{
+    symbol: string
+    name: string
+    description: string
+  }>
+  imports: Array<{
+    symbol: string
+    name: string
+    description: string
+  }>
+  exchange: Array<{
+    symbol: string
+    name: string
+    description: string
+  }>
+  transactions: Array<{
+    shipSymbol: string
+    tradeSymbol: string
+    type: string
+    units: 1
+    pricePerUnit: 1
+    totalPrice: 1
+    timestamp: string
+  }>
+  tradeGoods: Array<{
+    symbol: string
+    tradeVolume: 1
+    supply: string
+    purchasePrice: 0
+    sellPrice: 0
+  }>
 }
 
 export type RegisterAgentResponse = {
