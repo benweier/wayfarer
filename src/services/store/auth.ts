779da5a5a004f7ed9633f5a2a666ca5a07616f27
Ben Weier <ben.weier@gmail.com>
2023-02-11 16:13:02 +1000

    refactor auth store to use the agent response object

--- src/services/store/auth.ts
+++ src/services/store/auth.ts
@@ -1,13 +1,13 @@
 import { useStore } from 'zustand'
-import { User } from '@/types/spacetraders'
+import { AgentResponse } from '@/types/spacetraders'
 import { createVanillaStore } from './base'
 
 type AuthState =
-  | { user: null; token: null; isAuthenticated: false }
-  | { user: User; token: string; isAuthenticated: true }
+  | { agent: null; token: null; isAuthenticated: false }
+  | { agent: AgentResponse; token: string; isAuthenticated: true }
 
 type AuthHandlers = {
-  setAuth: (state: { user?: User | null; token?: string | null } | null) => void
+  setAuth: (state?: { agent?: AgentResponse | null; token?: string | null } | null) => void
 }
 
 type AuthStore = AuthState & AuthHandlers
@@ -15,18 +15,18 @@ type AuthStore = AuthState & AuthHandlers
 const store = createVanillaStore<AuthStore>((set) => ({
   isAuthenticated: false,
   token: null,
-  user: null,
-  setAuth: (state: { user?: User | null; token?: string | null } | null) => {
-    if (!state) return set({ isAuthenticated: false, token: null, user: null })
+  agent: null,
+  setAuth: (state: { agent?: AgentResponse | null; token?: string | null } | null = null) => {
+    if (!state) return set({ isAuthenticated: false, token: null, agent: null })
 
-    if (state.user && state.token) {
-      return set({ isAuthenticated: true, token: state.token, user: state.user })
+    if (state.agent && state.token) {
+      return set({ isAuthenticated: true, token: state.token, agent: state.agent })
     }
 
-    return set({ isAuthenticated: false, token: null, user: null })
+    return set({ isAuthenticated: false, token: null, agent: null })
   },
 }))
 
-export const { getState, setState, subscribe, destroy } = store
+export const { getState, setState, subscribe } = store
 
 export const useAuthStore = () => useStore(store)
--- src/services/store/base.ts
+++ src/services/store/base.ts
@@ -3,12 +3,11 @@ import { StateCreator, createStore } from 'zustand/vanilla'
 export const createVanillaStore = <T>(args: StateCreator<T, [], [], T>) => {
   const store = createStore<T>(args)
 
-  const { getState, setState, subscribe, destroy } = store
+  const { getState, setState, subscribe } = store
 
   return {
     getState,
     setState,
     subscribe,
-    destroy,
   }
 }
