11b34283885ee21fa8004101362db447d4f00067
Ben Weier <ben.weier@gmail.com>
2023-02-12 18:52:15 +1000

    remove lazy loading of app component

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,20 +1,9 @@
-import { StrictMode, Suspense } from 'react'
-import { GiNorthStarShuriken } from 'react-icons/gi'
-import { lazy } from './utilities/lazy'
+import { StrictMode } from 'react'
+import { App } from '@/components/App'
 import './styles/tailwind.css'
-
-const { App } = lazy(() => import('@/components/App'), ['App'])
-
-const Loading = () => (
-  <div className="grid h-screen w-screen animate-pulse items-center justify-center text-zinc-200 dark:text-zinc-700">
-    <GiNorthStarShuriken size={96} />
-  </div>
-)
 
 export const Root = () => (
   <StrictMode>
-    <Suspense fallback={<Loading />}>
-      <App />
-    </Suspense>
+    <App />
   </StrictMode>
 )
