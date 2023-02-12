e2d5c3847ec69a4637744381f8d75bdfce0425a2
Ben Weier <ben.weier@gmail.com>
2023-02-12 14:26:50 +1000

    update app import to use lazy utility

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,8 +1,9 @@
-import { StrictMode, Suspense, lazy } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { GiNorthStarShuriken } from 'react-icons/gi'
+import { lazy } from './utilities/lazy'
 import './styles/tailwind.css'
 
-const App = lazy(() => import('@/components/App').then((mod) => ({ default: mod.App })))
+const { App } = lazy(() => import('@/components/App'), ['App'])
 
 const Loading = () => (
   <div className="grid h-screen w-screen animate-pulse items-center justify-center text-zinc-200 dark:text-zinc-700">
