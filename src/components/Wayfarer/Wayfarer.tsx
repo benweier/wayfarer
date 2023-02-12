019c06c4fcd7072eea420c39715ffbb0010d85bf
Ben Weier <ben.weier@gmail.com>
2023-02-12 18:59:20 +1000

    change word case

--- src/components/Wayfarer/Wayfarer.tsx
+++ src/components/Wayfarer/Wayfarer.tsx
@@ -1,5 +1,5 @@
 import { cx } from '@/utilities/cx'
 
 export const Wayfarer = ({ className }: WithClassName) => {
-  return <div className={cx('font-black leading-none text-black dark:text-white', className)}>WAYFARER</div>
+  return <div className={cx('font-black leading-none text-black dark:text-white', className)}>Wayfarer</div>
 }
