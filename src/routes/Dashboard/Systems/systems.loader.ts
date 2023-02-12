de4dab5d4f924d45cb3aeeffee6cbf61263a9044
Ben Weier <ben.weier@gmail.com>
2023-02-12 18:53:15 +1000

    add loader function for single system by id param

--- src/routes/Dashboard/Systems/systems.loader.ts
+++ src/routes/Dashboard/Systems/systems.loader.ts
@@ -1,10 +1,10 @@
 import { QueryClient } from '@tanstack/react-query'
 import { LoaderFunction, defer, redirect } from 'react-router'
 import { ROUTES } from '@/config/routes'
-import { getSystemsList } from '@/services/api/spacetraders'
+import { getSystemById, getSystemsList } from '@/services/api/spacetraders'
 import { getState } from '@/services/store/auth'
 
-export const loader =
+export const list =
   (client: QueryClient): LoaderFunction =>
   () => {
     const { isAuthenticated } = getState()
@@ -17,6 +17,31 @@ export const loader =
       const systems = client.ensureQueryData({ queryKey: ['systems'], queryFn: getSystemsList })
 
       return defer({ systems })
+    } catch (err) {
+      return null
+    }
+  }
+
+export const view =
+  (client: QueryClient): LoaderFunction =>
+  ({ params }) => {
+    const { isAuthenticated } = getState()
+    const { systemID } = params
+
+    if (!isAuthenticated) {
+      redirect(ROUTES.LOGIN)
+      return null
+    }
+
+    if (!systemID) {
+      redirect(ROUTES.SYSTEMS)
+      return null
+    }
+
+    try {
+      const system = client.ensureQueryData({ queryKey: ['system', systemID], queryFn: () => getSystemById(systemID) })
+
+      return defer({ system })
     } catch (err) {
       return null
     }
