9ac4e45321310c58121f1eca41828e47e29d20c4
Ben Weier <ben.weier@gmail.com>
2023-02-15 16:31:39 +1000

    extract space traders status query function

--- src/components/SpaceTradersStatus/useSpaceTradersStatus.ts
+++ src/components/SpaceTradersStatus/useSpaceTradersStatus.ts
@@ -1,23 +1,23 @@
 import { useQuery } from '@tanstack/react-query'
 import { get } from '@/services/fetch'
 
+const getSpacetradersStatus = () => {
+  const url = new URL('/game/status', 'https://api.spacetraders.io')
+  return get<{ status: string }>(url)
+}
+
 const select = (response: { status: string }) => ({
   status: response.status === 'spacetraders is currently online and available to play',
 })
 
 export const useSpaceTradersStatus = (): { status: 'UNKNOWN' | 'ONLINE' | 'OFFLINE'; isChecking: boolean } => {
-  const { data, isLoading, isFetching, isSuccess, isError } = useQuery(
-    ['status'],
-    () => {
-      const url = new URL('/game/status', 'https://api.spacetraders.io')
-      return get<{ status: string }>(url)
-    },
-    {
-      refetchInterval: 60_000,
-      staleTime: 60_000,
-      select,
-    },
-  )
+  const { data, isLoading, isFetching, isSuccess, isError } = useQuery({
+    queryKey: ['status'],
+    queryFn: getSpacetradersStatus,
+    refetchInterval: 60_000,
+    staleTime: 60_000,
+    select,
+  })
 
   return {
     status: isLoading
