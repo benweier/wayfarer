f3c92b55cbf634956aa5e483bec75a4b89ea611b
Ben Weier <ben.weier@gmail.com>
2023-02-20 10:17:32 +1000

    refactor update nav status producers, rename component props and mutation arguments

--- src/features/Fleet/ShipActions.tsx
+++ src/features/Fleet/ShipActions.tsx
@@ -4,142 +4,94 @@ import { createShipDock, createShipOrbit, createShipScanWaypoint } from '@/servi
 import { SpaceTradersResponse } from '@/services/api/spacetraders/core'
 import { ShipResponse } from '@/types/spacetraders'
 
-export const Orbit = ({ ship }: { ship: string }) => {
+const updateShipNavStatus = produce<SpaceTradersResponse<ShipResponse>, [string]>((draft, state) => {
+  draft.data.nav.status = state
+})
+
+const updateShipInFleetNavStatus = produce<SpaceTradersResponse<ShipResponse[]>, [number, string]>(
+  (draft, index, state) => {
+    draft.data[index].nav.status = state
+  },
+)
+
+export const Orbit = ({ symbol }: { symbol: string }) => {
   const client = useQueryClient()
-  const { mutate, data } = useMutation({
-    mutationKey: ['ship-orbit', ship],
-    mutationFn: (id: string) => createShipOrbit({ path: id }),
-    onMutate: (id) => {
+  const { mutate } = useMutation({
+    mutationKey: ['ship', symbol],
+    mutationFn: (symbol: string) => createShipOrbit({ path: symbol }),
+    onMutate: (symbol) => {
       void client.cancelQueries({ queryKey: ['ships'] })
-      void client.cancelQueries({ queryKey: ['ship', id] })
+      void client.cancelQueries({ queryKey: ['ship', symbol] })
 
       // Snapshot the previous value
-      const ship = client.getQueryData<SpaceTradersResponse<ShipResponse>>(['ship', id])
+      const ship = client.getQueryData<SpaceTradersResponse<ShipResponse>>(['ship', symbol])
       const ships = client.getQueryData<SpaceTradersResponse<ShipResponse[]>>(['ships'])
 
+      const index = ships?.data.findIndex((s) => s.symbol === symbol) ?? -1
+
       // Optimistically update to the new value
-      client.setQueryData(
-        ['ship', id],
-        produce(ship, (draft) => {
-          if (!draft) return
-
-          draft.data.nav.status = 'IN_ORBIT'
-
-          return draft
-        }),
-      )
-
-      client.setQueryData(
-        ['ships'],
-        produce(ships, (draft) => {
-          if (!draft) return
-
-          const index = draft.data.findIndex((s) => s.symbol === id)
-          draft.data[index].nav.status = 'IN_ORBIT'
-
-          return draft
-        }),
-      )
+      if (ship) client.setQueryData(['ship', symbol], updateShipNavStatus(ship, 'IN_ORBIT'))
+      if (ships && index > -1) client.setQueryData(['ships'], updateShipInFleetNavStatus(ships, index, 'IN_ORBIT'))
 
       // Return a context object with the snapshotted value
-      return { ship }
+      return { ship, ships }
     },
     // If the mutation fails,
     // use the context returned from onMutate to roll back
-    onError: (_err, id, context) => {
-      client.setQueryData(['ship', id], context?.ship)
+    onError: (_err, symbol, context) => {
+      client.setQueryData(['ships'], context?.ships)
+      client.setQueryData(['ship', symbol], context?.ship)
     },
     // Always refetch after error or success:
-    onSettled: (ship) => {
+    onSettled: (symbol) => {
       void client.invalidateQueries({ queryKey: ['ships'] })
-      void client.invalidateQueries({ queryKey: ['ship', ship] })
+      void client.invalidateQueries({ queryKey: ['ship', symbol] })
     },
   })
 
-  return (
-    <button
-      onClick={() => {
-        mutate(ship)
-      }}
-    >
-      Orbit
-    </button>
-  )
+  return <button onClick={() => mutate(symbol)}>Orbit</button>
 }
 
-export const Dock = ({ ship }: { ship: string }) => {
+export const Dock = ({ symbol }: { symbol: string }) => {
   const client = useQueryClient()
-  const { mutate, data } = useMutation({
-    mutationKey: ['ship-dock', ship],
-    mutationFn: (id: string) => createShipDock({ path: id }),
-    onMutate: (id) => {
+  const { mutate } = useMutation({
+    mutationKey: ['ship', symbol],
+    mutationFn: (symbol: string) => createShipDock({ path: symbol }),
+    onMutate: (symbol) => {
       // Snapshot the previous value
-      const ship = client.getQueryData<SpaceTradersResponse<ShipResponse>>(['ship', id])
+      const ship = client.getQueryData<SpaceTradersResponse<ShipResponse>>(['ship', symbol])
       const ships = client.getQueryData<SpaceTradersResponse<ShipResponse[]>>(['ships'])
 
+      const index = ships?.data.findIndex((s) => s.symbol === symbol) ?? -1
+
       // Optimistically update to the new value
-      client.setQueryData(
-        ['ship', id],
-        produce(ship, (draft) => {
-          if (!draft) return
-
-          draft.data.nav.status = 'DOCKED'
-
-          return draft
-        }),
-      )
-
-      client.setQueryData(
-        ['ships'],
-        produce(ships, (draft) => {
-          if (!draft) return
-
-          const index = draft.data.findIndex((s) => s.symbol === id)
-          draft.data[index].nav.status = 'DOCKED'
-
-          return draft
-        }),
-      )
+      if (ship) client.setQueryData(['ship', symbol], updateShipNavStatus(ship, 'DOCKED'))
+      if (ships && index > -1) client.setQueryData(['ships'], updateShipInFleetNavStatus(ships, index, 'DOCKED'))
 
       // Return a context object with the snapshotted value
-      return { ship }
+      return { ship, ships }
     },
     // If the mutation fails,
     // use the context returned from onMutate to roll back
-    onError: (_err, id, context) => {
-      client.setQueryData(['ship', id], context?.ship)
+    onError: (_err, symbol, context) => {
+      client.setQueryData(['ship', symbol], context?.ship)
+      client.setQueryData(['ships'], context?.ships)
     },
     // Always refetch after error or success:
-    onSettled: (ship) => {
+    onSettled: (symbol) => {
       void client.invalidateQueries({ queryKey: ['ships'] })
-      void client.invalidateQueries({ queryKey: ['ship', ship] })
+      void client.invalidateQueries({ queryKey: ['ship', symbol] })
     },
   })
 
-  return (
-    <button
-      onClick={() => {
-        mutate(ship)
-      }}
-    >
-      Dock
-    </button>
-  )
+  return <button onClick={() => mutate(symbol)}>Dock</button>
 }
 
-export const ScanWaypoints = ({ ship }: { ship: string }) => {
-  const { mutate, data } = useMutation({
-    mutationKey: ['ship-scan-waypoints', ship],
+export const ScanWaypoints = ({ symbol }: { symbol: string }) => {
+  const { mutate } = useMutation({
+    mutationKey: ['ship-scan-waypoints', symbol],
     mutationFn: (id: string) => createShipScanWaypoint({ path: id }),
   })
 
-  return (
-    <button
-      onClick={() => {
-        mutate(ship)
-      }}
-    >
-      Scan
-    </button>
-  )
+  return <button onClick={() => mutate(symbol)}>Scan</button>
 }
