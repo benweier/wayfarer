1f0a33cf7b59839cfcb830f500ae7d335f1d41b9
Ben Weier <ben.weier@gmail.com>
2023-02-21 14:58:49 +1000

    add status messages and status codes object maps

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,3 +1,6 @@
+type Keys<T> = keyof T
+type Values<T> = T[keyof T]
+
 export type HttpError = Response
 
 export const http = async <T = unknown>(url: URL | RequestInfo, args: RequestInit): Promise<T> => {
@@ -15,7 +18,7 @@ export const http = async <T = unknown>(url: URL | RequestInfo, args: RequestIni
   }
 }
 
-export const isHttpError = (err: any, status?: number): err is HttpError => {
+export const isHttpError = (err: any, status?: Values<typeof STATUS_CODES>): err is HttpError => {
   if (!err) return false
 
   if (typeof err === 'object' && err.ok === false) {
@@ -25,3 +28,41 @@ export const isHttpError = (err: any, status?: number): err is HttpError => {
 
   return false
 }
+
+export const STATUS_MESSAGES: Record<Keys<typeof STATUS_CODES>, string> = {
+  OK: 'OK',
+  CREATED: 'Created',
+  NO_CONTENT: 'No Content',
+  BAD_REQUEST: 'Bad Request',
+  UNAUTHORIZED: 'Unauthorized',
+  FORBIDDEN: 'Forbidden',
+  NOT_FOUND: 'Not Found',
+  METHOD_NOT_ALLOWED: 'Method Not Allowed',
+  NOT_ACCEPTABLE: 'Not Acceptable',
+  REQUEST_TIMEOUT: 'Request Timeout',
+  TEAPOT: "I'm a teapot",
+  UNPROCESSABLE_ENTITY: 'Unprocessable Entity',
+  INTERNAL_SERVER_ERROR: 'Internal Server Error',
+  BAD_GATEWAY: 'Bad Gateway',
+  SERVICE_UNAVAILABLE: 'Service Unavailable',
+  GATEWAY_TIMEOUT: 'Gateway Timeout',
+}
+
+export const STATUS_CODES = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  METHOD_NOT_ALLOWED: 405,
+  NOT_ACCEPTABLE: 406,
+  REQUEST_TIMEOUT: 408,
+  TEAPOT: 418,
+  UNPROCESSABLE_ENTITY: 422,
+  INTERNAL_SERVER_ERROR: 500,
+  BAD_GATEWAY: 502,
+  SERVICE_UNAVAILABLE: 503,
+  GATEWAY_TIMEOUT: 504,
+} as const
