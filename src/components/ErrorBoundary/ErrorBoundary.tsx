7d976cdb03c6e24c2f66ed3302114fb79a0a749c
Ben Weier <ben.weier@gmail.com>
2023-02-21 11:57:17 +1000

    remove children on error component type

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -2,7 +2,7 @@ import { FC, cloneElement, createElement, isValidElement } from 'react'
 import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary'
 import { ErrorBoundaryProps, ErrorComponentProps } from './types'
 
-const DefaultErrorFallback: FC<WithChildren<ErrorComponentProps>> = () => <></>
+const DefaultErrorFallback: FC<ErrorComponentProps> = () => <></>
 
 export const ErrorBoundary = ({
   component = <DefaultErrorFallback />,
--- src/components/QuerySuspenseBoundary/types.d.ts
+++ src/components/QuerySuspenseBoundary/types.d.ts
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren, ReactNode } from 'react'
 import { ErrorComponentProps } from '@/components/ErrorBoundary'
 
 export type QuerySuspenseBoundaryProps = {
-  error?: ReactNode | FC<PropsWithChildren<ErrorComponentProps>>
+  error?: ReactNode | FC<ErrorComponentProps>
   onError?: (error: Error) => void
   fallback?: ReactNode
 }
