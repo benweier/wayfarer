f568840e28760f1dc6d3fa2819f9a5b97f12387d
Ben Weier <ben.weier@gmail.com>
2023-02-24 16:03:58 +1000

    add tabs for waypoint services

--- src/features/Systems/ViewWaypoint.tsx
+++ src/features/Systems/ViewWaypoint.tsx
@@ -1,7 +1,16 @@
+import { Tab } from '@headlessui/react'
 import { useQuery } from '@tanstack/react-query'
 import { Link } from 'react-router-dom'
 import { WAYPOINT_TYPE } from '@/config/constants'
 import { getWaypointById } from '@/services/api/spacetraders'
+import { cx } from '@/utilities/cx'
+
+const tabs = [
+  { title: 'Fleet', content: <>Fleet</> },
+  { title: 'Market', content: <>Market</> },
+  { title: 'Shipyard', content: <>Shipyard</> },
+  { title: 'Jump Gate', content: <>Jump Gate</> },
+]
 
 export const ViewWaypoint = ({ systemID, waypointID }: { systemID: string; waypointID: string }) => {
   const { data, isSuccess } = useQuery({
@@ -27,6 +36,35 @@ export const ViewWaypoint = ({ systemID, waypointID }: { systemID: string; waypo
           </Link>
         </div>
       </div>
+
+      <Tab.Group as="div" className="grid gap-4">
+        <Tab.List className="isolate flex divide-x divide-zinc-200 rounded-lg border border-zinc-200 dark:divide-zinc-700 dark:border-zinc-700">
+          {tabs.map((tab) => (
+            <Tab
+              key={tab.title}
+              className={({ selected }) =>
+                cx(
+                  'first:rounded-l-md last:rounded-r-md',
+                  'group relative min-w-0 flex-1 overflow-hidden py-3 px-6 text-center text-sm font-medium focus:z-10',
+                  'ring-blue-400/50 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                  {
+                    'bg-white text-zinc-900 dark:bg-zinc-700/50 dark:text-white': selected,
+                    'bg-zinc-100 text-zinc-600 hover:bg-zinc-100/50 dark:bg-zinc-900/50 dark:text-zinc-300 dark:hover:bg-zinc-700/25':
+                      !selected,
+                  },
+                )
+              }
+            >
+              {tab.title}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels>
+          {tabs.map((tab) => (
+            <Tab.Panel key={tab.title}>{tab.content}</Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   )
 }
