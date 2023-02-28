975f614646f372f8fa517008e1e12c6b4698629a
Ben Weier <ben.weier@gmail.com>
2023-02-28 11:58:57 +1000

    update mapping of route error element messages

--- src/components/RouteErrorElement/RouteErrorElement.tsx
+++ src/components/RouteErrorElement/RouteErrorElement.tsx
@@ -1,12 +1,12 @@
-import { ReactNode } from 'react'
 import { useRouteError } from 'react-router-dom'
-import { NotFound } from '@/components/NotFound'
+import { STATUS_CODES, STATUS_MESSAGES } from '@/services/http'
 
-const errorElements: { [key: number]: ReactNode } = {
-  401: <NotFound />,
-  404: <NotFound />,
-  422: <NotFound />,
-  500: <NotFound />,
+const messages: { [key: number]: string } = {
+  [STATUS_CODES.UNAUTHORIZED]: STATUS_MESSAGES.UNAUTHORIZED,
+  [STATUS_CODES.FORBIDDEN]: STATUS_MESSAGES.FORBIDDEN,
+  [STATUS_CODES.NOT_FOUND]: STATUS_MESSAGES.NOT_FOUND,
+  [STATUS_CODES.UNPROCESSABLE_ENTITY]: STATUS_MESSAGES.UNPROCESSABLE_ENTITY,
+  [STATUS_CODES.INTERNAL_SERVER_ERROR]: STATUS_MESSAGES.INTERNAL_SERVER_ERROR,
 }
 
 const isRouteErrorResponse = (error: any): error is Response => {
@@ -17,7 +17,13 @@ export const RouteErrorElement = () => {
   const error: any = useRouteError()
 
   if (isRouteErrorResponse(error)) {
-    return <>{errorElements[error.status]}</> ?? <></>
+    return (
+      (
+        <div className="flex h-full grow flex-col items-center justify-center p-5">
+          <div className="text-5xl font-black">{messages[error.status]}</div>
+        </div>
+      ) ?? <></>
+    )
   }
 
   return <></>
