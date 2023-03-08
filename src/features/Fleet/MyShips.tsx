d436232a59af51963137359832bbafb377da5fea
Ben Weier <ben.weier@gmail.com>
2023-03-08 17:56:02 +1000

    clean up layout of ship list item

--- src/features/Fleet/MyShips.tsx
+++ src/features/Fleet/MyShips.tsx
@@ -49,45 +49,41 @@ const Ship = ({ ship }: { ship: ShipResponse }) => {
     >
       <div className="flex flex-row items-center justify-start gap-8">
         <div className="flex flex-1 flex-row items-center justify-between gap-4">
-          <div className="[width:250px]">
-            <div className="flex items-center gap-2">
-              <span className="text-xl font-bold">
-                <Link to={`/fleet/ship/${ship.symbol}`} className="link">
-                  {ship.symbol}
-                </Link>
-              </span>
+          <div className="[width:180px]">
+            <Link to={`/fleet/ship/${ship.symbol}`} className="link text-xl font-bold">
+              {ship.symbol}
+            </Link>
+          </div>
+          <div className="flex flex-row items-end gap-2 [width:400px]">
+            <div className="flex gap-8">
+              <div>
+                <div className="text-xs font-medium uppercase opacity-60">System</div>
+                <div className="font-semibold">
+                  <Link className="link" to={`${ROUTES.SYSTEMS}/${ship.nav.systemSymbol}`}>
+                    {ship.nav.systemSymbol}
+                  </Link>
+                </div>
+              </div>
+              <div>
+                <div className="text-xs font-medium uppercase opacity-60">Waypoint</div>
+                <div className="font-semibold">
+                  <Link
+                    className="link"
+                    to={`${ROUTES.SYSTEMS}/${ship.nav.systemSymbol}/waypoint/${ship.nav.waypointSymbol}`}
+                  >
+                    {ship.nav.waypointSymbol}
+                  </Link>
+                </div>
+              </div>
             </div>
-            <div className="flex flex-row gap-2">
-              <div className="rounded bg-black py-1 px-2 text-xs font-bold">
-                {SHIP_NAV_STATUS[ship.nav.status] ?? ship.nav.status}
-              </div>
-              <div className="rounded bg-black py-1 px-2 text-xs font-bold">
-                {SHIP_NAV_FLIGHT_MODE[ship.nav.flightMode] ?? ship.nav.flightMode}
-              </div>
+            <div className="rounded bg-black py-1 px-2 text-xs font-bold">
+              {SHIP_NAV_STATUS[ship.nav.status] ?? ship.nav.status}
+            </div>
+            <div className="rounded bg-black py-1 px-2 text-xs font-bold">
+              {SHIP_NAV_FLIGHT_MODE[ship.nav.flightMode] ?? ship.nav.flightMode}
             </div>
           </div>
-          <div className="flex gap-8 [width:280px]">
-            <div>
-              <div className="text-xs font-medium uppercase opacity-60">System</div>
-              <div className="font-semibold">
-                <Link className="link" to={`${ROUTES.SYSTEMS}/${ship.nav.systemSymbol}`}>
-                  {ship.nav.systemSymbol}
-                </Link>
-              </div>
-            </div>
-            <div>
-              <div className="text-xs font-medium uppercase opacity-60">Waypoint</div>
-              <div className="font-semibold">
-                <Link
-                  className="link"
-                  to={`${ROUTES.SYSTEMS}/${ship.nav.systemSymbol}/waypoint/${ship.nav.waypointSymbol}`}
-                >
-                  {ship.nav.waypointSymbol}
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="flex gap-8 [width:400px]">
+          <div className="flex gap-8 [width:350px]">
             <div className="flex items-center gap-2">
               <BoltIcon className="h-5 w-5 text-teal-500" />
               <div className="text-sm">
