487375408fa9e775379dabe401df557c509c5bf5
Ben Weier <ben.weier@gmail.com>
2023-03-09 22:56:25 +1000

    make loadout display in column on mobile

--- src/features/Ship/Loadout.tsx
+++ src/features/Ship/Loadout.tsx
@@ -2,7 +2,7 @@ import { ShipResponse } from '@/types/spacetraders'
 
 export const Loadout = ({ ship }: { ship: ShipResponse }) => {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
       <div className="flex flex-col gap-4">
         <div className="text-overline text-center">Modules</div>
         <div className="flex flex-col gap-2">
