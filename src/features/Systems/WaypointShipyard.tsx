81fce0766003603d998b7fb9b223164b4a7321ca
Ben Weier <ben.weier@gmail.com>
2023-03-12 16:13:57 +1000

    display list of available ships

--- src/features/Systems/WaypointShipyard.tsx
+++ src/features/Systems/WaypointShipyard.tsx
@@ -11,9 +11,28 @@ export const WaypointShipyard = ({ systemID, waypointID }: { systemID: string; w
 
   if (!isSuccess) return null
 
-  const shipyard = data.data
+  const ships = data.data.ships ?? []
 
-  return <></>
+  return (
+    <div className="grid gap-4">
+      <div className="grid grid-cols-1 gap-2">
+        {ships.map((ship) => {
+          return (
+            <div
+              key={ship.type}
+              className="flex flex-col gap-2 rounded bg-zinc-500 bg-opacity-5 py-3 px-4 dark:bg-opacity-10"
+            >
+              <div className="flex items-center justify-between gap-8 lg:justify-start">
+                <span className="font-semibold">{ship.name}</span>
+                <span>{new Intl.NumberFormat('en-US').format(ship.purchasePrice)}</span>
+              </div>
+              <div className="text-secondary text-sm">{ship.description}</div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export const ShipyardError = ({ error }: ErrorComponentProps) => {
