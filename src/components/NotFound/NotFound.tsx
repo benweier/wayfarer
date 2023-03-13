00b06d59c1b129efad8a3f88b3f459b15cc5768e
Ben Weier <ben.weier@gmail.com>
2023-03-13 11:51:14 +1000

    reduce text size

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,7 +1,7 @@
 export const NotFound = () => {
   return (
     <div className="flex h-full grow flex-col items-center justify-center p-5">
-      <div className="text-7xl font-black">Not Found</div>
+      <div className="text-5xl font-black">Not Found</div>
     </div>
   )
 }
