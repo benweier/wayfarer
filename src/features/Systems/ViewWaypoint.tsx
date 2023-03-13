1ab34787aa496fe8463ba46b3f28b814c633a04c
Ben Weier <ben.weier@gmail.com>
2023-03-13 23:57:35 +1000

    move tab styles to tailwind theme

--- src/features/Systems/ViewWaypoint.tsx
+++ src/features/Systems/ViewWaypoint.tsx
@@ -43,24 +43,10 @@ export const ViewWaypoint = ({ systemID, waypointID }: { systemID: string; waypo
         </div>
       </div>
 
-      <Tab.Group as="div" className="grid gap-4">
-        <Tab.List className="isolate flex divide-x divide-zinc-200 rounded-lg border border-zinc-200 dark:divide-zinc-700 dark:border-zinc-700">
+      <Tab.Group as="div" className="tab-group">
+        <Tab.List className="tab-list">
           {tabs.map((tab) => (
-            <Tab
-              key={tab.title}
-              className={({ selected }) =>
-                cx(
-                  'first:rounded-l-md last:rounded-r-md',
-                  'group relative min-w-0 flex-1 overflow-hidden py-3 px-6 text-center text-sm font-medium focus:z-10',
-                  'ring-blue-400/50 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                  {
-                    'bg-white text-zinc-900 dark:bg-zinc-700/50 dark:text-white': selected,
-                    'bg-zinc-100 text-zinc-600 hover:bg-zinc-100/50 dark:bg-zinc-900/50 dark:text-zinc-300 dark:hover:bg-zinc-700/25':
-                      !selected,
-                  },
-                )
-              }
-            >
+            <Tab key={tab.title} className={({ selected }) => cx('tab group', { selected: selected })}>
               {tab.title}
             </Tab>
           ))}
