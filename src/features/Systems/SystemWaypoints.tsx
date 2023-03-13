ce37ef332c4f1a484f84bd46e501a2c75772677c
Ben Weier <ben.weier@gmail.com>
2023-03-13 12:31:27 +1000

    tweak system waypoint styles

--- src/features/Systems/SystemWaypoints.tsx
+++ src/features/Systems/SystemWaypoints.tsx
@@ -2,22 +2,36 @@ import { Link } from 'react-router-dom'
 import { WAYPOINT_TYPE } from '@/config/constants'
 import { ROUTES } from '@/config/routes'
 import { SystemWaypoint, SystemsResponse } from '@/types/spacetraders'
+import { cx } from '@/utilities/cx'
 
 const Waypoint = ({ systemID, waypoint }: { systemID: string; waypoint: SystemWaypoint }) => {
   return (
-    <div className="flex flex-col items-center justify-between gap-2 rounded bg-zinc-200/50 p-4 shadow-sm dark:bg-zinc-700/25 md:flex-row md:flex-wrap">
-      <div className="flex gap-1">
-        <div className="text-lg font-black leading-none">
-          <Link className="link" to={`${ROUTES.SYSTEMS}/${systemID}/waypoint/${waypoint.symbol}`}>
-            {waypoint.symbol}
-          </Link>
-          <div className="text-base">
-            <span className="font-medium">{WAYPOINT_TYPE[waypoint.type]}</span>{' '}
-            <span className="font-light">
-              ({waypoint.x}, {waypoint.y})
-            </span>
-          </div>
-        </div>
+    <div className="flex flex-col justify-between rounded bg-zinc-200/50 p-4 shadow-sm dark:bg-zinc-700/25">
+      <div>
+        <Link className="link text-lg" to={`${ROUTES.SYSTEMS}/${systemID}/waypoint/${waypoint.symbol}`}>
+          {waypoint.symbol}
+        </Link>
+      </div>
+      <div className="flex items-center justify-between gap-1">
+        <span
+          className={cx('rounded-sm px-2 py-1 text-xs font-bold uppercase leading-none', {
+            'bg-slate-500 text-slate-200': waypoint.type === 'MOON',
+            'bg-orange-600 text-orange-100': waypoint.type === 'GAS_GIANT',
+            'bg-yellow-300 text-yellow-900': waypoint.type === 'NEBULA',
+            'bg-lime-300 text-lime-900': waypoint.type === 'ASTEROID_FIELD',
+            'bg-emerald-500 text-emerald-50': waypoint.type === 'PLANET',
+            'bg-cyan-300 text-cyan-900': waypoint.type === 'DEBRIS_FIELD',
+            'bg-fuchsia-600 text-fuchsia-100': waypoint.type === 'ORBITAL_STATION',
+            'bg-zinc-100 text-zinc-900': waypoint.type === 'JUMP_GATE',
+            'bg-zinc-900 text-zinc-50': waypoint.type === 'GRAVITY_WELL',
+          })}
+        >
+          {WAYPOINT_TYPE[waypoint.type]}
+        </span>
+
+        <span className="text-secondary text-sm">
+          ({waypoint.x}, {waypoint.y})
+        </span>
       </div>
     </div>
   )
