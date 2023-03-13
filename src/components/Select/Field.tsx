34a8d4a79a4bf7476f86272e89bf515ee3a867cb
Ben Weier <ben.weier@gmail.com>
2023-03-13 13:50:46 +1000

    clean up select field styles, icons, and selected value comparison

--- src/components/Select/Field.tsx
+++ src/components/Select/Field.tsx
@@ -1,5 +1,5 @@
 import { Listbox } from '@headlessui/react'
-import { HiCheck, HiSelector } from 'react-icons/hi'
+import { CheckCircleIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import { cx } from '@/utilities/cx'
 import { SelectOption } from './types'
 
@@ -9,53 +9,54 @@ export const SelectField = <T extends SelectOption = SelectOption>({
   options = [],
   onChange,
 }: {
-  label: string
+  label?: string
   selected?: T | null
   options?: T[]
   onChange: (value?: T) => void
 }) => {
   return (
-    <Listbox value={selected} onChange={onChange}>
-      <>
+    <Listbox value={selected} by={(a, b) => a?.id === b?.id} onChange={onChange} as="div" className="relative">
+      {label && (
         <Listbox.Label as="label" className="label">
           {label}
         </Listbox.Label>
-        <div className="relative mt-1">
-          <Listbox.Button className="relative w-full cursor-default rounded-md border-2 border-gray-500 bg-gray-700 py-2 pl-4 pr-10 text-left text-sm shadow-inner focus:border-gray-800 focus:outline-none focus:ring focus:ring-emerald-400">
-            <span className="block truncate font-semibold">{selected?.name ?? <>&nbsp;</>}</span>
-            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-              <HiSelector size={20} className="text-gray-400" aria-hidden="true" />
-            </span>
-          </Listbox.Button>
+      )}
 
-          {options.length > 0 && (
-            <Listbox.Options className="absolute z-10 mt-1 max-h-48 w-full overflow-auto rounded-md bg-gray-700 text-sm outline-none focus:ring-2 focus:ring-emerald-400">
-              {options.map((option) => (
-                <Listbox.Option key={option.id} value={option} className="relative">
-                  {({ selected, active }) => (
-                    <>
-                      <span
-                        className={cx(
-                          'relative m-1 block cursor-default select-none truncate rounded py-2 pl-3 pr-9 transition-colors duration-100 ease-in-out',
-                          { 'bg-gray-600 bg-opacity-80': active },
-                        )}
-                      >
-                        {option.name}
+      <Listbox.Button className="select">
+        <span className="block truncate">{selected?.name ?? <>&nbsp;</>}</span>
+        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+          <ChevronUpDownIcon className="h-4 w-4 text-zinc-400" aria-hidden="true" />
+        </span>
+      </Listbox.Button>
+
+      {options.length > 0 && (
+        <Listbox.Options className="absolute z-10 mt-1 max-h-48 w-full overflow-auto rounded-md border-2 border-zinc-100 bg-white/90 text-sm outline-none backdrop-blur-md dark:border-zinc-800 dark:bg-zinc-900/90">
+          {options.map((option) => (
+            <Listbox.Option key={option.id} value={option} className="relative">
+              {({ selected, active }) => {
+                return (
+                  <div className="relative p-1">
+                    <span
+                      className={cx(
+                        'relative block cursor-default select-none truncate rounded py-2 pl-3 pr-9 transition-colors duration-100 ease-in-out',
+                        { 'bg-zinc-900/5 dark:bg-zinc-100/10': active },
+                      )}
+                    >
+                      {option.name}
+                    </span>
+
+                    {selected ? (
+                      <span className="absolute inset-y-0 right-0 flex items-center pr-4">
+                        <CheckCircleIcon aria-hidden="true" className="h-4 w-4 text-emerald-500" />
                       </span>
-
-                      {selected ? (
-                        <span className="absolute inset-y-0 right-0 flex items-center pr-4">
-                          <HiCheck size={20} aria-hidden="true" className="text-emerald-400" />
-                        </span>
-                      ) : null}
-                    </>
-                  )}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          )}
-        </div>
-      </>
+                    ) : null}
+                  </div>
+                )
+              }}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      )}
     </Listbox>
   )
 }
