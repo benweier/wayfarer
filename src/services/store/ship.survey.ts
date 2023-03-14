f87bc3c1742038521a8c55521c927a82a36b7799
Ben Weier <ben.weier@gmail.com>
2023-03-14 17:17:46 +1000

    create ship survey state store

---
+++ src/services/store/ship.survey.ts
@@ -0,0 +1,41 @@
+import { produce } from 'immer'
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+import { SurveyResponse } from '@/types/spacetraders'
+
+type ShipSurveyState = { surveys: SurveyResponse[] }
+
+type ShipSurveyHandlers = {
+  addSurvey: (survey: SurveyResponse) => void
+  removeSurvey: (signature: string) => void
+}
+
+type ShipSurveyStore = ShipSurveyState & ShipSurveyHandlers
+
+export const useShipSurveyStore = create<ShipSurveyStore>()(
+  persist(
+    (set, get) => ({
+      surveys: [],
+      addSurvey: (survey) => {
+        return set(
+          produce((draft) => {
+            draft.surveys.push(survey)
+          }),
+        )
+      },
+      removeSurvey: (signature) => {
+        const index = get().surveys.findIndex((survey) => survey.signature === signature)
+        return set(
+          produce((draft) => {
+            if (index > -1) draft.surveys.splice(index, 1)
+          }),
+        )
+      },
+    }),
+    {
+      version: 0,
+      name: 'ship.surveys',
+      partialize: (state) => ({ surveys: state.surveys }),
+    },
+  ),
+)
