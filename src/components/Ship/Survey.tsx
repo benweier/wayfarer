2feaa91d347a4618bb6be867369de7ed1ce90bac
Ben Weier <ben.weier@gmail.com>
2023-03-14 20:04:29 +1000

    update survey list output, add discard and extract actions

--- src/components/Ship/Survey.tsx
+++ src/components/Ship/Survey.tsx
@@ -1,8 +1,9 @@
 import { Actions, Cooldown } from '@/components/Ship'
+import { TRADE_SYMBOL } from '@/config/constants'
 import { useShipSurveyStore } from '@/services/store/ship.survey'
 import { ShipResponse } from '@/types/spacetraders'
 
-const List = () => {
+const List = ({ ship }: { ship: ShipResponse }) => {
   const { surveys, removeSurvey } = useShipSurveyStore((state) => ({
     surveys: state.surveys,
     removeSurvey: state.removeSurvey,
@@ -11,17 +12,39 @@ const List = () => {
   return (
     <div className="grid grid-cols-1 gap-2 lg:grid-cols-3">
       {surveys.map((survey) => {
+        const expiration = new Date(survey.expiration)
+
         return (
           <div
             key={survey.signature}
-            className="grid gap-2 rounded bg-zinc-500 bg-opacity-5 py-3 px-4 dark:bg-opacity-10"
+            className="flex flex-col gap-6 rounded bg-zinc-500 bg-opacity-5 py-3 px-4 dark:bg-opacity-10"
           >
             <div className="flex items-center justify-between gap-4">
-              <span className="font-semibold" onClick={() => removeSurvey(survey.signature)}>
-                {survey.size}
-              </span>
+              <div className="font-semibold">{survey.signature}</div>
+              <div className="text-secondary text-sm">{survey.size}</div>
             </div>
-            <div className="text-secondary text-sm">{survey.expiration}</div>
+
+            <div className="grid grid-cols-2 gap-2">
+              {survey.deposits.map((deposit, index) => (
+                <div key={index} className="text-sm">
+                  {TRADE_SYMBOL[deposit.symbol] ?? deposit.symbol}
+                </div>
+              ))}
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <button className="btn btn-outline btn-danger" onClick={() => removeSurvey(survey.signature)}>
+                Discard
+              </button>
+              <Actions.Extract
+                ship={ship}
+                survey={survey}
+                trigger={<button className="btn btn-confirm">Extract</button>}
+              />
+            </div>
+            <div className="text-secondary p-2 text-center text-sm">
+              {expiration.toLocaleDateString()} {expiration.toLocaleTimeString()}
+            </div>
           </div>
         )
       })}
@@ -42,7 +65,7 @@ export const Survey = ({ ship }: { ship: ShipResponse }) => {
         </div>
       </div>
 
-      <List />
+      <List ship={ship} />
     </div>
   )
 }
