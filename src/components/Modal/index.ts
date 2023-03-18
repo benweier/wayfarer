b7c8c787373b912e19718e996e73c23dc287251a
Ben Weier <ben.weier@gmail.com>
2023-03-18 23:47:53 +1000

    update export to include trigger component off the modal object

--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -1,3 +1,7 @@
-export { Modal } from './Modal.Context'
+import { Modal as ModalComponent } from './Modal.Context'
+import { Trigger } from './Modal.Trigger'
+
 export { useModalContext } from './useModalContext.hook'
 export type { ModalProps, ModalDialogProps, ModalImperativeRef } from './modal.types'
+
+export const Modal = Object.assign(ModalComponent, { Trigger })
