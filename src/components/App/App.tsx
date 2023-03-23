271364414565dd42125a9a4f5415e2935b655203
Ben Weier <ben.weier@gmail.com>
2023-03-23 10:40:11 +1000

    increase loading text contrast

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -46,7 +46,7 @@ Sentry.init({
 const sentryCreateBrowserRouter = Sentry.wrapCreateBrowserRouter(createBrowserRouter)
 
 const Loading = () => (
-  <div className="flex h-screen w-screen animate-pulse items-center justify-center text-5xl font-black text-zinc-900/5 dark:text-zinc-500/10">
+  <div className="flex h-screen w-screen animate-pulse items-center justify-center text-5xl font-black text-zinc-900/20 dark:text-zinc-500/40">
     <div>Wayfarer</div>
   </div>
 )
