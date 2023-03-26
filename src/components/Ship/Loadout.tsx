9b1ecc0ecc61a4ca84d84441a9be5ccfa9fe305c
Ben Weier <ben.weier@gmail.com>
2023-03-26 12:12:44 +1000

    update ship core layout, split loadout item component

--- src/components/Ship/Loadout.tsx
+++ src/components/Ship/Loadout.tsx
@@ -1,5 +1,14 @@
 import { ShipResponse } from '@/types/spacetraders'
 
+const LoadoutItem = ({ name, description }: { name: string; description: string }) => {
+  return (
+    <div className="grid gap-2 rounded bg-zinc-500 bg-opacity-5 py-3 px-4 dark:bg-opacity-10">
+      <span className="font-semibold">{name}</span>
+      <span className="text-secondary text-sm">{description}</span>
+    </div>
+  )
+}
+
 export const Loadout = ({ ship }: { ship: ShipResponse }) => {
   return (
     <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
@@ -7,38 +16,25 @@ export const Loadout = ({ ship }: { ship: ShipResponse }) => {
         <div className="text-overline text-center">Modules</div>
         <div className="flex flex-col gap-2">
           {ship.modules.map((module, index) => (
-            <div key={index} className="grid gap-2 rounded bg-zinc-500 bg-opacity-5 py-3 px-4 dark:bg-opacity-10">
-              <span className="font-semibold">{module.name}</span>
-              <span className="text-secondary text-sm">{module.description}</span>
-            </div>
+            <LoadoutItem key={index} name={module.name} description={module.description} />
           ))}
         </div>
       </div>
       <div className="flex flex-col gap-4">
-        <div className="text-overline text-center">Core</div>
-        <div className="flex flex-col gap-2">
-          <div className="grid gap-2 rounded bg-zinc-500 bg-opacity-5 py-3 px-4 dark:bg-opacity-10">
-            <span className="font-semibold">{ship.frame.name}</span>
-            <span className="text-secondary text-sm">{ship.frame.description}</span>
-          </div>
-          <div className="grid gap-2 rounded bg-zinc-500 bg-opacity-5 py-3 px-4 dark:bg-opacity-10">
-            <span className="font-semibold">{ship.reactor.name}</span>
-            <span className="text-secondary text-sm">{ship.reactor.description}</span>
-          </div>
-          <div className="grid gap-2 rounded bg-zinc-500 bg-opacity-5 py-3 px-4 dark:bg-opacity-10">
-            <span className="font-semibold">{ship.engine.name}</span>
-            <span className="text-secondary text-sm">{ship.engine.description}</span>
-          </div>
-        </div>
+        <div className="text-overline text-center">Frame</div>
+        <LoadoutItem name={ship.frame.name} description={ship.frame.description} />
+
+        <div className="text-overline text-center">Reactor</div>
+        <LoadoutItem name={ship.reactor.name} description={ship.reactor.description} />
+
+        <div className="text-overline text-center">Engine</div>
+        <LoadoutItem name={ship.engine.name} description={ship.engine.description} />
       </div>
       <div className="flex flex-col gap-4">
         <div className="text-overline text-center">Mounts</div>
         <div className="flex flex-col gap-2">
           {ship.mounts.map((mount, index) => (
-            <div key={index} className="grid gap-2 rounded bg-zinc-500 bg-opacity-5 py-3 px-4 dark:bg-opacity-10">
-              <span className="font-semibold">{mount.name}</span>
-              <span className="text-secondary text-sm">{mount.description}</span>
-            </div>
+            <LoadoutItem key={index} name={mount.name} description={mount.description} />
           ))}
         </div>
       </div>
