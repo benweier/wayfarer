61275863381f87f93d21c86f5142e0275275531b
Ben Weier <ben.weier@gmail.com>
2023-04-28 21:43:53 +1000

    fix error component import

--- src/components/query-suspense-boundary/query-suspense-boundary.types.ts
+++ src/components/query-suspense-boundary/query-suspense-boundary.types.ts
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from 'react'
-import { ErrorComponentProps } from '@/components/ErrorBoundary'
+import { ErrorComponentProps } from '@/components/error-boundary'
 
 export type QuerySuspenseBoundaryProps = {
   error?: ReactNode | FC<ErrorComponentProps>
