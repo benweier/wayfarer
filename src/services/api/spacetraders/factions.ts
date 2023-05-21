3369e3ffbfd663ccb23cfe9bcf168628ec391b3f
Ben Weier <ben.weier@gmail.com>
2023-05-21 16:49:39 +1000

    add factions api methods

---
+++ src/services/api/spacetraders/factions.ts
@@ -0,0 +1,8 @@
+import { Meta, SpaceTradersResponse, queryFnFactory } from '@/services/api/spacetraders/core'
+import { FactionResponse } from '@/types/spacetraders'
+
+export const getFactionsList = queryFnFactory<SpaceTradersResponse<FactionResponse[], Meta>>(() => 'factions')
+
+export const getFactionById = queryFnFactory<SpaceTradersResponse<FactionResponse>, string>(
+  (faction) => `factions/${faction}`,
+)
