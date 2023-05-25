1122bdb31ebdc6a6d8e6b24d326efa28625335a2
Ben Weier <ben.weier@gmail.com>
2023-05-25 23:06:25 +1000

    update systems feature import

--- src/routes/systems/systems.route.tsx
+++ src/routes/systems/systems.route.tsx
@@ -1,13 +1,13 @@
 import { QuerySuspenseBoundary, withQSB } from '@/components/query-suspense-boundary'
-import { Fallback, List } from '@/features/systems'
+import * as Systems from '@/features/systems'
 
 const SystemListComponent = () => {
   return (
     <div className="grid gap-4 p-4">
       <h1 className="text-title">Systems</h1>
       <div className="grid gap-12">
-        <QuerySuspenseBoundary fallback={<Fallback />}>
-          <List />
+        <QuerySuspenseBoundary fallback={<Systems.Fallback />}>
+          <Systems.List />
         </QuerySuspenseBoundary>
       </div>
     </div>
