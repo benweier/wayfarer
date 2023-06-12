fba78a7589208e89781036df69da59ea40a0f592
Ben Weier <ben.weier@gmail.com>
2023-06-12 22:31:23 +1000

    update waypoint preview colour map

--- src/features/system/list/system-list.component.tsx
+++ src/features/system/list/system-list.component.tsx
@@ -9,15 +9,15 @@ import { SystemItem } from '../item'
 import { SystemListProps } from './system-list.types'
 
 const WAYPOINT_TYPE_STYLES: Record<string, string> = {
-  MOON: 'bg-slate-500 text-slate-200',
+  MOON: 'bg-slate-500 text-slate-50',
   GAS_GIANT: 'bg-orange-600 text-orange-100',
-  NEBULA: 'bg-yellow-300 text-yellow-900',
-  ASTEROID_FIELD: 'bg-lime-300 text-lime-900',
+  NEBULA: 'bg-yellow-300 text-yellow-950',
+  ASTEROID_FIELD: 'bg-lime-300 text-lime-950',
   PLANET: 'bg-emerald-600 text-emerald-50',
-  DEBRIS_FIELD: 'bg-cyan-300 text-cyan-900',
-  ORBITAL_STATION: 'bg-fuchsia-600 text-fuchsia-100',
-  JUMP_GATE: 'bg-zinc-100 text-zinc-900',
-  GRAVITY_WELL: 'bg-zinc-900 text-zinc-50',
+  DEBRIS_FIELD: 'bg-cyan-300 text-cyan-950',
+  ORBITAL_STATION: 'bg-fuchsia-600 text-fuchsia-50',
+  JUMP_GATE: 'bg-zinc-100 text-zinc-950',
+  GRAVITY_WELL: 'bg-gray-950 text-gray-50',
 }
 
 export const SystemList = ({ System = SystemItem }: SystemListProps) => {
