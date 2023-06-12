41f4716e6ae5890ae3372b0405dc0cb3ae2a73dd
Ben Weier <ben.weier@gmail.com>
2023-06-12 22:34:40 +1000

    tweak blue star background colour

--- src/components/system/tag/system-tag.component.tsx
+++ src/components/system/tag/system-tag.component.tsx
@@ -5,7 +5,7 @@ const SYSTEM_TYPE_STYLES: Record<string, string> = {
   NEUTRON_STAR: 'bg-slate-600 text-slate-50',
   RED_STAR: 'bg-red-700 text-red-50',
   ORANGE_STAR: 'bg-orange-300 text-orange-950',
-  BLUE_STAR: 'bg-cyan-300 text-cyan-950',
+  BLUE_STAR: 'bg-cyan-400 text-cyan-950',
   YOUNG_STAR: 'bg-emerald-300 text-emerald-950',
   WHITE_DWARF: 'bg-gray-100 text-gray-950',
   BLACK_HOLE: 'bg-gray-950 text-gray-50',
