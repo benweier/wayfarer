204411729974aa26478f851ca843235af87cafb0
Ben Weier <ben.weier@gmail.com>
2023-06-12 22:33:45 +1000

    add gap to waypoint item text

--- src/features/waypoint/item/waypoint-item.component.tsx
+++ src/features/waypoint/item/waypoint-item.component.tsx
@@ -6,7 +6,7 @@ import { WaypointItemProps } from './waypoint-item.types'
 
 export const WaypointItem = ({ systemID, waypoint }: WaypointItemProps) => {
   return (
-    <div className="flex flex-col justify-between rounded bg-zinc-200/50 p-4 shadow-sm dark:bg-zinc-700/25">
+    <div className="flex flex-col justify-between gap-1 rounded bg-zinc-200/50 p-4 shadow-sm dark:bg-zinc-700/25">
       <div>
         <Link className="link text-lg" to={`${ROUTES.SYSTEMS}/${systemID}/waypoint/${waypoint.symbol}`}>
           {waypoint.symbol}
