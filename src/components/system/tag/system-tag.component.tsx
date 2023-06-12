d14c92ccce9ea3f2bbf747f018cc69b8bf7b805f
Ben Weier <ben.weier@gmail.com>
2023-06-12 22:26:33 +1000

    tweak system and waypoint tag colours

--- src/components/system/tag/system-tag.component.tsx
+++ src/components/system/tag/system-tag.component.tsx
@@ -2,21 +2,26 @@ import { cx } from '@/utilities/cx'
 import { SystemTagProps } from './system-tag.types'
 
 const SYSTEM_TYPE_STYLES: Record<string, string> = {
-  NEUTRON_STAR: 'bg-slate-500 text-slate-200',
-  RED_STAR: 'bg-red-800 text-red-50',
-  ORANGE_STAR: 'bg-orange-200 text-orange-900',
-  BLUE_STAR: 'bg-sky-300 text-sky-950',
+  NEUTRON_STAR: 'bg-slate-600 text-slate-50',
+  RED_STAR: 'bg-red-700 text-red-50',
+  ORANGE_STAR: 'bg-orange-300 text-orange-950',
+  BLUE_STAR: 'bg-cyan-300 text-cyan-950',
   YOUNG_STAR: 'bg-emerald-300 text-emerald-950',
-  WHITE_DWARF: 'bg-gray-100 text-gray-900',
-  BLACK_HOLE: 'bg-gray-950 text-gray-100',
+  WHITE_DWARF: 'bg-gray-100 text-gray-950',
+  BLACK_HOLE: 'bg-gray-950 text-gray-50',
   HYPERGIANT: 'bg-violet-300 text-violet-950',
   NEBULA: 'bg-pink-600 text-pink-50',
-  UNSTABLE: 'bg-indigo-900 text-indigo-50',
+  UNSTABLE: 'bg-indigo-600 text-indigo-50',
 }
 
 export const SystemTag = ({ type, children }: SystemTagProps) => {
   return (
-    <div className={cx('bg- rounded-sm px-2 py-1 text-xs font-bold uppercase leading-none', SYSTEM_TYPE_STYLES[type])}>
+    <div
+      className={cx(
+        'bg- rounded-sm px-2 py-1 text-xs font-bold uppercase leading-none tracking-wide',
+        SYSTEM_TYPE_STYLES[type],
+      )}
+    >
       {children}
     </div>
   )
