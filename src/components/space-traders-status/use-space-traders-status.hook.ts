7982a372bb8b6515b4debee5df46f6a296be1f3c
Ben Weier <ben.weier@gmail.com>
2023-06-27 21:23:00 +1000

    fix status endpoint check

--- src/components/space-traders-status/use-space-traders-status.hook.ts
+++ src/components/space-traders-status/use-space-traders-status.hook.ts
@@ -1,13 +1,31 @@
 import { useQuery } from '@tanstack/react-query'
 import { get } from '@/services/fetch'
 
-const getSpacetradersStatus = () => {
-  const url = new URL('/game/status', 'https://api.spacetraders.io')
-  return get<{ status: string }>(url)
+type SpaceTradersStatusResponse = {
+  status: string
+  version: string
+  resetDate: string
+  description: string
+  stats: { agents: number; ships: number; systems: number; waypoints: number }
+  leaderboards: {
+    mostCredits: Array<{ agentSymbol: string; credits: number }>
+    mostSubmittedCharts: Array<{ agentSymbol: string; chartCount: number }>
+  }
+  serverResets: { next: string; frequency: 'weekly' }
+  announcements: Array<{
+    title: string
+    body: string
+  }>
+  links: Array<{ name: string; url: string }>
 }
 
-const select = (response: { status: string }) => ({
-  status: response.status === 'spacetraders is currently online and available to play',
+const getSpacetradersStatus = () => {
+  const url = new URL(import.meta.env.SPACETRADERS_API_BASE_URL)
+  return get<SpaceTradersStatusResponse>(url)
+}
+
+const select = (response: SpaceTradersStatusResponse) => ({
+  status: response.status === 'SpaceTraders is currently online and available to play',
 })
 
 export const useSpaceTradersStatus = (): { status: 'UNKNOWN' | 'ONLINE' | 'OFFLINE'; isChecking: boolean } => {
