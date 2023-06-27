0446b5561555537de6e84b3a517329db18a1c3c2
Ben Weier <ben.weier@gmail.com>
2023-06-27 23:17:02 +1000

    add status response type

--- src/types/spacetraders.d.ts
+++ src/types/spacetraders.d.ts
@@ -1,3 +1,21 @@
+type StatusResponse = {
+  status: string
+  version: string
+  resetDate: string
+  description: string
+  stats: { agents: number; ships: number; systems: number; waypoints: number }
+  leaderboards: {
+    mostCredits: Array<{ agentSymbol: string; credits: number }>
+    mostSubmittedCharts: Array<{ agentSymbol: string; chartCount: number }>
+  }
+  serverResets: { next: string; frequency: string }
+  announcements: Array<{
+    title: string
+    body: string
+  }>
+  links: Array<{ name: string; url: string }>
+}
+
 export type RegisterAgentRequest = {
   symbol: string
   faction: string
