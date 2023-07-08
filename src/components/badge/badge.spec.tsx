527746ab1e892a8427749699d88d23fb00e1327c
Ben Weier <ben.weier@gmail.com>
2023-07-08 15:56:07 +1000

    add badge component spec

---
+++ src/components/badge/badge.spec.tsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react'
+import { Badge } from './badge.component'
+
+describe('badge', () => {
+  it('should render', () => {
+    const { container } = render(<Badge>Content</Badge>)
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <span
+          class="text-primary text-inverse my-0.5 rounded-sm bg-zinc-700 px-2.5 text-xs font-bold dark:bg-zinc-300"
+        >
+          Content
+        </span>
+      </div>
+    `)
+  })
+})
