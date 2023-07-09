09cfeced31a617700fc6226533dc4caab265a3a3
Ben Weier <ben.weier@gmail.com>
2023-07-09 18:04:14 +1000

    clean up icon usage

--- src/features/auth/auth.layout.tsx
+++ src/features/auth/auth.layout.tsx
@@ -1,16 +1,13 @@
-import { IconContext } from 'react-icons'
-import { GiNorthStarShuriken } from 'react-icons/gi'
-import { HiOutlineStatusOffline, HiOutlineStatusOnline, HiRefresh } from 'react-icons/hi'
-import { VscGithub, VscRocket } from 'react-icons/vsc'
+import { AppIcon } from '@/components/icons'
 import { QuerySuspenseBoundary } from '@/components/query-suspense-boundary'
 import { useSpaceTradersStatus } from '@/components/space-traders-status'
 import { Wayfarer } from '@/components/wayfarer'
 import { cx } from '@/utilities/cx'
 
 const icon = {
-  UNKNOWN: <HiRefresh size={20} />,
-  ONLINE: <HiOutlineStatusOnline size={20} />,
-  OFFLINE: <HiOutlineStatusOffline size={20} />,
+  UNKNOWN: 'connection:unknown',
+  ONLINE: 'connection:online',
+  OFFLINE: 'connection:offline',
 }
 
 const SpaceTradersStatus = () => {
@@ -26,7 +23,7 @@ const SpaceTradersStatus = () => {
           'text-yellow-400': isChecking,
         })}
       >
-        {icon[status]}
+        <AppIcon id={icon[status]} className="h-6 w-6" aria-hidden />
       </div>
       <span className="text-sm font-semibold">{status}</span>
     </>
@@ -36,45 +33,35 @@ const SpaceTradersStatus = () => {
 export const Layout = ({ children }: WithChildren) => {
   return (
     <div className="grid min-h-screen w-full auto-rows-min items-center gap-6 [grid-template-rows:auto_1fr_auto]">
-      <div className="grid grid-flow-row items-center justify-center py-8">
-        <div className="place-self-center text-zinc-500">
-          <GiNorthStarShuriken size={64} />
-        </div>
+      <div className="grid grid-flow-row items-center justify-center py-12">
         <Wayfarer className="text-center text-6xl font-black lg:text-7xl" />
         <div className="text-center text-xl font-semibold text-zinc-500">A SpaceTraders API Interface</div>
         <div className="grid grid-flow-col items-center justify-center gap-2 py-4">
-          <QuerySuspenseBoundary>
+          <QuerySuspenseBoundary
+            fallback={<AppIcon id="connection:unknown" className="h-6 w-6 animate-spin text-yellow-400" aria-hidden />}
+            error={() => <AppIcon id="connection:offline" className="h-6 w-6 text-rose-400" aria-hidden />}
+          >
             <SpaceTradersStatus />
           </QuerySuspenseBoundary>
         </div>
       </div>
 
       <div>
-        <div className="bg-zinc-200/40 dark:bg-zinc-700/20">
+        <div className="bg-zinc-200/40 dark:bg-zinc-800/40">
           <div className="mx-auto grid w-full max-w-lg items-center">
             <div className="p-8">{children}</div>
           </div>
         </div>
       </div>
 
-      <div className="grid grid-flow-col gap-8 py-8">
+      <div className="grid grid-flow-col gap-8 py-12">
         <div className="grid grid-flow-col items-center justify-center gap-8 text-zinc-500">
-          <IconContext.Provider value={{ size: '32' }}>
-            <a
-              href="https://spacetraders.io"
-              title="SpaceTraders"
-              className="rounded-full p-1 outline-none ring-emerald-400 ring-offset-2 focus:ring-2 focus:ring-offset-gray-800"
-            >
-              <VscRocket />
-            </a>
-            <a
-              href="https://github.com/benweier/wayfarer"
-              title="GitHub"
-              className="rounded-full p-1 outline-none ring-emerald-400 ring-offset-2 focus:ring-2 focus:ring-offset-gray-800"
-            >
-              <VscGithub />
-            </a>
-          </IconContext.Provider>
+          <a href="https://spacetraders.io" title="SpaceTraders" className="rounded-full p-2">
+            <AppIcon id="rocket" className="h-8 w-8" />
+          </a>
+          <a href="https://github.com/benweier/wayfarer" title="GitHub" className="rounded-full p-2">
+            <AppIcon id="github" className="h-8 w-8" />
+          </a>
         </div>
       </div>
     </div>
