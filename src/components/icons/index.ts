00281025d15b6b88082987e67d05cf33ea596d2a
Ben Weier <ben.weier@gmail.com>
2023-07-09 13:40:13 +1000

    create menu icon component

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,1 +1,2 @@
+export { MenuIcon } from './menu-icon.component'
 export { ShipIcon } from './ship-icon.component'
---
+++ src/components/icons/menu-icon.component.tsx
@@ -0,0 +1,6 @@
+import { SVGProps } from 'react'
+import { Icon } from './icon-base.component'
+
+export const MenuIcon = (props: SVGProps<SVGSVGElement>) => {
+  return <Icon {...props} href="/icons/menu.svg" />
+}
