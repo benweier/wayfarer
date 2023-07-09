38e3285b9d345e0c810910af7a54dc80f022a148
Ben Weier <ben.weier@gmail.com>
2023-07-09 12:00:01 +1000

    wrap link in div

--- src/features/waypoint/jumpgate/waypoint-jumpgate-item.component.tsx
+++ src/features/waypoint/jumpgate/waypoint-jumpgate-item.component.tsx
@@ -4,9 +4,11 @@ import { JumpGateConnectedSystem } from '@/types/spacetraders'
 export const WaypointJumpGateItem = ({ system }: { system: JumpGateConnectedSystem }) => {
   return (
     <div key={system.symbol} className="grid gap-2 rounded bg-zinc-500 bg-opacity-5 px-4 py-3 dark:bg-opacity-10">
-      <Link key={system.symbol} className="link font-semibold" to={`/systems/${system.symbol}`}>
-        {system.symbol}
-      </Link>
+      <div>
+        <Link key={system.symbol} className="link font-semibold" to={`/systems/${system.symbol}`}>
+          {system.symbol}
+        </Link>
+      </div>
       <div className="flex items-center justify-between gap-4">
         <div className="text-secondary text-sm">Distance: {system.distance}</div>
         <div className="text-secondary text-sm">
