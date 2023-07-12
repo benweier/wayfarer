37f806a5d8cb58e0bcf565c7ed8b6a039ba6e3b9
Ben Weier <ben.weier@gmail.com>
2023-07-12 22:55:48 +1000

    add the curly bois

--- src/features/ship/list/ship-list.component.tsx
+++ src/features/ship/list/ship-list.component.tsx
@@ -12,12 +12,13 @@ export const ShipList = () => {
 
   const ships = data.data
 
-  if (ships.length === 0)
+  if (ships.length === 0) {
     return (
       <div className="rounded border-2 border-dashed border-zinc-300 px-3 py-9 dark:border-zinc-600">
         <div className="text-secondary text-center text-sm">You have don&apos;t own any ships!</div>
       </div>
     )
+  }
 
   return (
     <div className="grid grid-cols-1 gap-2">
