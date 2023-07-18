53ecbcb3034a9b973a4e52da3704c4d23b58b34f
Ben Weier <ben.weier@gmail.com>
2023-07-18 23:18:35 +1000

    remove market display mode, clean up layout classes

--- src/features/waypoint/market/waypoint-market.layout.tsx
+++ src/features/waypoint/market/waypoint-market.layout.tsx
@@ -1,7 +1,4 @@
-import { useAtom } from 'jotai'
 import { ReactNode } from 'react'
-import { marketDisplayAtom } from '@/store/atoms/market.display'
-import { cx } from '@/utilities/cx'
 
 export const WaypointMarketLayout = ({
   imports,
@@ -12,26 +9,19 @@ export const WaypointMarketLayout = ({
   exports: ReactNode
   exchange: ReactNode
 }) => {
-  const [displayMode] = useAtom(marketDisplayAtom)
-
   return (
-    <div
-      className={cx('grid grid-cols-1', {
-        'gap-12 lg:grid-cols-1': displayMode === 'list',
-        'gap-4 lg:grid-cols-3': displayMode === 'grid',
-      })}
-    >
+    <div className="space-y-12">
       <div className="flex flex-col gap-4">
         <div className="text-headline text-left">Imports</div>
-        <div className="flex flex-col gap-2">{imports}</div>
+        <div className="grid grid-cols-1 gap-2">{imports}</div>
       </div>
       <div className="flex flex-col gap-4">
         <div className="text-headline text-left">Exports</div>
-        <div className="flex flex-col gap-2">{exports}</div>
+        <div className="grid grid-cols-1 gap-2">{exports}</div>
       </div>
       <div className="flex flex-col gap-4">
         <div className="text-headline text-left">Exchange</div>
-        <div className="flex flex-col gap-2">{exchange}</div>
+        <div className="grid grid-cols-1 gap-2">{exchange}</div>
       </div>
     </div>
   )
