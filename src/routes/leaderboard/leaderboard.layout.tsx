b9c5eb98934220a7f38d596b66613eb29070f5c0
Ben Weier <ben.weier@gmail.com>
2023-07-28 22:28:47 +1000

    update leaderboard layout to match auth layout

--- src/routes/leaderboard/leaderboard.layout.tsx
+++ src/routes/leaderboard/leaderboard.layout.tsx
@@ -1,16 +1,11 @@
-import { IconContext } from 'react-icons'
-import { GiNorthStarShuriken } from 'react-icons/gi'
-import { VscGithub, VscRocket } from 'react-icons/vsc'
 import { Outlet } from 'react-router-dom'
+import { AppIcon } from '@/components/icons'
 import { Wayfarer } from '@/components/wayfarer'
 
 export const Layout = ({ children = <Outlet /> }: WithChildren) => {
   return (
     <div className="grid min-h-screen w-full items-start gap-6 [grid-template-rows:auto_1fr_auto]">
-      <div className="grid grid-flow-row items-start justify-center py-8">
-        <div className="place-self-center text-zinc-500">
-          <GiNorthStarShuriken size={64} />
-        </div>
+      <div className="grid grid-flow-row items-center justify-center py-12">
         <Wayfarer className="text-center text-6xl font-black lg:text-7xl" />
         <div className="text-center text-xl font-semibold text-zinc-500">A SpaceTraders API Interface</div>
       </div>
@@ -23,24 +18,14 @@ export const Layout = ({ children = <Outlet /> }: WithChildren) => {
         </div>
       </div>
 
-      <div className="grid grid-flow-col gap-8 py-8">
-        <div className="grid grid-flow-col items-start justify-center gap-8 text-zinc-500">
-          <IconContext.Provider value={{ size: '32' }}>
-            <a
-              href="https://spacetraders.io"
-              title="SpaceTraders"
-              className="rounded-full p-1 outline-none ring-emerald-400 ring-offset-2 focus:ring-2 focus:ring-offset-gray-800"
-            >
-              <VscRocket />
-            </a>
-            <a
-              href="https://github.com/benweier/wayfarer"
-              title="GitHub"
-              className="rounded-full p-1 outline-none ring-emerald-400 ring-offset-2 focus:ring-2 focus:ring-offset-gray-800"
-            >
-              <VscGithub />
-            </a>
-          </IconContext.Provider>
+      <div className="grid grid-flow-col gap-8 py-12">
+        <div className="grid grid-flow-col items-center justify-center gap-8 text-zinc-500">
+          <a href="https://spacetraders.io" title="SpaceTraders" className="rounded-full p-2">
+            <AppIcon id="rocket" className="h-8 w-8" />
+          </a>
+          <a href="https://github.com/benweier/wayfarer" title="GitHub" className="rounded-full p-2">
+            <AppIcon id="github" className="h-8 w-8" />
+          </a>
         </div>
       </div>
     </div>
