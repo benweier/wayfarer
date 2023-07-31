291763c849a0ab04451ac12ab1e03bd6c766d59f
Ben Weier <ben.weier@gmail.com>
2023-07-31 20:27:16 +1000

    clean up types, use shared credentials const value

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -1,13 +1,14 @@
 import { http } from './http'
 
-export type QueryParams = Record<string, any> | null | undefined | unknown
-export type RequestPayload = Record<string, any> | null | undefined | unknown
+export type QueryParams = Record<string, any> | null | undefined
+export type RequestPayload = Record<string, any> | null | undefined
 
+const DEFAULT_CREDENTIALS = 'same-origin'
 export const get = <T>(url: URL | RequestInfo, args: RequestInit = {}) => {
-  return http<T>(url, { ...args, method: 'GET', credentials: 'same-origin' })
+  return http<T>(url, { ...args, method: 'GET', credentials: DEFAULT_CREDENTIALS })
 }
 
-export const post = <T = unknown, P extends RequestPayload = unknown>(
+export const post = <T = unknown, P extends RequestPayload = undefined>(
   url: URL | RequestInfo,
   payload?: P,
   args: RequestInit = {},
@@ -15,12 +16,12 @@ export const post = <T = unknown, P extends RequestPayload = unknown>(
   return http<T>(url, {
     ...args,
     method: 'POST',
-    credentials: 'same-origin',
+    credentials: DEFAULT_CREDENTIALS,
     body: payload ? JSON.stringify(payload) : undefined,
   })
 }
 
-export const patch = <T = unknown, P extends RequestPayload = unknown>(
+export const patch = <T = unknown, P extends RequestPayload = undefined>(
   url: URL | RequestInfo,
   payload?: P,
   args: RequestInit = {},
@@ -28,16 +29,16 @@ export const patch = <T = unknown, P extends RequestPayload = unknown>(
   return http<T>(url, {
     ...args,
     method: 'PATCH',
-    credentials: 'same-origin',
+    credentials: DEFAULT_CREDENTIALS,
     body: payload ? JSON.stringify(payload) : undefined,
   })
 }
 
 export const put = <T>(url: URL | RequestInfo, payload: BodyInit, args: RequestInit = {}) => {
-  return http<T>(url, { ...args, method: 'POST', credentials: 'same-origin', body: payload })
+  return http<T>(url, { ...args, method: 'POST', credentials: DEFAULT_CREDENTIALS, body: payload })
 }
 
-export const remove = <T = unknown, P extends RequestPayload = unknown>(
+export const remove = <T = unknown, P extends RequestPayload = undefined>(
   url: URL | RequestInfo,
   payload: P,
   args: RequestInit = {},
@@ -45,7 +46,7 @@ export const remove = <T = unknown, P extends RequestPayload = unknown>(
   return http<T>(url, {
     ...args,
     method: 'DELETE',
-    credentials: 'same-origin',
+    credentials: DEFAULT_CREDENTIALS,
     body: payload ? JSON.stringify(payload) : undefined,
   })
 }
