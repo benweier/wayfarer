406e963ff6a747b479e35c2435e26d718f0154e5
Ben Weier <ben.weier@gmail.com>
2023-08-08 23:05:42 +1000

    remove unnecessary postcss plugins

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,5 @@
 module.exports = {
   plugins: [
-    require('postcss-preset-env'),
-    require('postcss-import'),
     require('tailwind-apply/postcss'),
     require('tailwindcss/nesting'),
     require('tailwindcss'),
