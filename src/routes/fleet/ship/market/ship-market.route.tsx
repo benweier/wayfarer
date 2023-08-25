5bbac5abc527321fdf6753154aa9232147cb7065
Ben Weier <ben.weier@gmail.com>
2023-08-25 22:18:36 +1000

    show waypoint market list in ship market route

--- src/routes/fleet/ship/market/ship-market.route.tsx
+++ src/routes/fleet/ship/market/ship-market.route.tsx
@@ -1,10 +1,27 @@
-import { withQSB } from '@/components/query-suspense-boundary'
+import { QuerySuspenseBoundary, withQSB } from '@/components/query-suspense-boundary'
 import { useShipResponse } from '@/context/ship.context'
+import { WaypointStore } from '@/context/waypoint.context'
+import { WaypointMarketList } from '@/features/waypoint/market'
 
 const ShipMarketRouteComponent = () => {
   const ship = useShipResponse()
 
-  return <>{ship.symbol}</>
+  return (
+    <div className="grid gap-4 p-4">
+      <div className="flex items-center justify-start gap-6">
+        <h1 className="text-title">
+          Market: <span className="font-normal">{ship.nav.waypointSymbol}</span>
+        </h1>
+      </div>
+      <div className="grid gap-12">
+        <QuerySuspenseBoundary>
+          <WaypointStore systemSymbol={ship.nav.systemSymbol} waypointSymbol={ship.nav.waypointSymbol}>
+            <WaypointMarketList />
+          </WaypointStore>
+        </QuerySuspenseBoundary>
+      </div>
+    </div>
+  )
 }
 
 export const Route = withQSB()(ShipMarketRouteComponent)
