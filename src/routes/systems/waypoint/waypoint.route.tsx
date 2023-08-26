861436f28518a92850b06ca51e4f071a1843e53d
Ben Weier <ben.weier@gmail.com>
2023-08-26 11:01:35 +1000

    return null if missing required params

--- src/routes/systems/waypoint/waypoint.route.tsx
+++ src/routes/systems/waypoint/waypoint.route.tsx
@@ -7,21 +7,21 @@ import { WaypointTabs } from '@/features/waypoint/tabs'
 export const WaypointRouteComponent = () => {
   const { systemSymbol, waypointSymbol } = useParams()
 
+  if (!systemSymbol || !waypointSymbol) return null
+
   return (
     <div className="grid gap-4 p-4">
       <h1 className="text-title">
         Waypoint: <span className="whitespace-nowrap font-normal">{waypointSymbol}</span>
       </h1>
 
-      {systemSymbol && waypointSymbol && (
-        <QuerySuspenseBoundary>
-          <WaypointStore systemSymbol={systemSymbol} waypointSymbol={waypointSymbol}>
-            <WaypointDetail>
-              <WaypointTabs />
-            </WaypointDetail>
-          </WaypointStore>
-        </QuerySuspenseBoundary>
-      )}
+      <QuerySuspenseBoundary>
+        <WaypointStore systemSymbol={systemSymbol} waypointSymbol={waypointSymbol}>
+          <WaypointDetail>
+            <WaypointTabs />
+          </WaypointDetail>
+        </WaypointStore>
+      </QuerySuspenseBoundary>
     </div>
   )
 }
