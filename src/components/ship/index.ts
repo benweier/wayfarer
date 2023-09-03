d89eab026dde2ff1dbcc01e5224f3911e59a1873
Ben Weier <ben.weier@gmail.com>
2023-09-03 23:15:58 +1000

    remove loadout export

--- src/components/ship/index.ts
+++ src/components/ship/index.ts
@@ -1,4 +1,3 @@
 export { Cooldown } from './cooldown.component'
 export { Inventory } from './inventory.component'
-export { Loadout } from './loadout.component'
 export { Survey } from './survey.component'
