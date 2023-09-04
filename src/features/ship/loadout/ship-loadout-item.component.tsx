7576f8cca46a5ba460841fec66c6d3fa582ee3d4
Ben Weier <ben.weier@gmail.com>
2023-09-04 20:00:52 +1000

    refactor layout of ship loadout items

--- src/features/ship/loadout/ship-loadout-item.component.tsx
+++ src/features/ship/loadout/ship-loadout-item.component.tsx
@@ -2,7 +2,7 @@ import { type ShipLoadoutItemProps } from './ship-loadout.types'
 
 export const ShipLoadoutItem = ({ name, description }: ShipLoadoutItemProps) => {
   return (
-    <div className="grid gap-2 rounded bg-zinc-500 bg-opacity-5 px-4 py-3 dark:bg-opacity-10">
+    <div className="flex flex-col gap-2 rounded bg-zinc-500 bg-opacity-5 px-4 py-3 dark:bg-opacity-10">
       <span className="font-semibold">{name}</span>
       <span className="text-secondary text-sm">{description}</span>
     </div>
--- src/features/ship/loadout/ship-loadout-list.component.tsx
+++ src/features/ship/loadout/ship-loadout-list.component.tsx
@@ -6,31 +6,45 @@ export const ShipLoadoutList = ({ Item = ShipLoadoutItem }: ShipLoadoutListProps
   const ship = useShipResponse()
 
   return (
-    <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
-      <div className="flex flex-col gap-4">
-        <div className="text-overline text-center">Modules ({`${ship.modules.length}/${ship.frame.moduleSlots}`})</div>
-        <div className="flex flex-col gap-2">
-          {ship.modules.map((module, index) => (
-            <Item key={index} name={module.name} description={module.description} />
-          ))}
+    <div className="grid gap-12">
+      <div className="grid gap-8">
+        <div>
+          <div className="text-secondary text-sm uppercase">Frame</div>
+          <div className="text-overline">{ship.frame.name}</div>
+          <div className="">{ship.frame.description}</div>
         </div>
-      </div>
-      <div className="flex flex-col gap-4">
-        <div className="text-overline text-center">Frame</div>
-        <Item name={ship.frame.name} description={ship.frame.description} />
 
-        <div className="text-overline text-center">Reactor</div>
-        <Item name={ship.reactor.name} description={ship.reactor.description} />
+        <div className="grid grid-cols-2 gap-2">
+          <div className="flex flex-col gap-2">
+            <div className="text-secondary text-sm uppercase">Reactor</div>
+            <Item name={ship.reactor.name} description={ship.reactor.description} />
+          </div>
 
-        <div className="text-overline text-center">Engine</div>
-        <Item name={ship.engine.name} description={ship.engine.description} />
-      </div>
-      <div className="flex flex-col gap-4">
-        <div className="text-overline text-center">Mounts ({`${ship.mounts.length}/${ship.frame.mountingPoints}`})</div>
+          <div className="flex flex-col gap-2">
+            <div className="text-secondary text-sm uppercase">Engine</div>
+            <Item name={ship.engine.name} description={ship.engine.description} />
+          </div>
+        </div>
+
         <div className="flex flex-col gap-2">
-          {ship.mounts.map((mount, index) => (
-            <Item key={index} name={mount.name} description={mount.description} />
-          ))}
+          <div className="text-secondary text-sm uppercase">
+            Modules ({`${ship.modules.length}/${ship.frame.moduleSlots}`})
+          </div>
+          <div className="grid grid-cols-3 gap-2">
+            {ship.modules.map((module, index) => (
+              <Item key={index} name={module.name} description={module.description} />
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="text-secondary text-sm uppercase">
+            Mounts ({`${ship.mounts.length}/${ship.frame.mountingPoints}`})
+          </div>
+          <div className="grid grid-cols-3 gap-2">
+            {ship.mounts.map((mount, index) => (
+              <Item key={index} name={mount.name} description={mount.description} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
