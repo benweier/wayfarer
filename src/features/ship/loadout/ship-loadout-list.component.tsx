4feedd108ac3d8f4c0e340f2665a1aca84b0ead3
Ben Weier <ben.weier@gmail.com>
2023-09-08 23:53:05 +1000

    update loadout layout

--- src/features/ship/loadout/ship-loadout-list.component.tsx
+++ src/features/ship/loadout/ship-loadout-list.component.tsx
@@ -1,32 +1,53 @@
+import { useNavigate } from 'react-router-dom'
 import { useShipResponse } from '@/context/ship.context'
 import { ShipLoadoutItem } from './ship-loadout-item.component'
 import { type ShipLoadoutListProps } from './ship-loadout.types'
 
 export const ShipLoadoutList = ({ Item = ShipLoadoutItem }: ShipLoadoutListProps) => {
+  const navigate = useNavigate()
   const ship = useShipResponse()
 
   return (
-    <div className="grid gap-12">
-      <div className="grid gap-8">
-        <div>
-          <div className="text-secondary text-sm uppercase">Frame</div>
-          <div className="text-overline">{ship.frame.name}</div>
-          <div className="">{ship.frame.description}</div>
+    <div className="space-y-8">
+      <div className="grid gap-4 md:grid-cols-2">
+        <div className="space-y-6">
+          <div>
+            <div className="text-secondary text-sm uppercase">Frame</div>
+            <div className="text-overline">{ship.frame.name}</div>
+            <div className="w-4/5">{ship.frame.description}</div>
+          </div>
+
+          <ul className="text-secondary text-sm">
+            <li>Module Slots {ship.frame.moduleSlots}</li>
+            <li>Mounting Points {ship.frame.mountingPoints}</li>
+            <li>Power Required {ship.frame.requirements.power}</li>
+          </ul>
+
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              navigate(`/fleet/ship/${ship.symbol}/loadout`)
+            }}
+          >
+            Manage Loadout
+          </button>
         </div>
 
-        <div className="grid grid-cols-2 gap-2">
-          <div className="flex flex-col gap-2">
+        <div className="space-y-6">
+          <div className="space-y-2">
             <div className="text-secondary text-sm uppercase">Reactor</div>
             <Item name={ship.reactor.name} description={ship.reactor.description} />
           </div>
 
-          <div className="flex flex-col gap-2">
+          <div className="space-y-2">
             <div className="text-secondary text-sm uppercase">Engine</div>
             <Item name={ship.engine.name} description={ship.engine.description} />
           </div>
         </div>
+      </div>
 
-        <div className="flex flex-col gap-2">
+      {ship.frame.moduleSlots > 0 && (
+        <div className="space-y-2">
           <div className="text-secondary text-sm uppercase">
             Modules ({`${ship.modules.length}/${ship.frame.moduleSlots}`})
           </div>
@@ -36,7 +57,9 @@ export const ShipLoadoutList = ({ Item = ShipLoadoutItem }: ShipLoadoutListProps
             ))}
           </div>
         </div>
-        <div className="flex flex-col gap-2">
+      )}
+      {ship.frame.mountingPoints > 0 && (
+        <div className="space-y-2">
           <div className="text-secondary text-sm uppercase">
             Mounts ({`${ship.mounts.length}/${ship.frame.mountingPoints}`})
           </div>
@@ -46,7 +69,7 @@ export const ShipLoadoutList = ({ Item = ShipLoadoutItem }: ShipLoadoutListProps
             ))}
           </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
