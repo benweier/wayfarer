132ca08d58cba8cbb4a3bcce83ed18c000488132
Ben Weier <ben.weier@gmail.com>
2023-09-23 18:22:56 +1000

    increase query client gc time

--- src/services/query-client.ts
+++ src/services/query-client.ts
@@ -4,7 +4,7 @@ export const client = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 60_000,
-      gcTime: 10_000,
+      gcTime: 60_000,
       refetchOnWindowFocus: false,
       throwOnError: true,
       retry: false,
