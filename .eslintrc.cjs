7e9c1ee1fb6c31f9d0d6010795a0b7012bfc4777
Ben Weier <ben.weier@gmail.com>
2023-10-02 01:01:52 +1000

    re-enable tanstack query lint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -120,7 +120,7 @@ module.exports = {
       extends: [
         'plugin:react/recommended',
         'plugin:react-hooks/recommended',
-        // 'plugin:@tanstack/eslint-plugin-query/recommended',
+        'plugin:@tanstack/eslint-plugin-query/recommended',
       ],
       env: {
         browser: true,
