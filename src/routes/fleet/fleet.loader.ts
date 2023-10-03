1deaf0514e126297a42e30dffc58704d13fef788
Ben Weier <ben.weier@gmail.com>
2023-10-03 23:02:11 +1000

    add pagination to ship list

--- src/routes/fleet/fleet.loader.ts
+++ src/routes/fleet/fleet.loader.ts
@@ -4,28 +4,38 @@ import { getShipListQuery } from '@/services/api/spacetraders/fleet'
 import { STATUS_CODES, STATUS_MESSAGES, isHttpError } from '@/services/http'
 import { getState } from '@/store/auth'
 
-export const loader: QueryClientLoaderFn = (client) => async () => {
-  const { isAuthenticated } = getState()
+const getPageNumber = (page: string | null) => {
+  const pageNumber = parseInt(page ?? '1')
 
-  if (!isAuthenticated) {
-    redirect(ROUTES.LOGIN)
-    throw new Response(STATUS_MESSAGES.UNAUTHORIZED, { status: STATUS_CODES.UNAUTHORIZED })
-  }
+  return Number.isNaN(pageNumber) ? 1 : Math.max(1, pageNumber)
+}
 
-  try {
-    const ships = client.ensureQueryData({
-      queryKey: getShipListQuery.getQueryKey(),
-      queryFn: getShipListQuery.queryFn,
-    })
+export const loader: QueryClientLoaderFn =
+  (client) =>
+  async ({ request }) => {
+    const { isAuthenticated } = getState()
+    const url = new URL(request.url)
+    const page = getPageNumber(url.searchParams.get('page'))
+    const limit = 20
 
-    return defer({
-      ships: await ships,
-    })
-  } catch (err) {
-    if (isHttpError(err, STATUS_CODES.NOT_FOUND)) {
-      throw new Response(STATUS_MESSAGES.NOT_FOUND, { status: STATUS_CODES.NOT_FOUND })
+    if (!isAuthenticated) {
+      return redirect(ROUTES.LOGIN)
     }
 
-    throw new Response(STATUS_MESSAGES.INTERNAL_SERVER_ERROR, { status: STATUS_CODES.INTERNAL_SERVER_ERROR })
+    try {
+      const ships = client.ensureQueryData({
+        queryKey: getShipListQuery.getQueryKey({ page, limit }),
+        queryFn: getShipListQuery.queryFn,
+      })
+
+      return defer({
+        ships: await ships,
+      })
+    } catch (err) {
+      if (isHttpError(err, STATUS_CODES.NOT_FOUND)) {
+        throw new Response(STATUS_MESSAGES.NOT_FOUND, { status: STATUS_CODES.NOT_FOUND })
+      }
+
+      throw new Response(STATUS_MESSAGES.INTERNAL_SERVER_ERROR, { status: STATUS_CODES.INTERNAL_SERVER_ERROR })
+    }
   }
-}
