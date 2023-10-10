b59a6b7c33846b3e4b2d31492e191f9bc97386ff
Ben Weier <ben.weier@gmail.com>
2023-10-10 21:53:30 +1000

    use redirect schema to check destination path

--- src/routes/auth/auth.route.tsx
+++ src/routes/auth/auth.route.tsx
@@ -4,17 +4,19 @@ import { ROUTES } from '@/config/routes'
 import { Layout } from '@/features/auth'
 import { useLocation } from '@/hooks/use-location.hook'
 import { useAuthStore } from '@/store/auth'
+import { redirectSchema } from '@/validators/redirect.schema'
 
 type Redirect = { destination: string }
 
 export const Route = () => {
   const { isAuthenticated } = useAuthStore()
-  const location = useLocation<{ redirect: Redirect }>()
+  const loc = useLocation<{ redirect: Redirect }>()
 
   if (isAuthenticated) {
-    const to = location.state?.redirect?.destination ?? ROUTES.OVERVIEW
+    const result = redirectSchema.safeParse(loc.state)
+    const to = result.success ? result.data.redirect.destination : ROUTES.OVERVIEW
 
-    return <Navigate to={to} replace />
+    return <Navigate to={to} replace state={null} />
   }
 
   return (
--- src/routes/auth/required.route.tsx
+++ src/routes/auth/required.route.tsx
@@ -1,13 +1,16 @@
 import { type PropsWithChildren } from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-import { ROUTES } from '@/config/routes'
+import { Navigate, Outlet, useLoaderData } from 'react-router-dom'
 import { useAuthStore } from '@/store/auth'
+import { redirectSchema } from '@/validators/redirect.schema'
 
 export const Required = ({ children = <Outlet /> }: PropsWithChildren) => {
   const { isAuthenticated } = useAuthStore()
+  const data = useLoaderData()
 
   if (!isAuthenticated) {
-    return <Navigate to={ROUTES.LOGIN} replace state={{ redirect: { destination: window.location.pathname } }} />
+    const result = redirectSchema.safeParse(data)
+
+    return <Navigate to="/login" replace state={result.success ? result.data : null} />
   }
 
   return <>{children}</>
