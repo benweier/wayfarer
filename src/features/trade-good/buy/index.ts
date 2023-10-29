7aa988a91a094b42352de81d8e406fbf1ac037ce
Ben Weier <ben.weier@gmail.com>
2023-10-29 19:02:50 +1000

    export trade good buy and sell component props

--- src/features/trade-good/buy/index.ts
+++ src/features/trade-good/buy/index.ts
@@ -1,1 +1,2 @@
 export { TradeGoodBuy } from './trade-good-buy.component'
+export type { TradeGoodBuyProps } from './trade-good-buy.types'
--- src/features/trade-good/sell/index.ts
+++ src/features/trade-good/sell/index.ts
@@ -1,1 +1,2 @@
 export { TradeGoodSell } from './trade-good-sell.component'
+export type { TradeGoodSellProps } from './trade-good-sell.types'
