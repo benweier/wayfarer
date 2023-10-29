cf8d374dd2e4148b9eb6838eeb2c15198b0f0b7c
Ben Weier <ben.weier@gmail.com>
2023-10-29 14:47:16 +1000

    update waypoint tag colours for all asteroid types

--- src/components/waypoint/tag/waypoint-tag.component.tsx
+++ src/components/waypoint/tag/waypoint-tag.component.tsx
@@ -1,11 +1,16 @@
 import { cx } from 'class-variance-authority'
 import { type WaypointTagProps } from './waypoint-tag.types'
 
+const ASTEROID_CLASSES = 'bg-stone-200 dark:bg-stone-600 dark:text-stone-100 text-stone-900'
 const WAYPOINT_TYPE_STYLES: Record<string, string> = {
   MOON: 'bg-slate-500 text-slate-50',
   GAS_GIANT: 'bg-orange-700 text-orange-50',
   NEBULA: 'bg-amber-400 text-amber-950',
-  ASTEROID: 'bg-lime-300 text-lime-950',
+  ASTEROID_FIELD: ASTEROID_CLASSES,
+  ASTEROID_BASE: ASTEROID_CLASSES,
+  ASTEROID: ASTEROID_CLASSES,
+  ENGINEERED_ASTEROID: ASTEROID_CLASSES,
+  FUEL_STATION: 'bg-lime-300 text-lime-950',
   PLANET: 'bg-emerald-600 text-emerald-50',
   DEBRIS_FIELD: 'bg-teal-300 text-teal-950',
   ORBITAL_STATION: 'bg-fuchsia-600 text-fuchsia-50',
