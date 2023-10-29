82d2984629fb7713e77da6eda17d051cca18a98d
Ben Weier <ben.weier@gmail.com>
2023-10-29 13:36:53 +1000

    remove sorting and item description preferences

--- src/features/waypoint/market/waypoint-market-preferences.component.tsx
+++ src/features/waypoint/market/waypoint-market-preferences.component.tsx
@@ -1,47 +1,10 @@
-import { RadioGroup, Switch } from '@headlessui/react'
 import { useIsFetching, useQueryClient } from '@tanstack/react-query'
-import { cx } from 'class-variance-authority'
-import { useAtom } from 'jotai'
 import { startTransition, useEffect, useState } from 'react'
-import { useSearchParams } from 'react-router-dom'
 import { Button } from '@/components/button'
 import { useWaypointResponse } from '@/context/waypoint.context'
 import { getWaypointMarketQuery } from '@/services/api/spacetraders'
-import { marketDescriptionAtom } from '@/store/atoms/market.display'
 import { relativeTime } from '@/utilities/date'
 
-const WaypointMarketSortBy = () => {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const value = searchParams.get('sort') ?? 'name'
-
-  return (
-    <RadioGroup
-      value={value}
-      onChange={(value) => {
-        setSearchParams({ sort: value }, { replace: true })
-      }}
-      className="flex flex-col gap-1"
-    >
-      <RadioGroup.Label className="text-xs font-bold">Sort By</RadioGroup.Label>
-      <div className="flex gap-2">
-        {[
-          { label: 'Item Name', value: 'name' },
-          { label: 'Buy Price', value: 'buy' },
-          { label: 'Sell Price', value: 'sell' },
-          { label: 'Trade Volume', value: 'volume' },
-        ].map((item) => (
-          <RadioGroup.Option
-            key={item.value}
-            value={item.value}
-            className={({ checked }) => cx('btn btn-sm', { 'btn-primary btn-outline': checked })}
-          >
-            <RadioGroup.Label>{item.label}</RadioGroup.Label>
-          </RadioGroup.Option>
-        ))}
-      </div>
-    </RadioGroup>
-  )
-}
 const WaypointMarketRefresh = () => {
   const [lastUpdate, forceUpdate] = useState(() => Date.now())
   const client = useQueryClient()
@@ -97,40 +60,9 @@ const WaypointMarketRefresh = () => {
 }
 
 export const WaypointMarketPreferences = () => {
-  const [showDescription, setShowDescription] = useAtom(marketDescriptionAtom)
-
   return (
-    <div className="flex items-end justify-between gap-4">
-      <WaypointMarketSortBy />
-
-      <div className="flex items-center justify-end gap-4">
-        <WaypointMarketRefresh />
-        <div className="flex items-center gap-2">
-          <span className="text-secondary text-sm">Show item description</span>
-          <Switch
-            checked={showDescription}
-            onChange={setShowDescription}
-            className={cx(
-              'relative inline-flex h-6 w-12 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 ',
-              {
-                'bg-emerald-500 dark:bg-emerald-600': showDescription,
-                'bg-zinc-700 dark:bg-zinc-900': !showDescription,
-              },
-            )}
-          >
-            <span
-              aria-hidden="true"
-              className={cx(
-                'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out',
-                {
-                  'translate-x-0': !showDescription,
-                  'translate-x-6': showDescription,
-                },
-              )}
-            />
-          </Switch>
-        </div>
-      </div>
+    <div className="flex justify-end">
+      <WaypointMarketRefresh />
     </div>
   )
 }
