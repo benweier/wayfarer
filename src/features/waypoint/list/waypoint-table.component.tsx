794968d4b22f83613090cedae54876537127155e
Ben Weier <ben.weier@gmail.com>
2023-11-11 17:35:38 +1000

    further improvement to waypoint table filters

--- src/features/waypoint/list/waypoint-table.component.tsx
+++ src/features/waypoint/list/waypoint-table.component.tsx
@@ -1,8 +1,7 @@
-import { Listbox } from '@headlessui/react'
+import * as Tooltip from '@radix-ui/react-tooltip'
 import {
   type ColumnFiltersState,
   type SortingState,
-  type VisibilityState,
   createColumnHelper,
   flexRender,
   getCoreRowModel,
@@ -12,7 +11,6 @@ import {
   getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table'
-import { cx } from 'class-variance-authority'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Badge } from '@/components/badge'
@@ -20,8 +18,8 @@ import { Button } from '@/components/button'
 import { AppIcon } from '@/components/icons'
 import { WaypointTag } from '@/components/waypoint/tag'
 import { WAYPOINT_TYPE } from '@/config/constants'
-import { type WaypointResponse, type WaypointTrait } from '@/types/spacetraders'
-import { FilterColumn } from './filter-column.component'
+import { type WaypointResponse } from '@/types/spacetraders'
+import { TypeFilter } from './type-filter.component'
 
 const FILTERED_TRAITS = new Set([
   'UNCHARTED',
@@ -64,28 +62,59 @@ const columns = [
         </Link>
       )
     },
+    sortingFn: 'alphanumeric',
     enableSorting: true,
     enableHiding: false,
   }),
   columnHelper.accessor((row) => `${row.waypoint.x}, ${row.waypoint.y}`, {
     id: 'coordinates',
-    header: () => <div className="text-left">Coordinates</div>,
+    header: ({ column }) => (
+      <div className="flex items-center justify-start gap-2 text-left">
+        <div>Coordinates</div>
+        <div>
+          <Button
+            intent={column.getIsSorted() === false ? 'dim' : 'primary'}
+            kind={column.getIsSorted() === false ? undefined : 'flat'}
+            size="small"
+            onClick={column.getToggleSortingHandler()}
+          >
+            <AppIcon id="arrow:up-down" className="h-4 w-4" />
+          </Button>
+        </div>
+
+        <Tooltip.Provider delayDuration={100}>
+          <Tooltip.Root>
+            <Tooltip.Trigger asChild>
+              <button>
+                <AppIcon id="help" className="text-secondary h-5 w-5" />
+              </button>
+            </Tooltip.Trigger>
+            <Tooltip.Portal>
+              <Tooltip.Content className="w-64 rounded-md bg-zinc-200 px-4 py-2 text-xs text-zinc-800" sideOffset={5}>
+                Sort coordinates by their distance from the system origin (0, 0).
+              </Tooltip.Content>
+            </Tooltip.Portal>
+          </Tooltip.Root>
+        </Tooltip.Provider>
+      </div>
+    ),
     cell: ({ getValue }) => {
       return <div className="text-secondary text-left text-sm">{getValue()}</div>
     },
-    enableSorting: false,
+    sortingFn: (a, b) => {
+      const [ax, ay] = [a.original.waypoint.x, a.original.waypoint.y]
+      const [bx, by] = [b.original.waypoint.x, b.original.waypoint.y]
+      const ad = ax * ax + ay * ay
+      const bd = bx * bx + by * by
+
+      return ad - bd
+    },
+    enableSorting: true,
     enableHiding: true,
   }),
   columnHelper.accessor((row) => row.waypoint.type, {
     id: 'type',
-    header: ({ column }) => {
-      const facets: string[] = Array.from(column.getFacetedUniqueValues().keys()).sort()
-      const selected = column.getFilterValue() as Array<{ id: string; value: string }> | undefined
-      const options = facets.map((value) => ({
-        id: value,
-        value: WAYPOINT_TYPE.get(value) ?? value,
-      }))
-
+    header: ({ column, table }) => {
       return (
         <div className="flex items-center justify-start gap-2 text-right">
           <div>Type</div>
@@ -100,13 +129,7 @@ const columns = [
             </Button>
           </div>
           <div>
-            <FilterColumn
-              selected={selected}
-              options={options}
-              onChange={(value) => {
-                column.setFilterValue(value)
-              }}
-            />
+            <TypeFilter table={table} />
           </div>
         </div>
       )
@@ -131,32 +154,23 @@ const columns = [
   }),
   columnHelper.accessor((row) => row.waypoint.traits, {
     id: 'traits',
-    header: ({ column }) => {
-      const facets: WaypointTrait[] = Array.from(column.getFacetedUniqueValues().keys()).flat()
-      const filterValues = column.getFilterValue() as WaypointTrait[] | undefined
-      const selected = filterValues?.map((value) => ({
-        id: value.symbol,
-        value: value.name,
-      }))
-      const options = facets
-        .filter((value, index, self) => index === self.findIndex((trait) => trait.symbol === value.symbol))
-        .map((value) => ({
-          id: value.symbol,
-          value: value.name,
-        }))
+    header: () => {
+      // const facets: WaypointTrait[] = Array.from(column.getFacetedUniqueValues().keys()).flat()
+      // const filterValues = column.getFilterValue() as WaypointTrait[] | undefined
+      // const selected = filterValues?.map((value) => ({
+      //   id: value.symbol,
+      //   value: value.name,
+      // }))
+      // const options = facets
+      //   .filter((value, index, self) => index === self.findIndex((trait) => trait.symbol === value.symbol))
+      //   .map((value) => ({
+      //     id: value.symbol,
+      //     value: value.name,
+      //   }))
 
       return (
         <div className="flex items-center justify-end gap-2 text-right">
           <div>Traits</div>
-          <div>
-            <FilterColumn
-              selected={selected}
-              options={options}
-              onChange={(value) => {
-                column.setFilterValue(value)
-              }}
-            />
-          </div>
         </div>
       )
     },
@@ -173,11 +187,11 @@ const columns = [
         </div>
       )
     },
-    filterFn: (row, _id, filterValue: Array<{ id: string; value: string }> = []) => {
+    filterFn: (row, _id, filterValue: Array<{ symbol: string; value: string }> = []) => {
       if (filterValue.length === 0) return true
 
       const matchedTraits = filterValue.filter((value) =>
-        row.original.waypoint.traits.some((trait) => trait.symbol === value.id),
+        row.original.waypoint.traits.some((trait) => trait.symbol === value.symbol),
       )
 
       return matchedTraits.length > 0
@@ -204,16 +218,9 @@ const columns = [
   //   enableHiding: true,
   // }),
 ]
-const COLUMN_FILTERS: Record<string, string> = {
-  coordinates: 'Coordinates',
-  type: 'Type',
-  traits: 'Traits',
-  // 'modifiers': 'Modifiers',
-}
 
 export const WaypointListTable = ({ data }: { data: Array<{ waypoint: WaypointResponse; presence?: boolean }> }) => {
   const [sorting, setSorting] = useState<SortingState>([{ id: 'symbol', desc: false }])
-  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
   const table = useReactTable({
     data,
@@ -224,121 +231,16 @@ export const WaypointListTable = ({ data }: { data: Array<{ waypoint: WaypointRe
     getFacetedRowModel: getFacetedRowModel(),
     getFacetedUniqueValues: getFacetedUniqueValues(),
     onSortingChange: setSorting,
-    onColumnVisibilityChange: setColumnVisibility,
     onColumnFiltersChange: setColumnFilters,
-    state: { sorting, columnVisibility, columnFilters },
+    state: { sorting, columnFilters },
     enableHiding: true,
     enableFilters: true,
     enableColumnFilters: true,
     enableGlobalFilter: true,
   })
-  const columnVisibilityOptions = table.getAllColumns().filter((column) => column.getCanHide())
-  const types = table.getColumn('type')
-  const facetedValues: Map<string, number> | undefined = types?.getFacetedUniqueValues()
-  const filterValues = (types?.getFilterValue() as string[] | undefined) ?? []
-  const filterOptions: string[] = facetedValues === undefined ? [] : Array.from(facetedValues.keys()).sort()
 
   return (
     <div className="max-w-screen space-y-4">
-      <div className="flex gap-4">
-        <Listbox
-          as="div"
-          className="relative"
-          value={filterValues}
-          onChange={(value) => {
-            table.getColumn('type')?.setFilterValue(value)
-          }}
-          multiple
-        >
-          <Listbox.Button className="select pr-8">
-            <>
-              <label className="block truncate">
-                Type {`(${filterValues.length === 0 ? 'All' : filterValues.length})`}
-              </label>
-              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                <AppIcon id="chevron:up-down" className="h-4 w-4 text-zinc-400" aria-hidden="true" />
-              </span>
-            </>
-          </Listbox.Button>
-
-          {filterOptions.length > 0 && (
-            <Listbox.Options className="absolute z-10 mt-1 max-h-48 w-auto overflow-auto rounded-md border-2 border-zinc-100 bg-white/90 text-sm outline-none backdrop-blur-md dark:border-zinc-800 dark:bg-zinc-900/90">
-              {filterOptions.map((option) => {
-                return (
-                  <Listbox.Option key={option} value={option}>
-                    {({ selected }) => {
-                      return (
-                        <div className="relative">
-                          <span className="absolute inset-y-0 left-0 flex items-center px-4">
-                            <AppIcon
-                              id="check"
-                              aria-hidden="true"
-                              className={cx('h-5 w-5', {
-                                'text-emerald-500': selected,
-                                'text-zinc-500': !selected,
-                              })}
-                            />
-                          </span>
-                          <span
-                            className={cx(
-                              'relative block cursor-default select-none truncate rounded py-2 pl-12 pr-4 transition-colors duration-100 ease-in-out',
-                            )}
-                          >
-                            {WAYPOINT_TYPE.get(option)} {`(${facetedValues?.get(option) ?? 0})`}
-                          </span>
-                        </div>
-                      )
-                    }}
-                  </Listbox.Option>
-                )
-              })}
-            </Listbox.Options>
-          )}
-        </Listbox>
-        <Listbox
-          as="div"
-          className="relative"
-          value={Object.keys(columnVisibility)}
-          onChange={(value) => {
-            table.setColumnVisibility(value.reduce((acc, key) => ({ ...acc, [key]: false }), {}))
-          }}
-          multiple
-        >
-          <Listbox.Button className="select pr-8">
-            <>
-              <label className="block truncate">Columns</label>
-              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                <AppIcon id="chevron:up-down" className="h-4 w-4 text-zinc-400" aria-hidden="true" />
-              </span>
-            </>
-          </Listbox.Button>
-
-          {columnVisibilityOptions.length > 0 && (
-            <Listbox.Options className="absolute z-10 mt-1 max-h-48 w-auto overflow-auto rounded-md border-2 border-zinc-100 bg-white/90 text-sm outline-none backdrop-blur-md dark:border-zinc-800 dark:bg-zinc-900/90">
-              {columnVisibilityOptions.map((column) => {
-                return (
-                  <Listbox.Option key={column.id} value={column.id}>
-                    <div className="relative p-1">
-                      {table.getColumn(column.id)?.getIsVisible() && (
-                        <span className="absolute inset-y-0 left-0 flex items-center px-4">
-                          <AppIcon id="check" aria-hidden="true" className="h-5 w-5 text-emerald-500" />
-                        </span>
-                      )}
-                      <span
-                        className={cx(
-                          'relative block cursor-default select-none truncate rounded py-2 pl-12 pr-4 transition-colors duration-100 ease-in-out',
-                        )}
-                      >
-                        {COLUMN_FILTERS[column.id]}
-                      </span>
-                    </div>
-                  </Listbox.Option>
-                )
-              })}
-            </Listbox.Options>
-          )}
-        </Listbox>
-      </div>
       <div className="overflow-hidden rounded-xl">
         <table className="min-w-full divide-y divide-zinc-200 dark:divide-zinc-950">
           <thead className="bg-zinc-100 dark:bg-zinc-800">
