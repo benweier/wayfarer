345a77f812fda06c94cf22a463901a65efb44558
Ben Weier <ben.weier@gmail.com>
2023-11-16 00:51:32 +1000

    fuel consumption is always at least 1

--- src/utilities/get-fuel-consumption.helper.ts
+++ src/utilities/get-fuel-consumption.helper.ts
@@ -1,13 +1,13 @@
 export const getFuelConsumption = (distance: number, mode: string): number => {
   switch (mode) {
     case 'CRUISE':
-      return Math.round(distance)
+      return Math.max(1, Math.round(distance))
     case 'DRIFT':
       return 1
     case 'BURN':
-      return 2 * Math.round(distance)
+      return Math.max(1, 2 * Math.round(distance))
     case 'STEALTH':
-      return Math.round(distance)
+      return Math.max(1, Math.round(distance))
     default:
       return distance
   }
