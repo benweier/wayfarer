a1af0aa1d38c028c37decf69e3299932bf0b363c
Ben Weier <ben.weier@gmail.com>
2023-11-20 17:13:26 +1000

    remove old layout components

--- src/layouts/Auth.tsx
+++
@@ -1,9 +0,0 @@
-import { type PropsWithChildren } from 'react'
-
-export const AuthLayout = ({ children }: PropsWithChildren) => {
-  return (
-    <div className="bg-zinc-200/40 dark:bg-zinc-700/20">
-      <div className="mx-auto grid w-full max-w-lg items-center">{children}</div>
-    </div>
-  )
-}
--- src/layouts/Main.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom'
-
-export const MainLayout = () => {
-  return (
-    <>
-      <Outlet />
-    </>
-  )
-}
