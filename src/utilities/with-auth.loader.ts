555a8d3cbea5d788edc43e1dea05819d4584c9b7
Ben Weier <ben.weier@gmail.com>
2023-11-20 17:18:19 +1000

    fix return behaviour to ensure loader is called when auth state changes

--- src/utilities/with-auth.loader.ts
+++ src/utilities/with-auth.loader.ts
@@ -4,14 +4,15 @@ import { getState } from '@/store/auth'
 export type WithAuthLoaderFunction = (loader: LoaderFunction) => LoaderFunction
 
 export const withAuth: WithAuthLoaderFunction = (loader) => {
-  const { isAuthenticated } = getState()
+  return (...args) => {
+    const { isAuthenticated } = getState()
 
-  if (!isAuthenticated) {
-    return () =>
-      json({
+    if (!isAuthenticated) {
+      return json({
         redirect: { destination: `${window.location.pathname}${window.location.search}` },
       })
-  }
+    }
 
-  return loader
+    return loader(...args)
+  }
 }
