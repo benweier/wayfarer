7066512608716628e6a0295bc807957ba70faa57
Ben Weier <ben.weier@gmail.com>
2023-11-22 19:00:48 +1000

    replace function config

--- vite.config.ts
+++ vite.config.ts
@@ -1,86 +1,71 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 import basicSSL from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react-swc'
-import { type PluginOption, defineConfig } from 'vite'
-import { warmup } from 'vite-plugin-warmup'
+import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig(({ mode }) => {
-  const plugins: PluginOption[] = []
-
-  if (mode === 'development') {
-    plugins.push(basicSSL())
-    plugins.push(warmup({ clientFiles: ['./index.html'] }))
-  }
-
-  plugins.push(tsconfigPaths())
-  plugins.push(react())
-  // plugins.push(
-  //   VitePWA({
-  //     registerType: 'prompt',
-  //     includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-  //     manifest: {
-  //       name: 'Wayfarer',
-  //       short_name: 'Wayfarer',
-  //       description: 'A SpaceTraders API interface',
-  //       theme_color: '#18181b',
-  //       icons: [
-  //         {
-  //           src: 'android-chrome-192x192.png',
-  //           sizes: '192x192',
-  //           type: 'image/png',
-  //         },
-  //         {
-  //           src: 'android-chrome-256x256.png',
-  //           sizes: '256x256',
-  //           type: 'image/png',
-  //         },
-  //       ],
-  //     },
-  //   }),
-  // )
-
-  if (mode === 'production') {
-    plugins.push(
-      sentryVitePlugin({
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-        org: process.env.SENTRY_ORG_NAME,
-        project: process.env.SENTRY_PROJECT_NAME,
-      }),
-    )
-  }
-
-  return {
-    build: {
-      sourcemap: true,
-      cssMinify: 'lightningcss',
-      modulePreload: { polyfill: false },
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            react: ['react', 'react-dom'],
-            core: ['react-router-dom', '@tanstack/react-query', '@sentry/react'],
-          },
+export default defineConfig({
+  build: {
+    sourcemap: true,
+    cssMinify: 'lightningcss',
+    modulePreload: { polyfill: false },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          core: ['react-router-dom', '@tanstack/react-query', '@sentry/react'],
         },
       },
     },
-    server: {
-      port: 8080,
-      open: true,
+  },
+  server: {
+    port: 8080,
+    open: true,
+  },
+  envPrefix: ['SENTRY_', 'SPACETRADERS_', 'REACT_'],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: './test/vitest.setup.ts',
+    clearMocks: true,
+    css: true,
+  },
+  css: {
+    modules: {
+      scopeBehaviour: 'local',
     },
-    envPrefix: ['SENTRY_', 'SPACETRADERS_', 'REACT_'],
-    test: {
-      globals: true,
-      environment: 'happy-dom',
-      setupFiles: './test/vitest.setup.ts',
-      clearMocks: true,
-      css: true,
-    },
-    css: {
-      modules: {
-        scopeBehaviour: 'local',
-      },
-    },
-    plugins,
-  }
+  },
+  plugins: [
+    basicSSL(),
+    tsconfigPaths(),
+    react(),
+    sentryVitePlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: process.env.SENTRY_ORG_NAME,
+      project: process.env.SENTRY_PROJECT_NAME,
+      silent: true,
+    }),
+    // VitePWA({
+    //   registerType: 'prompt',
+    //   includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+    //   manifest: {
+    //     name: 'Wayfarer',
+    //     short_name: 'Wayfarer',
+    //     description: 'A SpaceTraders API interface',
+    //     theme_color: '#18181b',
+    //     icons: [
+    //       {
+    //         src: 'android-chrome-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: 'android-chrome-256x256.png',
+    //         sizes: '256x256',
+    //         type: 'image/png',
+    //       },
+    //     ],
+    //   },
+    // }),
+  ],
 })
