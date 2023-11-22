ad062fb5be3ed58dd81a8acff759a650b6626302
Ben Weier <ben.weier@gmail.com>
2023-11-22 20:37:53 +1000

    pass t function to meta handle

--- src/components/meta/meta.component.tsx
+++ src/components/meta/meta.component.tsx
@@ -1,41 +1,41 @@
 import { Fragment } from 'react'
 import { Helmet } from 'react-helmet-async'
+import { useTranslation } from 'react-i18next'
 import { Await, useMatches } from 'react-router-dom'
 import { isFunction } from '@/utilities/is-function'
 import { hasRouteHandle } from '@/utilities/route-handle.helper'
-import { type MetaObject, type MetaProps } from './meta.types'
+import { type MetaProps } from './meta.types'
 
 export const Meta = ({ titleTemplate }: MetaProps) => {
+  const { t } = useTranslation()
   const matches = useMatches()
-  const helmet = matches
-    .filter(hasRouteHandle<(...args: unknown[]) => MetaObject[]>('meta', isFunction))
-    .map((match) => {
-      return (
-        <Fragment key={match.id}>
-          <Await resolve={match.data}>
-            {(data) => {
-              const metas = match.handle.meta(data)
+  const helmet = matches.filter(hasRouteHandle<MetaFunction>('meta', isFunction)).map((match) => {
+    return (
+      <Fragment key={match.id}>
+        <Await resolve={match.data}>
+          {(data) => {
+            const metas = match.handle.meta(t, data)
 
-              return (
-                <Helmet titleTemplate={titleTemplate}>
-                  {metas.map((meta) => {
-                    if ('title' in meta) {
-                      return <title key={meta.title}>{meta.title}</title>
-                    }
+            return (
+              <Helmet titleTemplate={titleTemplate}>
+                {metas.map((meta) => {
+                  if ('title' in meta) {
+                    return <title key={meta.title}>{meta.title}</title>
+                  }
 
-                    if ('name' in meta) {
-                      return <meta key={`${meta.name}|${meta.content}`} name={meta.name} content={meta.content} />
-                    }
+                  if ('name' in meta) {
+                    return <meta key={`${meta.name}|${meta.content}`} name={meta.name} content={meta.content} />
+                  }
 
-                    return <Fragment key={meta} />
-                  })}
-                </Helmet>
-              )
-            }}
-          </Await>
-        </Fragment>
-      )
-    })
+                  return <Fragment key={meta} />
+                })}
+              </Helmet>
+            )
+          }}
+        </Await>
+      </Fragment>
+    )
+  })
 
   return <>{helmet}</>
 }
--- src/components/meta/meta.types.ts
+++ src/components/meta/meta.types.ts
@@ -1,9 +1,3 @@
-export type MetaObject =
-  | {
-      title: string
-    }
-  | { name: string; content: string }
-
 export type MetaProps = {
   titleTemplate: string
 }
