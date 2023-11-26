a2a2aa0aa97189e22361bbfff0bfdfe0c6321678
Ben Weier <ben.weier@gmail.com>
2023-11-26 22:05:30 +1000

    handle authentication redirect in route error

--- src/components/route-error/route-error.component.tsx
+++ src/components/route-error/route-error.component.tsx
@@ -1,5 +1,6 @@
-import { useRouteError } from 'react-router-dom'
+import { Navigate, isRouteErrorResponse, useRouteError } from 'react-router-dom'
 import { STATUS_CODES, STATUS_MESSAGES } from '@/services/http'
+import { redirectSchema } from '@/validators/redirect.schema'
 
 const messages: Record<number, string> = {
   [STATUS_CODES.UNAUTHORIZED]: STATUS_MESSAGES.UNAUTHORIZED,
@@ -8,20 +9,23 @@ const messages: Record<number, string> = {
   [STATUS_CODES.UNPROCESSABLE_ENTITY]: STATUS_MESSAGES.UNPROCESSABLE_ENTITY,
   [STATUS_CODES.INTERNAL_SERVER_ERROR]: STATUS_MESSAGES.INTERNAL_SERVER_ERROR,
 }
-const isRouteErrorResponse = (error: any): error is Response => {
-  return error !== null && typeof error === 'object' && Object.hasOwn(error, 'status')
-}
 
 export const RouteError = () => {
   const error = useRouteError()
 
-  if (isRouteErrorResponse(error)) {
-    return (
-      <div className="flex h-full grow flex-col items-center justify-center p-5">
-        <div className="text-5xl font-black">{messages[error.status]}</div>
-      </div>
-    )
+  if (!isRouteErrorResponse(error)) {
+    return <></>
   }
 
-  return <></>
+  if (error.status === STATUS_CODES.UNAUTHORIZED) {
+    const result = redirectSchema.safeParse(error.data)
+
+    return <Navigate to="/login" replace state={result.success ? result.data : null} />
+  }
+
+  return (
+    <div className="flex h-full grow flex-col items-center justify-center p-5">
+      <div className="text-5xl font-black">{messages[error.status]}</div>
+    </div>
+  )
 }
