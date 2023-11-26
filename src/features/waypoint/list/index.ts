3e9c4526c2d8fe2d17ae719411e667000100f1a3
Ben Weier <ben.weier@gmail.com>
2023-11-26 23:24:33 +1000

    create waypoint list fallback and error components

--- src/features/waypoint/list/index.ts
+++ src/features/waypoint/list/index.ts
@@ -1,2 +1,4 @@
 export { WaypointList } from './waypoint-list.component'
+export { WaypointListFallback } from './waypoint-list.fallback'
+export { WaypointListError } from './waypoint-list.err'
 export type { WaypointListProps } from './waypoint-list.types'
---
+++ src/features/waypoint/list/waypoint-list.err.tsx
@@ -0,0 +1,16 @@
+import { type ErrorComponentProps } from '@/components/error-boundary'
+import { isHttpError } from '@/services/http'
+
+export const WaypointListError = ({ error }: ErrorComponentProps) => {
+  if (!error) return <></>
+
+  if (isHttpError(error)) {
+    return (
+      <div className="rounded border-2 border-transparent px-3 py-9">
+        <div className="text-secondary text-center text-xl font-bold">{error.statusText}</div>
+      </div>
+    )
+  }
+
+  return <></>
+}
