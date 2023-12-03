ce7487e5c56654277af8773154fa007f44377c9a
Ben Weier <ben.weier@gmail.com>
2023-12-03 13:22:09 +1000

    update for loop

--- src/hooks/use-container-query.hook.ts
+++ src/hooks/use-container-query.hook.ts
@@ -1,17 +1,16 @@
 import { useCallback, useState } from 'react'
-import useResizeObserver from 'use-resize-observer'
-import { type ResizeHandler } from 'use-resize-observer'
+import useResizeObserver, { type ResizeHandler } from 'use-resize-observer'
 
 type BreakpointKey = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
 
 export type ContainerBreakpoints = Array<[BreakpointKey, number]>
 
 export const useContainerQuery = (breakpoints: ContainerBreakpoints) => {
-  const [breakpoint, setBreakpoint] = useState<BreakpointKey | undefined>(undefined)
+  const [breakpoint, setBreakpoint] = useState<BreakpointKey>()
   const onResize = useCallback<ResizeHandler>(
     ({ width }) => {
-      for (let i = 0; i < breakpoints.length; i = i + 1) {
-        const [key, value] = breakpoints[i]
+      for (const bp of breakpoints) {
+        const [key, value] = bp
 
         if (width !== undefined && width <= value) {
           setBreakpoint(key)
