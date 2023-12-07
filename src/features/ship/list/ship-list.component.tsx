b5dde6c8be74059986e2ffab3159447904713628
Ben Weier <ben.weier@gmail.com>
2023-12-07 19:14:43 +1000

    remove container query usage

--- src/features/ship/list/ship-list.component.tsx
+++ src/features/ship/list/ship-list.component.tsx
@@ -1,12 +1,9 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
-import { useDesktopContainerQuery } from '@/components/responsive/use-responsive.hook'
-import { ShipItem } from '@/features/ship/item'
 import { getShipListQuery } from '@/services/api/spacetraders'
 import { ShipListRefresh } from './ship-list-refresh.component'
 import { ShipListTable } from './ship-list.table'
 
 export const ShipList = () => {
-  const container = useDesktopContainerQuery()
   const { data } = useSuspenseQuery({
     queryKey: getShipListQuery.getQueryKey(),
     queryFn: getShipListQuery.queryFn,
@@ -16,20 +13,12 @@ export const ShipList = () => {
   const ships = data.data
 
   return (
-    <div ref={container.ref} className="space-y-4">
+    <div className="space-y-4">
       <div className="flex items-center justify-end gap-2">
         <ShipListRefresh />
       </div>
 
-      {container.match !== undefined && (
-        <>
-          {container.match ? (
-            <ShipListTable data={ships.map((ship) => ({ ship }))} />
-          ) : (
-            ships.map((ship) => <ShipItem key={ship.symbol} ship={ship} />)
-          )}
-        </>
-      )}
+      <ShipListTable data={ships.map((ship) => ({ ship }))} />
     </div>
   )
 }
