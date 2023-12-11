c395df6b6fd6ab58ac5da827ad2094c7da50a2f6
Ben Weier <ben.weier@gmail.com>
2023-12-11 23:02:46 +1000

    add can buy and can sell callbacks to trade good context

--- src/features/trade-good/context/trade-good.context.ts
+++ src/features/trade-good/context/trade-good.context.ts
@@ -1,5 +1,11 @@
 import { type FC, createContext } from 'react'
 import { type TradeGoodBuyProps } from '@/features/trade-good/buy'
 import { type TradeGoodSellProps } from '@/features/trade-good/sell'
+import { type MarketTradeGood } from '@/types/spacetraders'
 
-export const TradeGoodContext = createContext<{ Buy?: FC<TradeGoodBuyProps>; Sell?: FC<TradeGoodSellProps> }>({})
+export const TradeGoodContext = createContext<{
+  Buy?: FC<TradeGoodBuyProps>
+  Sell?: FC<TradeGoodSellProps>
+  canBuy?(good?: MarketTradeGood): boolean
+  canSell?(good?: MarketTradeGood): boolean
+}>({})
