b4f006647beadfdf027236737d0e33a6366c6832
Ben Weier <ben.weier@gmail.com>
2023-12-12 20:27:07 +1000

    update ship cargo fallback layout

--- src/features/ship/cargo/ship-cargo.fallback.tsx
+++ src/features/ship/cargo/ship-cargo.fallback.tsx
@@ -1,11 +1,9 @@
-import { ShipCargoLayout } from './ship-cargo.layout'
-
 export const ShipCargoFallback = () => {
   return (
-    <ShipCargoLayout>
+    <div className="grid grid-cols-1 gap-2">
       <div className="h-20 animate-pulse rounded bg-zinc-500 bg-opacity-5 dark:bg-opacity-10"></div>
       <div className="h-20 animate-pulse rounded bg-zinc-500 bg-opacity-5 dark:bg-opacity-10"></div>
       <div className="h-20 animate-pulse rounded bg-zinc-500 bg-opacity-5 dark:bg-opacity-10"></div>
-    </ShipCargoLayout>
+    </div>
   )
 }
