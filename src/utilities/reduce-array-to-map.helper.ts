363daf5f2e2af101c8eb00de41e14857966055ae
Ben Weier <ben.weier@gmail.com>
2023-12-13 21:09:20 +1000

    create array to map reducer

---
+++ src/utilities/reduce-array-to-map.helper.ts
@@ -0,0 +1,7 @@
+export const reduceArrayToMap = <T extends Record<string, any>>(arr: T[] = [], key: keyof T, initial = new Map()) => {
+  return arr.reduce<Map<string, T>>((result, item) => {
+    result.set(item[key], item)
+
+    return result
+  }, initial)
+}
