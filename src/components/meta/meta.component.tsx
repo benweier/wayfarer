376d9b24179ff9d16275bad759fb799e7db676d8
Ben Weier <ben.weier@gmail.com>
2023-12-22 18:11:34 +1000

    don't need to await data

--- src/components/meta/meta.component.tsx
+++ src/components/meta/meta.component.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useTranslation } from 'react-i18next'
-import { Await, useMatches } from 'react-router-dom'
+import { useMatches } from 'react-router-dom'
 import { isFunction } from '@/utilities/is-function'
 import { hasRouteHandle } from '@/utilities/route-handle.helper'
 import { type MetaProps } from './meta.types'
@@ -9,33 +9,26 @@ import { type MetaProps } from './meta.types'
 export const Meta = ({ titleTemplate }: MetaProps) => {
   const { t } = useTranslation()
   const matches = useMatches()
-  const helmet = matches.filter(hasRouteHandle<MetaFunction>('meta', isFunction)).map((match) => {
+
+  return matches.filter(hasRouteHandle<MetaFunction>('meta', isFunction)).map((match) => {
+    const metas = match.handle.meta(t, match.data)
+
     return (
       <Fragment key={match.id}>
-        <Await resolve={match.data}>
-          {(data) => {
-            const metas = match.handle.meta(t, data)
+        <Helmet titleTemplate={titleTemplate}>
+          {metas.map((meta) => {
+            if ('title' in meta) {
+              return <title key={meta.title}>{meta.title}</title>
+            }
 
-            return (
-              <Helmet titleTemplate={titleTemplate}>
-                {metas.map((meta) => {
-                  if ('title' in meta) {
-                    return <title key={meta.title}>{meta.title}</title>
-                  }
+            if ('name' in meta) {
+              return <meta key={`${meta.name}|${meta.content}`} name={meta.name} content={meta.content} />
+            }
 
-                  if ('name' in meta) {
-                    return <meta key={`${meta.name}|${meta.content}`} name={meta.name} content={meta.content} />
-                  }
-
-                  return <Fragment key={meta} />
-                })}
-              </Helmet>
-            )
-          }}
-        </Await>
+            return <Fragment key={meta} />
+          })}
+        </Helmet>
       </Fragment>
     )
   })
-
-  return <>{helmet}</>
 }
