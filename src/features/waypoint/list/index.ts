daa2aeb48271ce212fc3464db7dbf6f6f9482573
Ben Weier <ben.weier@gmail.com>
2023-12-23 12:31:36 +1000

    remove waypoint list props

--- src/features/waypoint/list/index.ts
+++ src/features/waypoint/list/index.ts
@@ -1,4 +1,3 @@
 export { WaypointList } from './waypoint-list.component'
 export { WaypointListFallback } from './waypoint-list.fallback'
 export { WaypointListError } from './waypoint-list.err'
-export type { WaypointListProps } from './waypoint-list.types'
--- src/features/waypoint/list/waypoint-list.types.ts
+++ src/features/waypoint/list/waypoint-list.types.ts
@@ -1,10 +1,4 @@
-import { type FC } from 'react'
-import { type WaypointItemProps } from '@/features/waypoint/item'
 import { type WaypointResponse } from '@/types/spacetraders'
-
-export type WaypointListProps = {
-  Waypoint?: FC<WaypointItemProps>
-}
 
 export type WaypointListTableSchema = {
   waypoint: WaypointResponse
