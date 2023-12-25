48c150cdd74cc445585daed1d42dec3e5d25658a
Ben Weier <ben.weier@gmail.com>
2023-12-25 21:41:13 +1000

    set up leaderboard and agents navigation items

--- src/features/navigation/navigation.component.tsx
+++ src/features/navigation/navigation.component.tsx
@@ -3,11 +3,14 @@ import { NavLink } from 'react-router-dom'
 import { MenuIcon } from '@/components/icons'
 import { ROUTES } from '@/config/routes'
 
-const menu = [
+const menu: Array<{ key: string; href: string; icon: string } | { key: 'divider'; divider: true }> = [
   // { name: 'Overview', href: ROUTES.OVERVIEW, icon: 'home' },
   // { name: 'Contracts', href: ROUTES.CONTRACTS, icon: 'contracts' },
   { key: 'navigation.fleet', href: ROUTES.FLEET, icon: 'fleet' },
   { key: 'navigation.systems', href: ROUTES.SYSTEMS, icon: 'systems' },
+  { key: 'divider', divider: true },
+  { key: 'navigation.leaderboard', href: ROUTES.LEADERBOARD, icon: 'leaderboard' },
+  { key: 'navigation.agents', href: ROUTES.AGENTS, icon: 'agents' },
 ]
 
 export const Navigation = () => {
@@ -15,18 +18,24 @@ export const Navigation = () => {
 
   return (
     <nav className="flex flex-col items-center justify-center gap-2 p-4">
-      {menu.map((item) => (
-        <NavLink
-          key={item.key}
-          to={item.href}
-          className="flex w-full items-center gap-4 overflow-hidden rounded-md border-2 border-blue-600 px-3 py-2 font-semibold text-blue-200 transition-all duration-100 hover:scale-105 hover:bg-blue-50/10 hover:shadow-sm active:scale-100 @[220px]/side:w-full [&.active]:bg-blue-50/20 [&.active]:text-white [&.active]:shadow [&.active]:shadow-blue-800"
-        >
-          <div>
-            <MenuIcon id={item.icon} className="size-5" aria-hidden />
-          </div>
-          <span className="sr-only text-sm @[220px]/side:not-sr-only">{t(item.key)}</span>
-        </NavLink>
-      ))}
+      {menu.map((item) => {
+        if ('divider' in item) {
+          return <div key={item.key} className="h-0.5 w-full rounded-full bg-zinc-100/10"></div>
+        }
+
+        return (
+          <NavLink
+            key={item.key}
+            to={item.href}
+            className="flex w-full items-center gap-4 overflow-hidden rounded-md border-2 border-blue-600 px-3 py-2 font-semibold text-blue-200 transition-all duration-100 hover:scale-105 hover:bg-blue-50/10 hover:shadow-sm active:scale-100 @[220px]/side:w-full [&.active]:bg-blue-50/20 [&.active]:text-white [&.active]:shadow [&.active]:shadow-blue-800"
+          >
+            <div>
+              <MenuIcon id={item.icon} className="size-5" aria-hidden />
+            </div>
+            <span className="sr-only text-sm @[220px]/side:not-sr-only">{t(item.key)}</span>
+          </NavLink>
+        )
+      })}
     </nav>
   )
 }
