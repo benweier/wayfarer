5fb8c3f8ea37abe7040e302118bd9bf83dbe8afe
Ben Weier <ben.weier@gmail.com>
2023-12-25 21:40:08 +1000

    add agent list query

--- src/services/api/spacetraders/agent.ts
+++ src/services/api/spacetraders/agent.ts
@@ -1,27 +1,36 @@
 import { type QueryFunctionContext } from '@tanstack/react-query'
-import { type SpaceTradersResponse, createHeaders } from '@/services/api/spacetraders/core'
+import {
+  type Meta,
+  type SpaceTradersResponse,
+  attachQueryParams,
+  createHeaders,
+} from '@/services/api/spacetraders/core'
 import { get } from '@/services/fetch'
+import { type AgentResponse } from '@/types/spacetraders'
+
+type AgentQueryKey<T extends keyof typeof AGENT_QUERIES> = ReturnType<(typeof AGENT_QUERIES)[T]>
 
 const AGENT_QUERIES = {
+  agentList: (params?: { page?: number; limit?: number }) => [{ scope: 'agents', entity: 'list' }, params] as const,
   agentBySymbol: (args: { agentSymbol: string }) => [{ scope: 'agents', entity: 'item' }, args] as const,
+}
+
+export const getAgentListQuery = {
+  getQueryKey: AGENT_QUERIES.agentList,
+  queryFn: async ({ queryKey: [, params], signal }: QueryFunctionContext<AgentQueryKey<'agentList'>>) => {
+    const url = new URL('agents', import.meta.env.SPACETRADERS_API_BASE_URL)
+
+    if (params) attachQueryParams(url, params)
+
+    return get<SpaceTradersResponse<AgentResponse[], Meta>>(url, { signal, headers: createHeaders() })
+  },
 }
 
 export const getAgentBySymbolQuery = {
   getQueryKey: (args: { agentSymbol: string }) => [{ scope: 'agents', entity: 'item' }, args] as const,
-  queryFn: async ({
-    queryKey: [, args],
-    signal,
-  }: QueryFunctionContext<ReturnType<(typeof AGENT_QUERIES)['agentBySymbol']>>) => {
+  queryFn: async ({ queryKey: [, args], signal }: QueryFunctionContext<AgentQueryKey<'agentBySymbol'>>) => {
     const url = new URL(`agents/${args.agentSymbol}`, import.meta.env.SPACETRADERS_API_BASE_URL)
 
-    return get<
-      SpaceTradersResponse<{
-        symbol: string
-        headquarters: string
-        credits: number
-        startingFaction: string
-        shipCount: number
-      }>
-    >(url, { signal, headers: createHeaders() })
+    return get<SpaceTradersResponse<AgentResponse>>(url, { signal, headers: createHeaders() })
   },
 }
