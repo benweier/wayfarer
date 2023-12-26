5386c776959352fd38cfa99357583e303964942b
Ben Weier <ben.weier@gmail.com>
2023-12-26 19:08:35 +1000

    create waypoint cell to chunk pages of system waypoints

--- src/features/system/list/system-list.columns.tsx
+++ src/features/system/list/system-list.columns.tsx
@@ -1,15 +1,12 @@
 import { createColumnHelper } from '@tanstack/react-table'
-import { cx } from 'class-variance-authority'
 import { Translation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 import { ShipIcon } from '@/components/icons'
 import { SystemTag } from '@/components/system/tag'
-import { WAYPOINT_TYPE_STYLES } from '@/config/waypoint.styles'
 import { ShipPresence } from '@/features/ship/presence'
-import { type SystemWaypoint } from '@/types/spacetraders'
+import { SystemWaypointsCell } from '@/features/system/list/waypoints-cell.component'
 import { type SystemListTableSchema } from './system-list.types'
 
-const EXCLUDED_WAYPOINTS = new Set(['ASTEROID', 'ENGINEERED_ASTEROID'])
 const columnHelper = createColumnHelper<SystemListTableSchema>()
 
 export const columns = [
@@ -92,57 +89,9 @@ export const columns = [
       )
     },
     cell: ({ getValue, row }) => {
-      const values = getValue().reduce<{ waypoints: SystemWaypoint[]; extra: number }>(
-        (result, waypoint) => {
-          if (EXCLUDED_WAYPOINTS.has(waypoint.type)) {
-            result.extra += 1
+      const waypoints = getValue()
 
-            return result
-          }
-
-          result.waypoints.push(waypoint)
-
-          return result
-        },
-        { waypoints: [], extra: 0 },
-      )
-
-      return (
-        <ul className="relative isolate flex list-none flex-wrap items-center justify-end -space-x-2">
-          {values.waypoints.map((waypoint) => {
-            return (
-              <li
-                key={waypoint.symbol}
-                className="list-none overflow-hidden rounded-full border-2 border-zinc-50 transition duration-100 ease-in-out hover:z-0 hover:scale-125 dark:border-zinc-800"
-              >
-                <Link
-                  aria-label={`${waypoint.symbol}: ${waypoint.type}`}
-                  className={cx('flex size-6 items-center justify-center', WAYPOINT_TYPE_STYLES[waypoint.type])}
-                  to={`/systems/${row.original.system.symbol}/waypoint/${waypoint.symbol}`}
-                >
-                  <span className="text-sm font-medium" aria-hidden>
-                    {waypoint.type.charAt(0)}
-                  </span>
-                  <span className="sr-only">{waypoint.symbol}</span>
-                </Link>
-              </li>
-            )
-          })}
-          {values.extra > 0 && (
-            <li className="list-none overflow-hidden rounded-full border-2 border-zinc-50 transition duration-100 ease-in-out hover:z-0 hover:scale-125 dark:border-zinc-800">
-              <Link
-                aria-label={`${values.extra} asteroids`}
-                className={cx('flex size-6 items-center justify-center', WAYPOINT_TYPE_STYLES.ASTEROID)}
-                to={`/systems/${row.original.system.symbol}`}
-              >
-                <span className="text-sm font-medium" aria-hidden>
-                  {values.extra}
-                </span>
-              </Link>
-            </li>
-          )}
-        </ul>
-      )
+      return <SystemWaypointsCell systemSymbol={row.original.system.symbol} waypoints={waypoints} />
     },
     minSize: 30,
     maxSize: 30,
---
+++ src/features/system/list/waypoints-cell.component.tsx
@@ -0,0 +1,130 @@
+import { cx } from 'class-variance-authority'
+import { Link } from 'react-router-dom'
+import { WAYPOINT_TYPE_STYLES } from '@/config/waypoint.styles'
+import { type SystemWaypoint } from '@/types/spacetraders'
+import { chunk } from '@/utilities/chunk.helper'
+
+const EXCLUDED_WAYPOINTS = new Set(['ASTEROID', 'ENGINEERED_ASTEROID', 'ASTEROID_BASE'])
+
+export const SystemWaypointsCell = ({
+  systemSymbol,
+  waypoints,
+}: {
+  systemSymbol: string
+  waypoints: SystemWaypoint[]
+}) => {
+  const values = waypoints.reduce<{
+    waypoints: Array<{
+      symbol: string
+      type: string
+      label: string | number
+    }>
+    asteroids: {
+      symbol: string
+      type: string
+      label: number
+    }
+  }>(
+    (result, waypoint) => {
+      if (EXCLUDED_WAYPOINTS.has(waypoint.type)) {
+        result.asteroids.label += 1
+
+        return result
+      }
+
+      result.waypoints.push({
+        symbol: waypoint.symbol,
+        type: waypoint.type,
+        label: waypoint.type.charAt(0),
+      })
+
+      return result
+    },
+    {
+      waypoints: [],
+      asteroids: {
+        symbol: 'ASTEROIDS',
+        type: 'ASTEROID',
+        label: 0,
+      },
+    },
+  )
+  const pages = chunk(values.waypoints, 10)
+
+  switch (pages.length) {
+    case 0: {
+      pages.push([values.asteroids])
+      break
+    }
+
+    case 1: {
+      pages[0].push(values.asteroids)
+      break
+    }
+
+    default: {
+      const lastPage = pages.at(-1)
+
+      if (lastPage !== undefined && lastPage.length < 3) {
+        const popped = pages.pop()
+
+        if (popped) pages[pages.length - 1] = pages[pages.length - 1].concat(popped)
+      }
+
+      pages[pages.length - 1].push(values.asteroids)
+      break
+    }
+  }
+
+  return (
+    <div className="relative isolate list-none -space-y-1">
+      {pages.map((page, index) => {
+        return (
+          <div key={index} className="flex items-center justify-end -space-x-2">
+            {page.map((waypoint) => {
+              if (waypoint.type !== 'ASTEROID') {
+                return (
+                  <Link
+                    key={waypoint.symbol}
+                    className={cx(
+                      'flex size-8 items-center justify-center rounded-full border-2 border-zinc-50 transition duration-100 ease-in-out hover:z-0 hover:scale-125 dark:border-zinc-800',
+                      WAYPOINT_TYPE_STYLES[waypoint.type],
+                    )}
+                    to={`/systems/${systemSymbol}/waypoint/${waypoint.symbol}`}
+                  >
+                    <span className="text-sm font-medium" aria-hidden>
+                      {waypoint.label}
+                    </span>
+                    <span className="sr-only">
+                      {waypoint.type} {waypoint.symbol}
+                    </span>
+                  </Link>
+                )
+              }
+
+              if (waypoint.label === 0) return null
+
+              return (
+                <Link
+                  key={waypoint.symbol}
+                  className={cx(
+                    'flex size-8 items-center justify-center rounded-full border-2 border-zinc-50 transition duration-100 ease-in-out hover:z-0 hover:scale-125 dark:border-zinc-800',
+                    WAYPOINT_TYPE_STYLES[waypoint.type],
+                  )}
+                  to={`/systems/${systemSymbol}`}
+                >
+                  <span className="text-sm font-medium" aria-hidden>
+                    {waypoint.label}
+                  </span>
+                  <span className="sr-only">
+                    {waypoint.label} {waypoint.symbol}
+                  </span>
+                </Link>
+              )
+            })}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
