4693566b8965a8c8adb459579f09a8adf042f739
Ben Weier <ben.weier@gmail.com>
2024-01-01 22:31:47 +1000

    add translation strings for contract features

--- src/routes/contracts/contract/contract.route.tsx
+++ src/routes/contracts/contract/contract.route.tsx
@@ -1,15 +1,17 @@
+import { useTranslation } from 'react-i18next'
 import { useParams } from 'react-router-dom'
 import { QuerySuspenseBoundary, withQSB } from '@/components/query-suspense-boundary'
 import { ContractDetail } from '@/features/contract/detail'
 
 const ContractRouteComponent = () => {
+  const { t } = useTranslation()
   const { contractId } = useParams()
 
   return (
     <div className="grid gap-4 p-4">
       <div className="flex items-center justify-start gap-6">
         <div className="text-title">
-          Contract: <span className="font-normal">{contractId}</span>
+          {t('contract.label')}: <span className="font-normal">{contractId}</span>
         </div>
       </div>
 
--- src/routes/contracts/contracts.route.tsx
+++ src/routes/contracts/contracts.route.tsx
@@ -1,10 +1,13 @@
+import { useTranslation } from 'react-i18next'
 import { QuerySuspenseBoundary, withQSB } from '@/components/query-suspense-boundary'
 import { ContractList, ContractListFallback } from '@/features/contract/list'
 
 export const ContractsRouteComponent = () => {
+  const { t } = useTranslation()
+
   return (
     <div className="grid gap-4 p-4">
-      <h1 className="text-title">Contracts</h1>
+      <h1 className="text-title">{t('contracts.label')}</h1>
 
       <QuerySuspenseBoundary fallback={<ContractListFallback />}>
         <ContractList />
