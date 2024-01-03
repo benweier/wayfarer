63d39b3db67b4b02409ae20e52148347bcf75fd9
Ben Weier <ben.weier@gmail.com>
2024-01-03 12:53:47 +1000

    mark predicate as optional

--- src/features/survey/list/survey-list.types.ts
+++ src/features/survey/list/survey-list.types.ts
@@ -1,7 +1,7 @@
 import { type SurveyResponse } from '@/types/spacetraders'
 
 export type SurveyListProps = {
-  predicate: (value: SurveyResponse, index: number, array: SurveyResponse[]) => boolean
+  predicate?: (value: SurveyResponse, index: number, array: SurveyResponse[]) => boolean
 }
 
 export type SurveyListTableSchema = {
