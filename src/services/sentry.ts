74b4d09599f4edad2927385087e05b5a2e91162a
Ben Weier <ben.weier@gmail.com>
2024-01-07 12:23:45 +1000

    remove react router instrumentation

--- src/services/sentry.ts
+++ src/services/sentry.ts
@@ -1,22 +1,9 @@
 import * as sentry from '@sentry/react'
-import { useEffect } from 'react'
-import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-router-dom'
 
 sentry.init({
   dsn: import.meta.env.SENTRY_DSN,
   enabled: import.meta.env.PROD,
-  integrations: [
-    new sentry.BrowserProfilingIntegration(),
-    new sentry.BrowserTracing({
-      routingInstrumentation: sentry.reactRouterV6Instrumentation(
-        useEffect,
-        useLocation,
-        useNavigationType,
-        createRoutesFromChildren,
-        matchRoutes,
-      ),
-    }),
-  ],
+  integrations: [new sentry.BrowserProfilingIntegration(), new sentry.BrowserTracing()],
   tracesSampleRate: 0.1,
 })
 
