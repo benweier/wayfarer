95dde52d082cfba7c1afb75bd3fecdab556c86eb
Ben Weier <ben.weier@gmail.com>
2024-01-07 22:36:25 +1000

    refactor meta component to handle route matches from tanstack router

--- src/components/meta/meta.component.tsx
+++ src/components/meta/meta.component.tsx
@@ -1,39 +1,40 @@
+import { useRouterState } from '@tanstack/react-router'
 import { Fragment } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useTranslation } from 'react-i18next'
-import { useMatches } from 'react-router-dom'
-import { isFunction } from '@/utilities/is-function'
-import { hasRouteHandle } from '@/utilities/route-handle.helper'
 import { type MetaProps } from './meta.types'
 
 export const Meta = ({ titleTemplate }: MetaProps) => {
+  const routerState = useRouterState()
   const { t } = useTranslation()
-  const matches = useMatches()
-  const handlers = matches.filter(hasRouteHandle<MetaFunction>('meta', isFunction))
-
-  if (handlers.length === 0) return null
+  const matches = [...routerState.matches]
+    .filter((match) => typeof match.routeContext.meta === 'function')
+    .map((match) => {
+      return {
+        id: match.id,
+        meta: match.routeContext.meta(t, match.loaderData) as MetaObject[],
+      }
+    })
 
   return (
     <Helmet titleTemplate={titleTemplate}>
-      {handlers.map((match) => {
-        const metas = match.handle.meta(t, match.data)
-
+      {matches.map(({ id, meta }) => {
         return (
-          <Fragment key={match.id}>
-            {metas.map((meta) => {
-              if ('title' in meta) {
-                return <title key={meta.title}>{meta.title}</title>
+          <Fragment key={id}>
+            {meta.map((tag) => {
+              if ('title' in tag) {
+                return <title key={tag.title}>{tag.title}</title>
               }
 
-              if ('name' in meta) {
-                return <meta key={`${meta.name}|${meta.content}`} name={meta.name} content={meta.content} />
+              if ('name' in tag) {
+                return <meta key={`${tag.name}|${tag.content}`} name={tag.name} content={tag.content} />
               }
 
-              if ('property' in meta) {
-                return <meta key={`${meta.property}|${meta.content}`} property={meta.property} content={meta.content} />
+              if ('property' in tag) {
+                return <meta key={`${tag.property}|${tag.content}`} property={tag.property} content={tag.content} />
               }
 
-              return <Fragment key={meta} />
+              return <Fragment key={tag} />
             })}
           </Fragment>
         )
