dee71e6bb10b26114df3ceaf2c4deb7a139dd526
Ben Weier <ben.weier@gmail.com>
2024-01-07 23:52:18 +1000

    add meta handler to not found route

--- src/routes/not-found.route.ts
+++ src/routes/not-found.route.ts
@@ -1,8 +1,14 @@
 import { NotFoundRoute } from '@tanstack/react-router'
+import { type TFunction } from 'i18next'
 import { NotFound } from '@/components/not-found'
 import { rootRoute } from './root.route'
 
 export const notFoundRoute = new NotFoundRoute({
   getParentRoute: () => rootRoute,
+  beforeLoad: () => {
+    return {
+      meta: (t: TFunction) => [{ title: t('not_found.title', { ns: 'meta' }) }],
+    }
+  },
   component: NotFound,
 })
