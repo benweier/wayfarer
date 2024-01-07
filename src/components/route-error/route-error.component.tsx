d25964f92f4ed93a2a17ea39bb2babe3f3599026
Ben Weier <ben.weier@gmail.com>
2024-01-07 23:52:54 +1000

    refactor route error for tanstack router

--- src/components/route-error/route-error.component.tsx
+++ src/components/route-error/route-error.component.tsx
@@ -1,33 +1,24 @@
-import { Navigate, isRouteErrorResponse, useRouteError } from 'react-router-dom'
-import { STATUS_CODES, STATUS_MESSAGES } from '@/services/http'
-import { redirectSchema } from '@/validators/redirect.schema'
+import { Navigate } from '@tanstack/react-router'
+import { NotFound } from '@/components/not-found'
+import { loginRoute } from '@/routes/auth.route'
+import { STATUS_CODES, isHttpErrorResponse } from '@/services/http'
 
-const messages: Record<number, string> = {
-  [STATUS_CODES.UNAUTHORIZED]: STATUS_MESSAGES.UNAUTHORIZED,
-  [STATUS_CODES.FORBIDDEN]: STATUS_MESSAGES.FORBIDDEN,
-  [STATUS_CODES.NOT_FOUND]: STATUS_MESSAGES.NOT_FOUND,
-  [STATUS_CODES.UNPROCESSABLE_ENTITY]: STATUS_MESSAGES.UNPROCESSABLE_ENTITY,
-  [STATUS_CODES.INTERNAL_SERVER_ERROR]: STATUS_MESSAGES.INTERNAL_SERVER_ERROR,
-}
+export const RouteError = ({ error }: { error: any }) => {
+  if (isHttpErrorResponse(error)) {
+    if (error.status === STATUS_CODES.UNAUTHORIZED) {
+      return (
+        <Navigate to={loginRoute.to} replace search={{ redirect: location.pathname }} mask={{ to: loginRoute.to }} />
+      )
+    }
 
-export const RouteError = () => {
-  const error = useRouteError()
-
-  if (!isRouteErrorResponse(error)) {
-    if (error instanceof Error) return <>{error.message}</>
-
-    return <></>
-  }
-
-  if (error.status === STATUS_CODES.UNAUTHORIZED) {
-    const result = redirectSchema.safeParse(error.data)
-
-    return <Navigate to="/login" replace state={result.success ? result.data : null} />
+    if (error.status === STATUS_CODES.NOT_FOUND) {
+      return <NotFound />
+    }
   }
 
   return (
     <div className="flex h-full grow flex-col items-center justify-center p-5">
-      <div className="text-5xl font-black">{messages[error.status]}</div>
+      <div className="text-5xl font-black">An error occurred while displaying this content</div>
     </div>
   )
 }
