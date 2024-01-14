775b09c0e6684b67ad46549b6d799a3c107c09eb
Ben Weier <ben.weier@gmail.com>
2024-01-14 17:06:33 +1000

    disable survey and extract if waypoint is not an asteroid

--- src/features/ship/survey-extract/ship-survey-extract.component.tsx
+++ src/features/ship/survey-extract/ship-survey-extract.component.tsx
@@ -1,16 +1,19 @@
 import { useTranslation } from 'react-i18next'
 import { Button } from '@/components/button'
+import { useWaypointResponse } from '@/context/waypoint.context'
 import * as ShipActions from '@/features/ship/actions'
 import { ShipCooldown } from '@/features/ship/cooldown'
 import { type ShipResponse } from '@/types/spacetraders'
 
 export const ShipSurveyExtract = ({ ship }: { ship: ShipResponse }) => {
   const { t } = useTranslation()
+  const waypoint = useWaypointResponse()
+  const disabled = waypoint.type !== 'ASTEROID'
 
   return (
     <div className="flex flex-col items-center justify-center gap-4 rounded border-2 border-dashed border-zinc-300 px-3 py-9 dark:border-zinc-600">
       <div className="flex gap-2">
-        <ShipActions.Survey ship={ship}>
+        <ShipActions.Survey ship={ship} disabled={disabled}>
           {(props) => (
             <Button intent="primary" {...props}>
               {t('ship.action.survey')} {ship.nav.waypointSymbol}
@@ -18,7 +21,7 @@ export const ShipSurveyExtract = ({ ship }: { ship: ShipResponse }) => {
           )}
         </ShipActions.Survey>
 
-        <ShipActions.Extract ship={ship}>
+        <ShipActions.Extract ship={ship} disabled={disabled}>
           {(props) => (
             <Button intent="confirm" {...props}>
               {t('ship.action.extract', { context: 'no_survey' })}
