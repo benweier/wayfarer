e50c4ea9f4e1c8530fbb6e7d10250a8c1e5674fd
Ben Weier <ben.weier@gmail.com>
2024-01-15 23:52:44 +1000

    remove ship row, clean up table hook config

--- src/features/ship/list/ship-list.table.tsx
+++ src/features/ship/list/ship-list.table.tsx
@@ -1,6 +1,4 @@
-import { useIsMutating } from '@tanstack/react-query'
 import {
-  type ColumnFiltersState,
   type SortingState,
   getCoreRowModel,
   getFacetedRowModel,
@@ -9,29 +7,13 @@ import {
   getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table'
-import { cx } from 'class-variance-authority'
 import { useState } from 'react'
-import { Row, Table } from '@/components/table'
+import { Table } from '@/components/table'
 import { columns } from './ship-list.columns'
-import { type ShipListTableProps, type ShipRowProps } from './ship-list.types'
-
-const ShipRow = ({ row }: ShipRowProps) => {
-  const isMutating =
-    useIsMutating({ mutationKey: [{ scope: 'ships', entity: 'item' }, { shipSymbol: row.original.ship.symbol }] }) > 0
-
-  return (
-    <Row
-      className={cx({
-        'pointer-events-none opacity-30': isMutating,
-      })}
-      row={row}
-    />
-  )
-}
+import { type ShipListTableProps } from './ship-list.types'
 
 export const ShipListTable = ({ data }: ShipListTableProps) => {
   const [sorting, setSorting] = useState<SortingState>([{ id: 'symbol', desc: false }])
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
   const table = useReactTable({
     data,
     columns,
@@ -41,13 +23,8 @@ export const ShipListTable = ({ data }: ShipListTableProps) => {
     getFacetedRowModel: getFacetedRowModel(),
     getFacetedUniqueValues: getFacetedUniqueValues(),
     onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
-    state: { sorting, columnFilters },
-    enableHiding: true,
-    enableFilters: true,
-    enableColumnFilters: true,
-    enableGlobalFilter: true,
+    state: { sorting },
   })
 
-  return <Table table={table} Row={ShipRow} />
+  return <Table table={table} />
 }
