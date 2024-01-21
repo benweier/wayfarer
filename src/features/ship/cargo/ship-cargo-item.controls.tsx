6a4c8dcc76c2b1be95ea7310081c1c5b4f8c3c12
Ben Weier <ben.weier@gmail.com>
2024-01-21 12:32:13 +1000

    refactor ship controls to use radix dropdown

--- src/features/ship/cargo/ship-cargo-item.controls.tsx
+++ src/features/ship/cargo/ship-cargo-item.controls.tsx
@@ -1,119 +1,69 @@
-import { FloatingPortal } from '@floating-ui/react'
-import { autoUpdate, offset, shift, useFloating } from '@floating-ui/react-dom'
-import { Menu, Transition } from '@headlessui/react'
-import { Fragment } from 'react'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import { useTranslation } from 'react-i18next'
-import { Button } from '@/components/button'
 import { AppIcon } from '@/components/icons'
-import { Modal, useModalActions, useModalImperativeHandle } from '@/components/modal'
+import { Modal, useModalImperativeHandle } from '@/components/modal'
 import { useShipResponse } from '@/context/ship.context'
-import * as ShipActions from '@/features/ship/actions'
 import { type CargoInventory } from '@/types/spacetraders'
-
-const CancelModal = () => {
-  const { t } = useTranslation()
-  const { closeModal } = useModalActions()
-
-  return (
-    <Button
-      onClick={() => {
-        closeModal()
-      }}
-    >
-      {t('general.cancel')}
-    </Button>
-  )
-}
+import { CargoJettison } from './controls/jettison'
+import { CargoTransfer } from './controls/transfer'
 
 export const ShipCargoItemControls = ({ item }: { item: CargoInventory }) => {
   const { t } = useTranslation()
+  const jettison = useModalImperativeHandle()
+  const transfer = useModalImperativeHandle()
   const ship = useShipResponse()
-  const { ref, modal } = useModalImperativeHandle()
-  const { x, y, refs } = useFloating<HTMLButtonElement>({
-    strategy: 'absolute',
-    placement: 'bottom-end',
-    middleware: [offset(8), shift({ padding: 4 })],
-    whileElementsMounted: (reference, floating, update) => {
-      return autoUpdate(reference, floating, update, {
-        animationFrame: true,
-      })
-    },
-  })
 
   return (
     <>
-      <Menu>
-        <Menu.Button ref={refs.setReference} className="btn btn-icon ui-open:bg-black/5 ui-open:dark:bg-blue-500">
-          <span className="sr-only">Manage</span>
-          <AppIcon id="more:vertical" className="size-4" aria-hidden="true" />
-        </Menu.Button>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger asChild>
+          <button className="btn btn-icon data-[state=open]:bg-blue-500 data-[state=open]:text-white">
+            <span className="sr-only">Manage</span>
+            <AppIcon id="more:vertical" className="size-4" aria-hidden="true" />
+          </button>
+        </DropdownMenu.Trigger>
 
-        <FloatingPortal>
-          <div
-            ref={refs.setFloating}
-            className="absolute left-0 top-0 z-10 w-max"
-            style={{
-              transform: `translate(${Math.round(x)}px,${Math.round(y)}px)`,
+        <DropdownMenu.Portal>
+          <DropdownMenu.Content
+            className="flex w-52 flex-col gap-2 overflow-y-auto rounded-md bg-zinc-100/75 p-1 ring ring-black/5 backdrop-blur-lg dark:bg-zinc-900/75 dark:ring-white/5"
+            sideOffset={5}
+            collisionPadding={{
+              right: 16,
+              bottom: 16,
             }}
           >
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="flex w-52 origin-top-right flex-col gap-1 overflow-y-auto rounded-md bg-zinc-100/75 p-1 ring ring-black/5 backdrop-blur-lg dark:bg-zinc-900/75 dark:ring-white/5">
-                <Menu.Item as={Fragment}>
-                  {({ active }) => (
-                    <Button
-                      intent={active ? 'dim' : undefined}
-                      kind="flat"
-                      size="small"
-                      className="w-full text-left"
-                      onClick={() => {
-                        modal.open()
-                      }}
-                    >
-                      {t('ship.action.jettison')}
-                    </Button>
-                  )}
-                </Menu.Item>
-              </Menu.Items>
-            </Transition>
-          </div>
-        </FloatingPortal>
-      </Menu>
-      <Modal ref={ref} size="sm" closeable>
-        <div className="grid gap-8">
-          <h3 className="text-title">{t('general.are_you_sure')}</h3>
-          <div>
-            {t('ship.action.confirm_jettison', {
-              itemSymbol: item.symbol,
-              units: item.units,
-              context: 'text',
-            })}
-          </div>
-          <div className="flex gap-2">
-            <ShipActions.Jettison
-              ship={ship}
-              symbol={item.symbol}
-              units={item.units}
-              onDone={() => {
-                modal.close()
-              }}
-            >
-              {(props) => (
-                <Button intent="danger" adornment={{ start: <AppIcon id="trash" className="size-5" /> }} {...props}>
-                  {t('ship.action.confirm_jettison', { context: 'action' })}
-                </Button>
-              )}
-            </ShipActions.Jettison>
-            <CancelModal />
-          </div>
-        </div>
+            <DropdownMenu.Item asChild>
+              <button
+                className="btn btn-flat btn-sm w-full text-left data-[highlighted]:btn-dim"
+                onClick={() => {
+                  transfer.modal.open()
+                }}
+              >
+                {t('ship.action.transfer')}
+              </button>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item asChild>
+              <button
+                className="btn btn-flat btn-sm w-full text-left data-[highlighted]:btn-dim"
+                onClick={() => {
+                  jettison.modal.open()
+                }}
+              >
+                {t('ship.action.jettison')}
+              </button>
+            </DropdownMenu.Item>
+
+            <DropdownMenu.Arrow />
+          </DropdownMenu.Content>
+        </DropdownMenu.Portal>
+      </DropdownMenu.Root>
+
+      <Modal ref={transfer.ref} size="sm" closeable>
+        <CargoTransfer ship={ship} item={item} />
+      </Modal>
+
+      <Modal ref={jettison.ref} size="sm" closeable>
+        <CargoJettison ship={ship} item={item} />
       </Modal>
     </>
   )
