0e79cda548db64f708d240f997c080792a24ae6c
Ben Weier <ben.weier@gmail.com>
2024-01-22 18:49:51 +1000

    remove unnecessary class

--- src/components/waypoint/tag/waypoint-tag.component.tsx
+++ src/components/waypoint/tag/waypoint-tag.component.tsx
@@ -3,9 +3,5 @@ import { WAYPOINT_TYPE_STYLES } from '@/config/waypoint.styles'
 import { type WaypointTagProps } from './waypoint-tag.types'
 
 export const WaypointTag = ({ type, children }: WaypointTagProps) => {
-  return (
-    <div className={cx('inline-block rounded-sm px-2 py-0.5 text-xs font-bold', WAYPOINT_TYPE_STYLES[type])}>
-      {children}
-    </div>
-  )
+  return <div className={cx('rounded-sm px-2 py-0.5 text-xs font-bold', WAYPOINT_TYPE_STYLES[type])}>{children}</div>
 }
