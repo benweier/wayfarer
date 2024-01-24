d6601a3b2689e394aee0a0ea977546008298ee4c
Ben Weier <ben.weier@gmail.com>
2024-01-24 22:51:45 +1000

    make page search param optional

--- src/routes/conf/_dashboard/agents/index.route.ts
+++ src/routes/conf/_dashboard/agents/index.route.ts
@@ -1,9 +1,9 @@
 import { FileRoute } from '@tanstack/react-router'
-import { fallback, minValue, number, object, parse } from 'valibot'
+import { fallback, minValue, number, object, optional, parse } from 'valibot'
 import { meta } from '@/routes/agents/agents-route.meta'
 
 const SearchParamsSchema = object({
-  page: fallback(number([minValue(1)]), 1),
+  page: fallback(optional(number([minValue(1)])), 1),
 })
 
 export const Route = new FileRoute('/_dashboard/agents/').createRoute({
