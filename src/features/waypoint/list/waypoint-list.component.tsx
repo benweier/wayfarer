539e48f54240f9cf58af964a77ce984d2dec1396
Ben Weier <ben.weier@gmail.com>
2024-01-26 12:17:15 +1000

    add input to filter list of waypoints

--- src/features/waypoint/list/waypoint-list.component.tsx
+++ src/features/waypoint/list/waypoint-list.component.tsx
@@ -1,4 +1,5 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
+import { useDeferredValue, useState } from 'react'
 import { useFleetResponse } from '@/context/fleet.context'
 import { useSystemResponse } from '@/context/system.context'
 import { getShipPresence } from '@/features/ship/utilities/get-ship-presence.helper'
@@ -9,12 +10,23 @@ export const WaypointList = () => {
   const system = useSystemResponse()
   const ships = useFleetResponse()
   const { data } = useSuspenseQuery(getWaypointListQuery({ systemSymbol: system.symbol }))
-  const waypoints = data.data
   const presence = getShipPresence(ships, 'waypointSymbol')
+  const [search, setSearch] = useState('')
+  const rows = useDeferredValue(
+    data.data
+      .filter((waypoint) => (search.length > 0 ? waypoint.symbol.toLowerCase().includes(search.toLowerCase()) : true))
+      .map((waypoint) => ({ waypoint, presence: presence.get(waypoint.symbol) ?? 0 })),
+  )
 
   return (
-    <WaypointListTable
-      data={waypoints.map((waypoint) => ({ waypoint, presence: presence.get(waypoint.symbol) ?? 0 }))}
-    />
+    <div className="space-y-4">
+      <input
+        className="input w-full md:w-1/2 xl:w-1/3"
+        type="text"
+        placeholder="Search By Symbol"
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <WaypointListTable data={rows} />
+    </div>
   )
 }
--- src/features/waypoint/navigation/waypoint-navigation.component.tsx
+++ src/features/waypoint/navigation/waypoint-navigation.component.tsx
@@ -1,4 +1,5 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
+import { useDeferredValue, useState } from 'react'
 import { useFleetResponse } from '@/context/fleet.context'
 import { WaypointNavigationActionContext } from '@/context/waypoint-navigation-action.context'
 import { ShipStatus } from '@/features/ship/status'
@@ -11,23 +12,33 @@ import { type WaypointNavigationProps } from './waypoint-navigation.types'
 export const WaypointNavigation = ({ ship }: WaypointNavigationProps) => {
   const ships = useFleetResponse()
   const { data } = useSuspenseQuery(getWaypointListQuery({ systemSymbol: ship.nav.systemSymbol }))
-  const waypoints = data.data
-  const activeWaypoint = waypoints.find((waypoint) => waypoint.symbol === ship.nav.waypointSymbol)
+  const [search, setSearch] = useState('')
+  const activeWaypoint = data.data.find((waypoint) => waypoint.symbol === ship.nav.waypointSymbol)
   const presence = getShipPresence(ships, 'waypointSymbol')
+  const rows = useDeferredValue(
+    data.data
+      .filter((waypoint) => (search.length > 0 ? waypoint.symbol.toLowerCase().includes(search.toLowerCase()) : true))
+      .map((waypoint) => ({
+        waypoint,
+        ship,
+        activeWaypoint,
+        presence: presence.get(waypoint.symbol) ?? 0,
+      })),
+  )
 
   return (
     <div className="space-y-4">
       <ShipStatus ship={ship} />
 
+      <input
+        className="input w-full md:w-1/2 xl:w-1/3"
+        type="text"
+        placeholder="Search By Symbol"
+        onChange={(event) => setSearch(event.target.value)}
+      />
+
       <WaypointNavigationActionContext.Provider value={{ Navigate: WaypointNavigationAction }}>
-        <WaypointNavigationTable
-          data={waypoints.map((waypoint) => ({
-            waypoint,
-            ship,
-            activeWaypoint,
-            presence: presence.get(waypoint.symbol) ?? 0,
-          }))}
-        />
+        <WaypointNavigationTable data={rows} />
       </WaypointNavigationActionContext.Provider>
     </div>
   )
