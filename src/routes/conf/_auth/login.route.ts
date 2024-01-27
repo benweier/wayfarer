58ac57cd4897bd401e6bffb32b12b630c4b5589f
Ben Weier <ben.weier@gmail.com>
2024-01-27 13:32:44 +1000

    restore redirect search param

--- src/routes/conf/_auth/login.route.ts
+++ src/routes/conf/_auth/login.route.ts
@@ -1,7 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { object, optional, parse, string } from 'valibot'
 
+const SearchParamsSchema = object({
+  redirect: optional(string()),
+})
 const meta: MetaFunction = (t) => [{ title: t('auth.login.title', { ns: 'meta' }) }]
 
 export const Route = createFileRoute('/_auth/login')({
-  beforeLoad: () => ({ meta }),
+  validateSearch: (search) => parse(SearchParamsSchema, search),
+  beforeLoad: ({ search }) => ({ meta, search }),
 })
