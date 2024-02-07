082442e9a999859f8103799f822152c899d62586
Ben Weier <ben.weier@gmail.com>
2024-02-07 23:29:05 +1000

    add waypoint link to agent headquarters

--- src/features/agent/detail/agent-detail.component.tsx
+++ src/features/agent/detail/agent-detail.component.tsx
@@ -1,4 +1,5 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
+import { Link } from '@tanstack/react-router'
 import { useTranslation } from 'react-i18next'
 import { getAgentBySymbolQuery } from '@/services/api/spacetraders/agent'
 import { formatNumber } from '@/utilities/number.helper'
@@ -7,6 +8,7 @@ export const AgentDetail = ({ agentSymbol }: { agentSymbol: string }) => {
   const { t } = useTranslation()
   const { data } = useSuspenseQuery(getAgentBySymbolQuery({ agentSymbol }))
   const agent = data.data
+  const [sector, system, waypoint] = agent.headquarters.split('-')
 
   return (
     <div className="max-w-md space-y-2">
@@ -14,7 +16,14 @@ export const AgentDetail = ({ agentSymbol }: { agentSymbol: string }) => {
         <span className="font-bold">{t('agent.credits')}:</span> {formatNumber(agent.credits)}
       </div>
       <div className="flex items-center justify-between gap-2">
-        <span className="font-bold">{t('agent.headquarters')}:</span> {agent.headquarters}
+        <span className="font-bold">{t('agent.headquarters')}:</span>
+        <Link
+          to="/systems/$systemSymbol/waypoint/$waypointSymbol"
+          params={{ systemSymbol: `${sector}-${system}`, waypointSymbol: `${sector}-${system}-${waypoint}` }}
+          className="link"
+        >
+          {agent.headquarters}
+        </Link>
       </div>
       <div className="flex items-center justify-between gap-2">
         <span className="font-bold">{t('agent.starting_faction')}:</span> {agent.startingFaction}
