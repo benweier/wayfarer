4271798ab664c954f72c1ca8b85239cac60ef76d
Ben Weier <ben.weier@gmail.com>
2024-02-26 18:54:29 +1000

    refactor meta tag reducer

--- src/components/meta/meta.component.tsx
+++ src/components/meta/meta.component.tsx
@@ -1,5 +1,5 @@
-import { getRenderedMatches, useRouterState } from '@tanstack/react-router'
-import { Fragment } from 'react'
+import { useRouterState } from '@tanstack/react-router'
+import { Fragment, type ReactNode } from 'react'
 import { useTranslation } from 'react-i18next'
 import { type MetaProps } from './meta.types'
 
@@ -7,46 +7,41 @@ export const Meta = ({ titleTemplate = '%s' }: MetaProps) => {
   const { t } = useTranslation()
   const metas = useRouterState({
     select: (state) => {
-      return getRenderedMatches(state).reduce<Array<{ id: string; meta: MetaObject[] }>>((metas, match) => {
+      const metas = state.matches.reduce<Map<string, ReactNode>>((metas, match) => {
         if (
           typeof match.routeContext === 'object' &&
           'meta' in match.routeContext &&
           typeof match.routeContext.meta === 'function'
         ) {
-          metas.push({
-            id: match.id,
-            meta: match.routeContext.meta(t, match.loaderData as any),
+          const meta = match.routeContext.meta(t, match.loaderData as any)
+
+          meta.forEach((tag) => {
+            if ('title' in tag) {
+              metas.set('title', <title>{`${titleTemplate.replace(/%s/g, () => tag.title)}`}</title>)
+            }
+
+            if ('name' in tag) {
+              metas.set(tag.name, <meta name={tag.name} content={tag.content} />)
+            }
+
+            if ('property' in tag) {
+              metas.set(tag.property, <meta property={tag.property} content={tag.content} />)
+            }
           })
         }
 
         return metas
-      }, [])
+      }, new Map())
+
+      return Array.from(metas.entries())
     },
   })
 
   return (
     <>
-      {metas.map(({ id, meta }) => {
-        return (
-          <Fragment key={id}>
-            {meta.map((tag) => {
-              if ('title' in tag) {
-                return <title key={tag.title}>{`${titleTemplate.replace(/%s/g, () => tag.title)}`}</title>
-              }
-
-              if ('name' in tag) {
-                return <meta key={`${tag.name}|${tag.content}`} name={tag.name} content={tag.content} />
-              }
-
-              if ('property' in tag) {
-                return <meta key={`${tag.property}|${tag.content}`} property={tag.property} content={tag.content} />
-              }
-
-              return <Fragment key={tag} />
-            })}
-          </Fragment>
-        )
-      })}
+      {metas.map(([key, tag]) => (
+        <Fragment key={key}>{tag}</Fragment>
+      ))}
     </>
   )
 }
