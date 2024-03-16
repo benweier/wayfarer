159e51bcb508eb1f70c7986609c9b291da704cf6
Ben Weier <ben.weier@gmail.com>
2024-03-16 11:55:16 +1000

    remove use callback

--- src/components/toast/toast.container.tsx
+++ src/components/toast/toast.container.tsx
@@ -1,20 +1,20 @@
-import { Fragment, useCallback } from 'react'
+import { Fragment } from 'react'
 import { ToastElement } from './toast.element'
 import { useToastStore } from './toast.store'
 
 export const ToastContainer = () => {
-  const toasts = useToastStore(useCallback((state) => Array.from(state.toasts.entries()), []))
+  const toasts = useToastStore((state) => Array.from(state.toasts.entries()))
 
   return (
     <div
       id="toast-container"
-      className="pointer-events-none fixed bottom-0 right-0 z-[99999] flex max-h-full w-full flex-col items-center justify-end gap-2 overflow-hidden p-5"
+      className="pointer-events-none fixed right-0 bottom-0 z-[99999] flex max-h-full w-full flex-col items-center justify-end gap-2 overflow-hidden p-5"
     >
       <ol>
         {toasts.map(([id, toast]) => {
           return (
             <Fragment key={id}>
-              <li role="status">
+              <li>
                 <ToastElement id={id} toast={toast} />
               </li>
             </Fragment>
