efd135e80d6d065fdae16ca570f0d29597ffc5da
Ben Weier <ben.weier@gmail.com>
2024-03-31 20:47:55 +1000

    update use modal context hook default arguments

--- src/components/modal/use-modal-store.hook.ts
+++ src/components/modal/use-modal-store.hook.ts
@@ -1,9 +1,9 @@
-import { useCallback, useContext } from 'react'
+import { useContext } from 'react'
 import { useStore } from 'zustand'
 import { ModalContext } from './modal.context'
 import { type ModalStore } from './modal.types'
 
-export const useModalContext = <T>(selector: (state: ModalStore) => T): T => {
+export const useModalContext = <T = ModalStore>(selector: (state: ModalStore) => T = (state) => state as T): T => {
   const store = useContext(ModalContext)
 
   if (!store) throw new Error('`useModalContext` must be used within a `Modal` Provider')
@@ -12,5 +12,5 @@ export const useModalContext = <T>(selector: (state: ModalStore) => T): T => {
 }
 
 export const useModalActions = () => {
-  return useModalContext(useCallback((state) => state.actions, []))
+  return useModalContext((state) => state.actions)
 }
