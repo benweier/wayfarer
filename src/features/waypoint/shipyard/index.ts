713d5f57a493bf89bf08c86a308698f3f07dc012
Ben Weier <ben.weier@gmail.com>
2024-04-03 19:54:29 +1000

    remove unused file

--- src/features/waypoint/shipyard/index.ts
+++ src/features/waypoint/shipyard/index.ts
@@ -1,5 +1,4 @@
 export { WaypointShipyardList } from './waypoint-shipyard-list.component'
-export { WaypointShipyardItem } from './waypoint-shipyard-item.component'
 export { WaypointShipyardFallback } from './waypoint-shipyard.fallback'
 export { WaypointShipyardError } from './waypoint-shipyard.err'
 export { WaypointShipyardNotAvailable } from './waypoint-shipyard.not-available'
