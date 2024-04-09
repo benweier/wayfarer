338c9ac79f0af1c0386620908dcc2b1e37e7729f
Ben Weier <ben.weier@gmail.com>
2024-04-09 18:56:16 +1000

    fetch all pages of factions

--- src/services/api/spacetraders/factions.ts
+++ src/services/api/spacetraders/factions.ts
@@ -2,16 +2,35 @@ import { queryOptions } from '@tanstack/react-query'
 import { type Meta, type SpaceTradersResponse, createHeaders } from '@/services/api/spacetraders/core'
 import { get } from '@/services/fetch'
 import { type FactionResponse } from '@/types/spacetraders'
+import { getPageList } from '@/utilities/get-page-list.helper'
 
 export const getFactionListQuery = () =>
   queryOptions({
+    staleTime: Infinity,
+    gcTime: Infinity,
     queryKey: [{ scope: 'factions', entity: 'list' }],
     queryFn: async ({ signal }) => {
       const url = new URL('factions', import.meta.env.SPACETRADERS_API_BASE_URL)
 
+      url.searchParams.set('page', '1')
       url.searchParams.set('limit', '20')
 
-      return get<SpaceTradersResponse<FactionResponse[], Meta>>(url, { signal, headers: createHeaders() })
+      const initial = await get<SpaceTradersResponse<FactionResponse[], Meta>>(url, {
+        signal,
+        headers: createHeaders(),
+      })
+      const pages = getPageList(Math.ceil(initial.meta.total / initial.meta.limit), 1)
+      const remaining = await Promise.all(
+        pages.map((page) => {
+          url.searchParams.set('page', String(page))
+
+          return get<SpaceTradersResponse<FactionResponse[], Meta>>(url, { signal, headers: createHeaders() })
+        }),
+      )
+      const data = initial.data.concat(...remaining.map((page) => page.data))
+      const meta = { page: 1, total: data.length, limit: data.length }
+
+      return { data, meta }
     },
   })
 
