e0f277961fddb69d1fda2c301562e6daff11ba89
Ben Weier <ben.weier@gmail.com>
2024-04-11 18:23:16 +1000

    use inline flex

--- src/components/badge/badge.component.tsx
+++ src/components/badge/badge.component.tsx
@@ -2,7 +2,7 @@ import { type PropsWithChildren } from 'react'
 
 export const Badge = ({ children }: PropsWithChildren) => {
   return (
-    <div className="text-background-secondary bg-foreground-secondary typography-xs inline-block rounded-sm px-2.5 font-bold">
+    <div className="text-background-secondary bg-foreground-secondary typography-xs inline-flex rounded-sm px-2.5 font-bold">
       {children}
     </div>
   )
