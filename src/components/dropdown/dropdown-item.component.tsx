bb0bc1dcb62f39711a0d8c1212c3759312b60050
Ben Weier <ben.weier@gmail.com>
2024-04-14 15:10:01 +1000

    update item classes to support icons

--- src/components/dropdown/dropdown-item.component.tsx
+++ src/components/dropdown/dropdown-item.component.tsx
@@ -9,7 +9,7 @@ const DropdownItem = (
     <DropdownMenu.Item
       ref={ref}
       {...props}
-      className="group text-foreground-secondary data-[disabled]:text-foreground-disabled data-[highlighted]:bg-background-tertiary data-[highlighted]:text-foreground-primary typography-sm relative flex items-center rounded-md py-2 pr-16 pl-8 outline-none select-none data-[disabled]:pointer-events-none"
+      className="group text-foreground-secondary data-[disabled]:text-foreground-disabled data-[highlighted]:bg-background-tertiary data-[highlighted]:text-foreground-primary typography-sm relative flex flex items-center items-center gap-2 rounded-md py-2 pr-16 pl-8 outline-none select-none data-[disabled]:pointer-events-none"
     >
       {children}
     </DropdownMenu.Item>
