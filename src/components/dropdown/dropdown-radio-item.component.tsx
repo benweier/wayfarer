6a340f12b38c0211230be04ba3ed0a4986211f8f
Ben Weier <ben.weier@gmail.com>
2024-04-14 15:12:45 +1000

    use item icon component for radio and checkbox items

--- src/components/dropdown/dropdown-radio-item.component.tsx
+++ src/components/dropdown/dropdown-radio-item.component.tsx
@@ -1,6 +1,7 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import { type ForwardedRef, forwardRef } from 'react'
 import { AppIcon } from '@/components/icons'
+import { ItemIcon } from './dropdown-item-icon.component'
 
 const RadioItemComponent = (
   { children, ...props }: DropdownMenu.DropdownMenuRadioItemProps,
@@ -12,8 +13,10 @@ const RadioItemComponent = (
       {...props}
       className="group text-foreground-secondary data-[disabled]:text-foreground-disabled data-[highlighted]:bg-background-secondary data-[highlighted]:text-foreground-primary typography-sm relative flex items-center rounded-sm py-2 pr-16 pl-8 select-none data-[disabled]:pointer-events-none data-[highlighted]:outline-none"
     >
-      <DropdownMenu.ItemIndicator className="absolute inset-y-0 left-1 inline-flex w-6 items-center justify-center">
-        <AppIcon id="dot" aria-hidden="true" className="text-foreground-success-primary size-4" />
+      <DropdownMenu.ItemIndicator asChild>
+        <ItemIcon>
+          <AppIcon id="dot" aria-hidden="true" className="text-foreground-success-primary size-4" />
+        </ItemIcon>
       </DropdownMenu.ItemIndicator>
 
       {children}
