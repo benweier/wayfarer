01dc811ae9ea46ab11959f9d74523ddacc289044
Ben Weier <ben.weier@gmail.com>
2024-04-14 15:21:44 +1000

    update dropdown components

--- src/features/ship/cargo/ship-cargo-item.controls.tsx
+++ src/features/ship/cargo/ship-cargo-item.controls.tsx
@@ -1,5 +1,5 @@
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import { useTranslation } from 'react-i18next'
+import * as Dropdown from '@/components/dropdown'
 import { AppIcon } from '@/components/icons'
 import { Modal, useModalImperativeHandle } from '@/components/modal'
 import { useShipResponse } from '@/context/ship.context'
@@ -15,45 +15,35 @@ export const ShipCargoItemControls = ({ item }: { item: CargoInventory }) => {
 
   return (
     <>
-      <DropdownMenu.Root>
-        <DropdownMenu.Trigger asChild>
+      <Dropdown.Field
+        trigger={
           <button className="btn btn-icon data-[state=open]:bg-blue-500 data-[state=open]:text-white">
             <span className="sr-only">Manage</span>
             <AppIcon id="more:vertical" className="size-4" aria-hidden="true" />
           </button>
-        </DropdownMenu.Trigger>
-
-        <DropdownMenu.Portal>
-          <DropdownMenu.Content
-            className="bg-background-secondary flex w-52 flex-col gap-2 overflow-y-auto rounded-md p-1 ring-3 ring-black/5 backdrop-blur-lg dark:ring-white/5"
-            sideOffset={5}
-            collisionPadding={16}
-          >
-            <DropdownMenu.Item asChild>
-              <button
-                className="btn btn-flat btn-sm data-[highlighted]:btn-dim w-full text-left"
-                onClick={() => {
-                  transfer.modal.open()
-                }}
-              >
-                {t('ship.action.transfer')}
-              </button>
-            </DropdownMenu.Item>
-            <DropdownMenu.Item asChild>
-              <button
-                className="btn btn-flat btn-sm data-[highlighted]:btn-dim w-full text-left"
-                onClick={() => {
-                  jettison.modal.open()
-                }}
-              >
-                {t('ship.action.jettison')}
-              </button>
-            </DropdownMenu.Item>
-
-            <DropdownMenu.Arrow />
-          </DropdownMenu.Content>
-        </DropdownMenu.Portal>
-      </DropdownMenu.Root>
+        }
+      >
+        <Dropdown.Item
+          onSelect={() => {
+            transfer.modal.open()
+          }}
+        >
+          <Dropdown.ItemIcon>
+            <AppIcon id="transfer" className="size-4" aria-hidden="true" />
+          </Dropdown.ItemIcon>
+          {t('ship.action.transfer')}
+        </Dropdown.Item>
+        <Dropdown.Item
+          onSelect={() => {
+            jettison.modal.open()
+          }}
+        >
+          <Dropdown.ItemIcon>
+            <AppIcon id="trash" className="size-4" aria-hidden="true" />
+          </Dropdown.ItemIcon>
+          {t('ship.action.jettison')}
+        </Dropdown.Item>
+      </Dropdown.Field>
 
       <Modal ref={transfer.ref} size="sm" closeable>
         <CargoTransfer ship={ship} item={item} />
