cbe894b226e0b2ba119286425eb01956dc33085a
Ben Weier <ben.weier@gmail.com>
2024-04-20 16:34:10 +1000

    update toast exports

--- src/components/toast/index.ts
+++ src/components/toast/index.ts
@@ -1,4 +1,4 @@
 export { ToastContainer } from './toast-container.component'
 export { ToastItem } from './toast-item.component'
-export { useToastStore } from './toast.store'
+export { useToastState, useToastActions } from './toast.store'
 export type { Toast, ToastStore } from './toast.types'
