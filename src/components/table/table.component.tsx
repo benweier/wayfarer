48819aeffc7df7e1433cf5c4d39ceb9bbd4ba9cf
Ben Weier <ben.weier@gmail.com>
2024-04-27 12:35:50 +1000

    add scroll area to table component

--- src/components/table/table.component.tsx
+++ src/components/table/table.component.tsx
@@ -1,3 +1,4 @@
+import * as ScrollArea from '@/components/scroll-area'
 import { flexRender } from '@tanstack/react-table'
 import { useTranslation } from 'react-i18next'
 import { Row as RowComponent } from './row.component'
@@ -12,40 +13,49 @@ export const Table = <T extends Record<string, any> = Record<string, never>>({
   const rows = table.getSortedRowModel().rows
 
   return (
-    <div className="w-full max-w-full overflow-x-auto overflow-y-hidden">
-      <table className="divide-border-primary w-full divide-y overflow-hidden rounded-xl">
-        <thead className="bg-background-tertiary">
-          {groups.map((group) => (
-            <tr key={group.id}>
-              {group.headers.map((header) => {
-                const sorted = header.column.getIsSorted()
+    <ScrollArea.Root width="100%">
+      <ScrollArea.Viewport>
+        <div className="overflow-hidden rounded-lg">
+          <table className="divide-border-primary w-full divide-y">
+            <thead className="bg-background-tertiary">
+              {groups.map((group) => (
+                <tr key={group.id}>
+                  {group.headers.map((header) => {
+                    const sorted = header.column.getIsSorted()
 
-                return (
-                  <th
-                    key={header.id}
-                    className="text-foreground-primary typography-sm py-3.5 px-3 font-semibold"
-                    style={{ width: `${header.getSize()}%` }}
-                    aria-sort={
-                      sorted
-                        ? sorted === 'asc'
-                          ? (t('general.sorting.asc') as 'ascending')
-                          : (t('general.sorting.desc') as 'descending')
-                        : undefined
-                    }
-                  >
-                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                  </th>
-                )
-              })}
-            </tr>
-          ))}
-        </thead>
-        <tbody className="bg-background-secondary divide-border-secondary divide-y">
-          {rows.map((row) => (
-            <Row key={row.id} row={row} />
-          ))}
-        </tbody>
-      </table>
-    </div>
+                    return (
+                      <th
+                        key={header.id}
+                        className="text-foreground-primary typography-sm py-3.5 px-3 font-semibold"
+                        style={{ width: `${header.getSize()}%` }}
+                        aria-sort={
+                          sorted
+                            ? sorted === 'asc'
+                              ? (t('general.sorting.asc') as 'ascending')
+                              : (t('general.sorting.desc') as 'descending')
+                            : undefined
+                        }
+                      >
+                        {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                      </th>
+                    )
+                  })}
+                </tr>
+              ))}
+            </thead>
+
+            {rows.length > 0 && (
+              <tbody className="bg-background-secondary divide-border-secondary divide-y">
+                {rows.map((row) => (
+                  <Row key={row.id} row={row} />
+                ))}
+              </tbody>
+            )}
+          </table>
+        </div>
+      </ScrollArea.Viewport>
+
+      <ScrollArea.Scrollbar orientation="horizontal" />
+    </ScrollArea.Root>
   )
 }
