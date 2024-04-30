02c783ca08e1a64319fa94fa98e625618127d6f0
Ben Weier <ben.weier@gmail.com>
2024-04-30 17:02:38 +1000

    update drawer content styles

--- src/components/drawer/drawer-content.component.tsx
+++ src/components/drawer/drawer-content.component.tsx
@@ -8,11 +8,12 @@ export const Content = ({
   height = 'auto',
   width = 'auto',
   children,
-}: PropsWithChildren<{
-  className?: string
-  height?: CSSProperties['height']
-  width?: CSSProperties['width']
-}>) => {
+}: PropsWithChildren<
+  {
+    height?: CSSProperties['height']
+    width?: CSSProperties['width']
+  } & WithClassName
+>) => {
   return (
     <Drawer.Content
       style={{
@@ -20,12 +21,12 @@ export const Content = ({
         '--drawer-max-width': typeof width === 'number' ? `${width}px` : width,
       }}
       className={cx(
-        'drawer bg-background-primary max-h-[var(--drawer-max-height)] max-w-[var(--drawer-max-width)] z-50 fixed border-1 border-border-primary',
+        'drawer bg-background-primary/90 backdrop-blur-lg max-h-[var(--drawer-max-height)] max-w-[var(--drawer-max-width)] z-50 fixed ring-3 ring-border-primary/20',
         className,
       )}
     >
       <ScrollArea.Root height={height} width={width}>
-        <ScrollArea.Viewport className="p-6 mx-auto">{children}</ScrollArea.Viewport>
+        <ScrollArea.Viewport className="flex items-stretch justify-stretch mx-auto">{children}</ScrollArea.Viewport>
         <ScrollArea.Scrollbar orientation="vertical" />
       </ScrollArea.Root>
     </Drawer.Content>
