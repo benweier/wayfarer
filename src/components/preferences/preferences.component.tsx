dba2c1ef32d79d21b0b0ab09407b10080557c7a5
Ben Weier <ben.weier@gmail.com>
2024-04-30 17:06:49 +1000

    use new popover component

--- src/components/preferences/preferences.component.tsx
+++ src/components/preferences/preferences.component.tsx
@@ -1,8 +1,6 @@
 import { MenuIcon } from '@/components/icons'
-import * as Popover from '@radix-ui/react-popover'
-import { cx } from 'class-variance-authority'
+import { Popover } from '@/components/popover'
 import { useTranslation } from 'react-i18next'
-import styles from './preferences.module.css'
 import { Sidebar } from './sidebar.component'
 import { Theme } from './theme.component'
 
@@ -10,32 +8,22 @@ export const Preferences = () => {
   const { t } = useTranslation()
 
   return (
-    <Popover.Root>
-      <Popover.Trigger className="group flex w-full items-center gap-4 overflow-hidden rounded-md py-2 px-3 font-medium text-white transition-all duration-100 hover:bg-blue-50/10 hover:shadow-sm">
-        <div className="size-6 text-blue-100">
-          <MenuIcon id="settings" className="size-6 group-data-[state=open]:animate-spin" aria-hidden />
-        </div>
-        <span className="typography-sm sr-only flex flex-col items-start justify-start @[220px]/side:not-sr-only">
-          {t('preferences.label')}
-        </span>
-      </Popover.Trigger>
-
-      <Popover.Portal>
-        <Popover.Content
-          side="top"
-          align="start"
-          className={cx(
-            styles.content,
-            'bg-background-primary/80 ring-border-primary/20 relative z-50 overflow-y-auto rounded-md p-6 ring-3 backdrop-blur-lg will-change-[transform,opacity]',
-          )}
-          sideOffset={5}
-        >
-          <div className="flex flex-col gap-4">
-            <Theme />
-            <Sidebar />
+    <Popover
+      trigger={
+        <button className="group flex w-full items-center gap-4 overflow-hidden rounded-md py-2 px-3 font-medium text-white transition-all duration-100 hover:bg-blue-50/10 hover:shadow-sm">
+          <div className="size-6 text-blue-100">
+            <MenuIcon id="settings" className="size-6 group-data-[state=open]:animate-spin" aria-hidden />
           </div>
-        </Popover.Content>
-      </Popover.Portal>
-    </Popover.Root>
+          <span className="typography-sm sr-only flex flex-col items-start justify-start @[220px]/side:not-sr-only">
+            {t('preferences.label')}
+          </span>
+        </button>
+      }
+    >
+      <div className="flex flex-col gap-4">
+        <Theme />
+        <Sidebar />
+      </div>
+    </Popover>
   )
 }
