898a9b80c0283b9352c8ccb96f20cbccda107f3c
Ben Weier <ben.weier@gmail.com>
2024-04-30 17:06:04 +1000

    merge class name prop

--- src/components/drawer/drawer-title.component.tsx
+++ src/components/drawer/drawer-title.component.tsx
@@ -1,9 +1,10 @@
 import type { DialogTitleProps } from '@radix-ui/react-dialog'
+import { cx } from 'class-variance-authority'
 import { Drawer } from 'vaul'
 
-export const Title = ({ children, ...props }: DialogTitleProps) => {
+export const Title = ({ className, children, ...props }: DialogTitleProps) => {
   return (
-    <Drawer.Title {...props} className="display-xs font-semibold">
+    <Drawer.Title {...props} className={cx('display-xs font-semibold', className)}>
       {children}
     </Drawer.Title>
   )
