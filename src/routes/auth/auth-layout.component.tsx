798e11d2cb934bb372c3a4286df8aa1c1d528014
Ben Weier <ben.weier@gmail.com>
2024-05-06 12:54:17 +1000

    add kbar actions to auth layout

--- src/routes/auth/auth-layout.component.tsx
+++ src/routes/auth/auth-layout.component.tsx
@@ -1,8 +1,36 @@
 import { Layout } from '@/features/auth'
-import { Outlet } from '@tanstack/react-router'
-import { Suspense } from 'react'
+import { Outlet, useNavigate } from '@tanstack/react-router'
+import { type Action, useRegisterActions } from 'kbar'
+import { Suspense, useMemo } from 'react'
 
 export const AuthLayout = () => {
+  const navigate = useNavigate()
+
+  const actions = useMemo<Action[]>(() => {
+    return [
+      {
+        id: 'login',
+        name: 'Login',
+        shortcut: ['Alt+KeyL'],
+        keywords: 'login',
+        perform: () => {
+          void navigate({ to: '/login' })
+        },
+      },
+      {
+        id: 'register',
+        name: 'Register',
+        shortcut: ['Alt+KeyR'],
+        keywords: 'register',
+        perform: () => {
+          void navigate({ to: '/register' })
+        },
+      },
+    ]
+  }, [navigate])
+
+  useRegisterActions(actions)
+
   return (
     <Layout>
       <Suspense fallback={null}>
