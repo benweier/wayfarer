ba8ec2ee4943809b0d0065ed7f49ca709011eeb6
Ben Weier <ben.weier@gmail.com>
2024-05-06 12:52:06 +1000

    update loading border radius

--- src/components/loading/loading.component.tsx
+++ src/components/loading/loading.component.tsx
@@ -1,3 +1,3 @@
 export const Loading = () => {
-  return <div className="bg-background-tertiary h-12 animate-pulse rounded"></div>
+  return <div className="bg-background-tertiary h-12 animate-pulse rounded-md" />
 }
