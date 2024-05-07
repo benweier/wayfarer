9b468665935e582c587cb252808a42690740075c
Ben Weier <ben.weier@gmail.com>
2024-05-07 18:46:47 +1000

    update kbar styles and config

--- src/components/kbar/kbar-results.component.tsx
+++ src/components/kbar/kbar-results.component.tsx
@@ -11,25 +11,29 @@ export function Results() {
         typeof item === 'string' ? (
           <div
             data-highlighted={active}
-            className="group text-foreground-secondary data-[disabled]:text-foreground-disabled data-[highlighted=true]:bg-background-tertiary data-[highlighted=true]:text-foreground-primary typography-sm relative flex items-center gap-2 rounded-md py-2 pr-16 pl-8 outline-none select-none data-[disabled]:pointer-events-none"
+            className="group flex items-center text-foreground-secondary typography-sm my-2 px-4"
           >
             {item}
           </div>
         ) : (
           <div
             data-highlighted={active}
-            className="group flex flex-row justify-between text-foreground-secondary data-[disabled]:text-foreground-disabled data-[highlighted=true]:bg-background-tertiary data-[highlighted=true]:text-foreground-primary typography-sm relative flex items-center gap-2 rounded-md py-3 px-6 outline-none select-none data-[disabled]:pointer-events-none"
+            className="group text-foreground-secondary data-[disabled]:text-foreground-disabled data-[highlighted=true]:bg-background-secondary data-[highlighted=true]:text-foreground-primary typography-base relative py-3 px-6 outline-none select-none data-[disabled]:pointer-events-none"
           >
-            {item.name}
-            {item.shortcut && (
-              <div dir="ltr">
-                {item.shortcut.map((shortcut, index) => {
-                  // Shortcut keys may be repeated, so aad the index to be unique
-                  const key = `${shortcut}-${index}`
-                  return <Kbd key={key}>{shortcut}</Kbd>
-                })}
+            <div className="flex justify-between gap-8">
+              <div className="flex grow justify-between">
+                <div>{item.name}</div> <div>{item.children.length > 0 && '>'}</div>
               </div>
-            )}
+              {item.shortcut && (
+                <div dir="ltr" className="shrink">
+                  {item.shortcut.map((shortcut, index) => {
+                    // Shortcut keys may be repeated, so aad the index to be unique
+                    const key = `${shortcut}-${index}`
+                    return <Kbd key={key}>{shortcut}</Kbd>
+                  })}
+                </div>
+              )}
+            </div>
           </div>
         )
       }
--- src/components/kbar/kbar.component.tsx
+++ src/components/kbar/kbar.component.tsx
@@ -4,15 +4,23 @@ import { Results } from './kbar-results.component'
 
 export const KBar = ({ children }: PropsWithChildren) => {
   return (
-    <KBarProvider>
+    <KBarProvider
+      options={{
+        disableDocumentLock: true,
+        disableScrollbarManagement: true,
+        toggleShortcut: '/',
+      }}
+    >
       {children}
 
       <KBarPortal>
         <KBarPositioner>
-          <div className="w-full max-w-[480px] p-4">
+          <div className="w-full max-w-[520px] p-4">
             <KBarAnimator>
-              <div className="flex flex-col gap-6 rounded-lg border border-border-primary bg-background-primary p-4 ring-3 ring-border-primary/30">
-                <KBarSearch className="input" />
+              <div className="flex flex-col rounded-lg border border-border-primary bg-background-primary ring-3 ring-border-primary/30 overflow-hidden shadow-[0_0_40px_-15px_var(--color-border-secondary)]">
+                <div className="p-4">
+                  <KBarSearch className="input" />
+                </div>
 
                 <Results />
               </div>
