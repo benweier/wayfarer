90e367cc517168a0b9fae99b77747f32ac52631d
Ben Weier <ben.weier@gmail.com>
2024-05-07 20:36:15 +1000

    create fleet actions hook

--- src/routes/fleet/fleet-route.component.tsx
+++ src/routes/fleet/fleet-route.component.tsx
@@ -1,9 +1,12 @@
 import { QuerySuspenseBoundary } from '@/components/query-suspense-boundary'
 import { ShipList, ShipListFallback } from '@/features/ship/list'
 import { useTranslation } from 'react-i18next'
+import { useFleetCommands } from './use-fleet-commands.hook'
 
 export const FleetRoute = () => {
   const { t } = useTranslation()
+
+  useFleetCommands()
 
   return (
     <div className="space-y-4 p-4">
---
+++ src/routes/fleet/use-fleet-commands.hook.ts
@@ -0,0 +1,38 @@
+import { getShipListQuery } from '@/services/api/spacetraders/fleet'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { useNavigate } from '@tanstack/react-router'
+import { type Action, Priority, useRegisterActions } from 'kbar'
+import { useMemo } from 'react'
+
+export const useFleetCommands = () => {
+  const navigate = useNavigate()
+  const { data } = useSuspenseQuery(getShipListQuery())
+  const ships = data.data
+
+  const actions = useMemo<Action[]>(() => {
+    return [
+      {
+        id: 'ships',
+        name: 'Ships',
+        shortcut: ['Alt+KeyS'],
+        priority: Priority.HIGH,
+      },
+      ...ships.map((ship) => {
+        return {
+          id: ship.symbol,
+          name: ship.symbol,
+          parent: 'ships',
+          keywords: ship.symbol,
+          perform: () => {
+            void navigate({
+              to: '/fleet/$shipSymbol',
+              params: { shipSymbol: ship.symbol },
+            })
+          },
+        }
+      }),
+    ]
+  }, [navigate, ships])
+
+  useRegisterActions(actions)
+}
