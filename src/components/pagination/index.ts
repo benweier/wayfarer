b7e45d631876bebf20477c2c606d489d8acfc99e
Ben Weier <ben.weier@gmail.com>
2024-05-12 22:42:48 +1000

    create pagination commands hook

--- src/components/pagination/index.ts
+++ src/components/pagination/index.ts
@@ -1,2 +1,3 @@
 export { Pagination } from './pagination.component'
+export { usePaginationCommands } from './use-pagination-commands.hook'
 export type { PaginationProps } from './pagination.types'
---
+++ src/components/pagination/use-pagination-commands.hook.ts
@@ -0,0 +1,44 @@
+import { useNavigate } from '@tanstack/react-router'
+import { type Action, useRegisterActions } from 'kbar'
+import { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
+
+export const usePaginationCommands = ({ min, max }: { min: number; max: number }) => {
+  const { t } = useTranslation()
+  const navigate = useNavigate()
+
+  const actions = useMemo<Action[]>(() => {
+    return [
+      {
+        id: 'previous-page',
+        name: t('general.previous_page'),
+        shortcut: ['Alt+ArrowLeft'],
+        perform: () => {
+          void navigate({
+            search: ({ page = 1 }) => {
+              if (page <= min) return { page: min }
+
+              return { page: page - 1 }
+            },
+          })
+        },
+      },
+      {
+        id: 'next-page',
+        name: t('general.next_page'),
+        shortcut: ['Alt+ArrowRight'],
+        perform: () => {
+          void navigate({
+            search: ({ page = 1 }) => {
+              if (page >= max) return { page: max }
+
+              return { page: page + 1 }
+            },
+          })
+        },
+      },
+    ]
+  }, [navigate, t, min, max])
+
+  useRegisterActions(actions)
+}
