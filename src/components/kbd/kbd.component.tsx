449ff6d386f41ac6fa72a80b2669da35f2b1d58a
Ben Weier <ben.weier@gmail.com>
2024-05-12 22:40:09 +1000

    add arrow key mapping

--- src/components/kbd/kbd.component.tsx
+++ src/components/kbd/kbd.component.tsx
@@ -12,9 +12,18 @@ const Win: Record<string, string> = {
   Meta: 'Win',
 }
 
+const Arrows: Record<string, string> = {
+  ArrowUp: '↑',
+  ArrowDown: '↓',
+  ArrowLeft: '←',
+  ArrowRight: '→',
+}
+
 // Get OS specific shortcut keys
 const getShortcutKey = (key: string) => {
-  const shortcut = key.replace(/(Key|\+)/g, '')
+  const shortcut = key
+    .replace(/(Key|\+)/g, '')
+    .replace(/(ArrowUp|ArrowDown|ArrowLeft|ArrowRight)/g, (_, k) => Arrows[k])
 
   if (navigator.userAgent.includes('Mac')) {
     return shortcut.replace(/(Alt|Ctrl|Shift|Meta)/g, (_, k) => Mac[k])
@@ -25,7 +34,7 @@ const getShortcutKey = (key: string) => {
 
 export const Kbd = ({ children }: { children: string }) => {
   return (
-    <kbd className="bg-background-tertiary typography-sm font-family-[unset] text-foreground-secondary border-1 border-b-3 border-border-primary rounded-sm px-1 mx-0.5">
+    <kbd className="bg-background-tertiary typography-sm font-family-[unset] text-foreground-secondary tracking-widest border-1 border-b-3 border-border-primary rounded-sm px-1 mx-0.5">
       {getShortcutKey(children)}
     </kbd>
   )
