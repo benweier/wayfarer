d9816d48d951d9aff674cf95f5794708a0a433bf
Ben Weier <ben.weier@gmail.com>
2024-05-12 22:41:33 +1000

    remove max page check effect

--- src/features/system/list/system-list.component.tsx
+++ src/features/system/list/system-list.component.tsx
@@ -4,22 +4,14 @@ import { getShipPresence } from '@/features/ship/utilities/get-ship-presence.hel
 import { getSystemListQuery } from '@/services/api/spacetraders/systems'
 import { formatNumber } from '@/utilities/number.helper'
 import { useSuspenseQuery } from '@tanstack/react-query'
-import { useEffect } from 'react'
 import { SystemListTable } from './system-list.table'
 import type { SystemListProps } from './system-list.types'
 
 export const SystemList = ({ page = 1, limit = 20, setPage }: SystemListProps) => {
   const systemsListQuery = useSuspenseQuery(getSystemListQuery({ page, limit }))
   const ships = useFleetResponse()
-
-  useEffect(() => {
-    const max = Math.ceil(systemsListQuery.data.meta.total / limit)
-
-    if (page > max) setPage(max)
-  }, [limit, systemsListQuery.data.meta, page, setPage])
-
-  const systems = systemsListQuery.data.data
   const presence = getShipPresence(ships, 'systemSymbol')
+  const systems = systemsListQuery.data.data
   const meta = systemsListQuery.data.meta
   const results = {
     from: formatNumber(page * limit + 1 - limit),
