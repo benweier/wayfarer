4549b07c42ac7deb7f80111706acf4068ac13570
Ben Weier <ben.weier@gmail.com>
2024-05-19 12:31:07 +1000

    use index checks instead of value checks

--- src/features/waypoint/utilities/has-trait.helper.ts
+++ src/features/waypoint/utilities/has-trait.helper.ts
@@ -1,5 +1,6 @@
+import type { WaypointTraits } from '@/config/spacetraders'
 import type { WaypointTrait } from '@/types/spacetraders'
 
-export const hasTrait = (traits?: WaypointTrait[], symbol?: string[]) => {
-  return traits?.find((trait) => symbol?.includes(trait.symbol)) !== undefined
+export const hasTrait = (traits: WaypointTrait[] = [], symbol: WaypointTraits[] = []) => {
+  return traits?.findIndex((trait) => symbol.indexOf(trait.symbol) > -1) > -1
 }
