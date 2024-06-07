26f1822fc66514f322f13ae926d572c121df669f
Ben Weier <ben.weier@gmail.com>
2024-06-07 23:34:41 +1000

    validate sidebar and theme values, add localstorage interface to replace default json storage

--- src/store/atoms/sidebar.ts
+++ src/store/atoms/sidebar.ts
@@ -1,9 +1,40 @@
 import { atomWithStorage } from 'jotai/utils'
+import * as v from 'valibot'
 
-export type SidebarState = 'collapsed' | 'expanded'
+export type SidebarState = v.InferOutput<typeof SidebarSchema>
 
 const SIDEBAR_STORAGE_KEY = 'sidebar'
 
-export const sidebarAtom = atomWithStorage<SidebarState>(SIDEBAR_STORAGE_KEY, 'expanded', undefined, {
-  getOnInit: true,
-})
+const SidebarSchema = v.pipe(v.string(), v.union([v.literal('expanded'), v.literal('collapsed')]))
+
+export const sidebarAtom = atomWithStorage<SidebarState>(
+  SIDEBAR_STORAGE_KEY,
+  'expanded',
+  {
+    getItem(key, initialValue) {
+      return v.parse(v.fallback(SidebarSchema, initialValue), localStorage.getItem(key))
+    },
+    setItem(key, value) {
+      localStorage.setItem(key, value)
+    },
+    removeItem(key) {
+      localStorage.removeItem(key)
+    },
+    subscribe(key, callback, initialValue) {
+      const storageHandler = (e: StorageEvent) => {
+        if (e.storageArea === localStorage && e.key === key) {
+          callback(v.parse(v.fallback(SidebarSchema, initialValue), e.newValue))
+        }
+      }
+
+      window.addEventListener('storage', storageHandler)
+
+      return () => {
+        window.removeEventListener('storage', storageHandler)
+      }
+    },
+  },
+  {
+    getOnInit: true,
+  },
+)
--- src/store/atoms/theme.ts
+++ src/store/atoms/theme.ts
@@ -1,13 +1,39 @@
 import { atomWithStorage } from 'jotai/utils'
+import * as v from 'valibot'
 
-export type ThemeState = 'system' | 'light' | 'dark'
+export type ThemeState = v.InferOutput<typeof ThemeSchema>
 
 const THEME_STORAGE_KEY = 'theme'
 
+const ThemeSchema = v.pipe(v.string(), v.union([v.literal('system'), v.literal('light'), v.literal('dark')]))
+
 export const themeAtom = atomWithStorage<ThemeState>(
   THEME_STORAGE_KEY,
-  (window.localStorage.getItem(THEME_STORAGE_KEY) as ThemeState | null) ?? 'system',
-  undefined,
+  'system',
+  {
+    getItem(key, initialValue) {
+      return v.parse(v.fallback(ThemeSchema, initialValue), localStorage.getItem(key))
+    },
+    setItem(key, value) {
+      localStorage.setItem(key, value)
+    },
+    removeItem(key) {
+      localStorage.removeItem(key)
+    },
+    subscribe(key, callback, initialValue) {
+      const storageHandler = (e: StorageEvent) => {
+        if (e.storageArea === localStorage && e.key === key) {
+          callback(v.parse(v.fallback(ThemeSchema, initialValue), e.newValue))
+        }
+      }
+
+      window.addEventListener('storage', storageHandler)
+
+      return () => {
+        window.removeEventListener('storage', storageHandler)
+      }
+    },
+  },
   {
     getOnInit: true,
   },
