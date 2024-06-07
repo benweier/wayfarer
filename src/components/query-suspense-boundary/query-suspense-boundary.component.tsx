48014216fff6230ffa2e29db978d2c57f9b57e11
Ben Weier <ben.weier@gmail.com>
2024-06-07 23:32:50 +1000

    replace query error reset boundary hook with component

--- src/components/query-suspense-boundary/query-suspense-boundary.component.tsx
+++ src/components/query-suspense-boundary/query-suspense-boundary.component.tsx
@@ -1,5 +1,5 @@
 import { ErrorBoundary } from '@/components/error-boundary'
-import { useQueryErrorResetBoundary } from '@tanstack/react-query'
+import { QueryErrorResetBoundary } from '@tanstack/react-query'
 import { type PropsWithChildren, Suspense } from 'react'
 import type { QuerySuspenseBoundaryProps } from './query-suspense-boundary.types'
 
@@ -9,11 +9,13 @@ export const QuerySuspenseBoundary = ({
   fallback = <></>,
   children,
 }: PropsWithChildren<QuerySuspenseBoundaryProps>) => {
-  const { reset } = useQueryErrorResetBoundary()
-
   return (
-    <ErrorBoundary component={error} onReset={reset} onError={onError}>
-      <Suspense fallback={fallback}>{children}</Suspense>
-    </ErrorBoundary>
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary component={error} onReset={reset} onError={onError}>
+          <Suspense fallback={fallback}>{children}</Suspense>
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
   )
 }
