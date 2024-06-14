3c5b60ced4e4cc2176f1a4ec877876bad777ac0c
Ben Weier <ben.weier@gmail.com>
2024-06-14 20:03:02 +1000

    use url constructor for theme css files

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -98,12 +98,14 @@ export const App = () => {
 
 const ThemeStyleSheet = () => {
   const theme = useAtomValue(themeAtom)
+  const light = new URL('/css/light.css', import.meta.url)
+  const dark = new URL('/css/dark.css', import.meta.url)
 
   return (
     <PrefersColorScheme
       preference={theme}
-      light={<link rel="stylesheet" href="/css/light.css" fetchPriority="high" />}
-      dark={<link rel="stylesheet" href="/css/dark.css" fetchPriority="high" />}
+      light={<link rel="stylesheet" href={light.href} fetchPriority="high" />}
+      dark={<link rel="stylesheet" href={dark.href} fetchPriority="high" />}
     />
   )
 }
