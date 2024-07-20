2cea66d8016187e086188cdc5ebc4cc2b0bb79c4
Ben Weier <ben.weier@gmail.com>
2024-07-20 19:05:58 +1000

    pagination no longer needs router integration

--- src/components/pagination/pagination.spec.tsx
+++ src/components/pagination/pagination.spec.tsx
@@ -1,6 +1,4 @@
 import { render } from '@/test/utilities/render.helper'
-import { createTestRouter, renderWithTestRouter } from '@/test/utilities/router'
-import { RouterProvider } from '@tanstack/react-router'
 import { act } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
 import { Pagination } from './pagination.component'
@@ -24,8 +22,8 @@ describe('pagination.component', () => {
   test('numbered page buttons', async () => {
     const user = userEvent.setup()
     const handleChange = vi.fn()
-    const { getAllByLabelText, getByLabelText } = await act(() =>
-      renderWithTestRouter(<Pagination current={1} min={1} max={10} onChange={handleChange} />),
+    const { getAllByLabelText, getByLabelText } = render(
+      <Pagination current={1} min={1} max={10} onChange={handleChange} />,
     )
 
     expect(getAllByLabelText(/go to page/i)).toHaveLength(5)
@@ -35,7 +33,7 @@ describe('pagination.component', () => {
 
     expect(page1).toHaveAttribute('aria-current', 'page')
 
-    await user.click(page2)
+    await act(() => user.click(page2))
 
     expect(handleChange).toHaveBeenCalledWith(2)
   })
@@ -43,85 +41,65 @@ describe('pagination.component', () => {
   test('first page button', async () => {
     const user = userEvent.setup()
     const handleChange = vi.fn()
-    const router = await createTestRouter(<Pagination current={2} min={1} max={10} onChange={handleChange} />)
-    const { getByLabelText, rerender } = await act(() => render(<RouterProvider router={router} />))
+    const { getByLabelText, rerender } = render(<Pagination current={2} min={1} max={10} onChange={handleChange} />)
     const firstPage = getByLabelText(/first page/i)
 
-    await user.click(firstPage)
+    await act(() => user.click(firstPage))
 
     expect(handleChange).toHaveBeenCalledWith(1)
 
-    {
-      const router = await createTestRouter(<Pagination current={1} min={1} max={10} onChange={handleChange} />)
+    act(() => rerender(<Pagination current={1} min={1} max={10} onChange={handleChange} />))
 
-      act(() => rerender(<RouterProvider router={router} />))
-
-      expect(getByLabelText(/first page/i)).toBeDisabled()
-    }
+    expect(getByLabelText(/first page/i)).toBeDisabled()
   })
 
   test('previous page button', async () => {
     const user = userEvent.setup()
     const handleChange = vi.fn()
-    const router = await createTestRouter(<Pagination current={2} min={1} max={10} onChange={handleChange} />)
-    const { getByLabelText, rerender } = await act(() => render(<RouterProvider router={router} />))
+    const { getByLabelText, rerender } = render(<Pagination current={2} min={1} max={10} onChange={handleChange} />)
     const previousPage = getByLabelText(/previous page/i)
     expect(previousPage).toBeEnabled()
 
-    await user.click(previousPage)
+    await act(() => user.click(previousPage))
 
     expect(handleChange).toHaveBeenCalledWith(1)
 
-    {
-      const router = await createTestRouter(<Pagination current={1} min={1} max={10} onChange={handleChange} />)
+    act(() => rerender(<Pagination current={1} min={1} max={10} onChange={handleChange} />))
 
-      act(() => rerender(<RouterProvider router={router} />))
-
-      expect(getByLabelText(/previous page/i)).toBeDisabled()
-    }
+    expect(getByLabelText(/previous page/i)).toBeDisabled()
   })
 
   test('next page button', async () => {
     const user = userEvent.setup()
     const handleChange = vi.fn()
-    const router = await createTestRouter(<Pagination current={1} min={1} max={10} onChange={handleChange} />)
-    const { getByLabelText, rerender } = await act(() => render(<RouterProvider router={router} />))
+    const { getByLabelText, rerender } = render(<Pagination current={1} min={1} max={10} onChange={handleChange} />)
     const nextPage = getByLabelText(/next page/i)
 
     expect(nextPage).toBeEnabled()
 
-    await user.click(nextPage)
+    await act(() => user.click(nextPage))
 
     expect(handleChange).toHaveBeenCalledWith(2)
 
-    {
-      const router = await createTestRouter(<Pagination current={10} min={1} max={10} onChange={handleChange} />)
+    act(() => rerender(<Pagination current={10} min={1} max={10} onChange={handleChange} />))
 
-      act(() => rerender(<RouterProvider router={router} />))
-
-      expect(getByLabelText(/next page/i)).toBeDisabled()
-    }
+    expect(getByLabelText(/next page/i)).toBeDisabled()
   })
 
   test('last page button', async () => {
     const user = userEvent.setup()
     const handleChange = vi.fn()
-    const router = await createTestRouter(<Pagination current={9} min={1} max={10} onChange={handleChange} />)
-    const { getByLabelText, rerender } = await act(() => render(<RouterProvider router={router} />))
+    const { getByLabelText, rerender } = render(<Pagination current={9} min={1} max={10} onChange={handleChange} />)
     const lastPage = getByLabelText(/last page/i)
 
     expect(lastPage).toBeEnabled()
 
-    await user.click(lastPage)
+    await act(() => user.click(lastPage))
 
     expect(handleChange).toHaveBeenCalledWith(10)
 
-    {
-      const router = await createTestRouter(<Pagination current={10} min={1} max={10} onChange={handleChange} />)
+    act(() => rerender(<Pagination current={10} min={1} max={10} onChange={handleChange} />))
 
-      act(() => rerender(<RouterProvider router={router} />))
-
-      expect(getByLabelText(/last page/i)).toBeDisabled()
-    }
+    expect(getByLabelText(/last page/i)).toBeDisabled()
   })
 })
