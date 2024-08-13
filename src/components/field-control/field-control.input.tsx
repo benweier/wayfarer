f07b2b97b11100992f0bf319fe535db3e0394341
Ben Weier <ben.weier@gmail.com>
2024-08-13 23:37:09 +1000

    add has error attribute to inputs

--- src/components/field-control/field-control.input.tsx
+++ src/components/field-control/field-control.input.tsx
@@ -15,6 +15,7 @@ export const Input = ({
       {...props}
       id={ctx?.field.name ?? id}
       className={cx('input', className)}
+      data-has-error={ctx?.fieldState?.error ? 'true' : 'false'}
       aria-labelledby={ctx?.field.name ? `${ctx.field.name}-label` : props['aria-labelledby']}
       aria-describedby={
         ctx?.field.name ? `${ctx.field.name}-description ${ctx.field.name}-error` : props['aria-describedby']
--- src/components/form-control/form-control.input.tsx
+++ src/components/form-control/form-control.input.tsx
@@ -1,5 +1,6 @@
 import { cx } from 'class-variance-authority'
 import { type InputHTMLAttributes, type RefAttributes, use } from 'react'
+import { useFormState } from 'react-hook-form'
 import { FormControlContext } from './form-control.component'
 
 export const Input = ({
@@ -7,6 +8,7 @@ export const Input = ({
   ...props
 }: InputHTMLAttributes<HTMLInputElement> & RefAttributes<HTMLInputElement>) => {
   const ctx = use(FormControlContext)
+  const { errors } = useFormState()
 
   return (
     <input
@@ -14,6 +16,7 @@ export const Input = ({
       {...props}
       id={ctx?.name}
       className={cx('input', className)}
+      data-has-error={ctx && errors[ctx.name] ? 'true' : 'false'}
       aria-labelledby={ctx?.name ? `${ctx.name}-label` : props['aria-labelledby']}
       aria-describedby={ctx?.name ? `${ctx.name}-description ${ctx.name}-error` : props['aria-describedby']}
     />
