f0cd2c7a69c2e9a0a0b08e30511259e2df87c00b
Ben Weier <ben.weier@gmail.com>
2024-08-19 18:48:31 +1000

    check existence of key in item

--- src/utilities/reduce-array-to-map.helper.ts
+++ src/utilities/reduce-array-to-map.helper.ts
@@ -1,10 +1,12 @@
 export const reduceArrayToMap = <T extends Record<string, any>>(
   arr: T[] = [],
-  key: keyof T = '',
+  key: keyof T = 'id',
   initial = new Map(),
 ) => {
   return arr.reduce<Map<string, T>>((result, item) => {
-    result.set(item[key], item)
+    if (key in item) {
+      result.set(item[key], item)
+    }
 
     return result
   }, initial)
