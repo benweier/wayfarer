e9f317f11a4f726d98d513faddc3e87906093073
Ben Weier <ben.weier@gmail.com>
2024-09-18 21:09:47 +1000

    add backdrop blur

--- src/components/modal/modal-footer.component.tsx
+++ src/components/modal/modal-footer.component.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react'
 
 export const Footer = ({ children }: PropsWithChildren) => {
   return (
-    <div className="-m-6 mt-6 flex flex-col gap-1 border-1 border-transparent border-t-border-tertiary bg-background-primary p-6 pt-5">
+    <div className="-m-6 mt-6 flex flex-col gap-1 border-1 border-transparent border-t-border-tertiary bg-background-primary/80 p-6 pt-5 backdrop-blur">
       {children}
     </div>
   )
--- src/components/modal/modal-header.component.tsx
+++ src/components/modal/modal-header.component.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react'
 
 export const Header = ({ children }: PropsWithChildren) => {
   return (
-    <div className="-m-6 mb-6 flex flex-col gap-1 border-1 border-transparent border-b-border-tertiary bg-background-primary p-6 pb-5">
+    <div className="-m-6 mb-6 flex flex-col gap-1 border-1 border-transparent border-b-border-tertiary bg-background-primary/80 p-6 pb-5 backdrop-blur">
       {children}
     </div>
   )
