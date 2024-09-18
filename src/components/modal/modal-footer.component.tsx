82eb79a925bec3c977c5f9c7a372a65bb710c5ae
Ben Weier <ben.weier@gmail.com>
2024-09-18 21:19:45 +1000

    remove transparent border

--- src/components/modal/modal-footer.component.tsx
+++ src/components/modal/modal-footer.component.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react'
 
 export const Footer = ({ children }: PropsWithChildren) => {
   return (
-    <div className="-m-6 mt-6 flex flex-col gap-1 border-1 border-transparent border-t-border-tertiary bg-background-primary/80 p-6 pt-5 backdrop-blur">
+    <div className="-m-6 mt-6 flex flex-col gap-1 border-t-1 border-t-border-tertiary bg-background-primary/80 p-6 pt-5 backdrop-blur">
       {children}
     </div>
   )
