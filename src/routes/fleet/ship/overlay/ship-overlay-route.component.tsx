a1a0b74d8667b3fc23a29f47a0c23ee0f19a6ec1
Ben Weier <ben.weier@gmail.com>
2024-09-22 12:05:05 +1000

    update ship modal close button

--- src/routes/fleet/ship/overlay/ship-overlay-route.component.tsx
+++ src/routes/fleet/ship/overlay/ship-overlay-route.component.tsx
@@ -1,12 +1,19 @@
+import { Button } from '@/components/button'
+import { AppIcon } from '@/components/icons'
 import { Modal } from '@/components/modal'
 import { ROUTES } from '@/config/routes'
+import { getShipByIdQuery } from '@/services/api/spacetraders/fleet'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { Outlet, getRouteApi, useNavigate } from '@tanstack/react-router'
+import { useTranslation } from 'react-i18next'
 
 const api = getRouteApi(ROUTES.SHIP)
 
 export const ShipOverlayRoute = () => {
+  const { t } = useTranslation()
   const navigate = useNavigate()
   const { shipSymbol } = api.useParams()
+  const { data } = useSuspenseQuery(getShipByIdQuery({ shipSymbol }))
 
   return (
     <Modal
@@ -18,7 +25,16 @@ export const ShipOverlayRoute = () => {
           onClick={() => {
             void navigate({ to: '/fleet/$shipSymbol', params: { shipSymbol } })
           }}
-        />
+        >
+          <Button
+            intent="neutral"
+            kind="outline"
+            size="small"
+            adornment={{ start: <AppIcon id="chevron:left" className="size-4" /> }}
+          >
+            {t('ship.market.back_to_ship', { shipSymbol: data.data.symbol })}
+          </Button>
+        </Modal.Close>
       }
     >
       <Outlet />
