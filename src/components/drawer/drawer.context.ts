5eeb5b33bc0e65e44585543f3065b516548c82f8
Ben Weier <ben.weier@gmail.com>
2024-10-06 11:19:53 +1000

    update zustand import

--- src/components/drawer/drawer.context.ts
+++ src/components/drawer/drawer.context.ts
@@ -1,5 +1,5 @@
 import { createContext } from 'react'
-import type { StoreApi } from 'zustand/vanilla'
+import type { StoreApi } from 'zustand'
 import type { DrawerStore } from './drawer.types'
 
 export const DrawerContext = createContext<StoreApi<DrawerStore> | null>(null)
