12e1ee5862737f8405c369c2ccb15c5ac2670fd3
Ben Weier <ben.weier@gmail.com>
2024-10-06 11:32:17 +1000

    export drawer overlay

--- src/components/drawer/index.ts
+++ src/components/drawer/index.ts
@@ -1,6 +1,7 @@
 import { Content } from './drawer-content.component'
 import { Description } from './drawer-description.component'
 import { Header } from './drawer-header.component'
+import { Overlay } from './drawer-overlay.component'
 import { Title } from './drawer-title.component'
 import { Trigger } from './drawer-trigger.component'
 import { Drawer as DrawerComponent } from './drawer.component'
@@ -9,4 +10,4 @@ export { useDrawerContext, useDrawerActions } from './use-drawer-store.hook'
 export { useDrawerImperativeHandle } from './use-drawer-imperative-handle.hook'
 export type { DrawerStore, DrawerActions, DrawerImperativeRef, DrawerProps, DrawerState } from './drawer.types'
 
-export const Drawer = Object.assign(DrawerComponent, { Trigger, Content, Header, Title, Description })
+export const Drawer = Object.assign(DrawerComponent, { Trigger, Content, Header, Title, Description, Overlay })
