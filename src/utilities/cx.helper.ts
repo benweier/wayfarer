de6ba150fbc580eb41c113b154ae53aeb08d3a7f
Ben Weier <ben.weier@gmail.com>
2024-11-29 17:42:58 +1000

    use tvm helper

--- src/utilities/cx.helper.ts
+++ src/utilities/cx.helper.ts
@@ -1,10 +1,6 @@
+import { twm } from '@/utilities/twm.helper'
 import { type ClassValue, clsx } from 'clsx'
-import { extendTailwindMerge } from 'tailwind-merge'
-
-const tw = extendTailwindMerge({
-  cacheSize: 1000,
-})
 
 export function cx(...args: ClassValue[]) {
-  return tw(clsx(args))
+  return twm(clsx(args))
 }
