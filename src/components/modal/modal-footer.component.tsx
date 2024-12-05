bb6892f4b41b016c58facf5460a12a73ef126dd3
Ben Weier <ben.weier@gmail.com>
2024-12-05 21:09:16 +1000

    tweak modal header and footer backdrop

--- src/components/modal/modal-footer.component.tsx
+++ src/components/modal/modal-footer.component.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react'
 
 export const Footer = ({ children }: PropsWithChildren) => {
   return (
-    <div className="-m-6 mt-6 flex flex-col gap-1 border-t-1 border-t-border-tertiary bg-background-primary/80 p-6 pt-5 backdrop-blur">
+    <div className="-m-6 mt-6 flex flex-col gap-1 border-t-2 border-t-border-tertiary bg-background-primary/80 p-6 pt-5 backdrop-blur-lg backdrop-brightness-150">
       {children}
     </div>
   )
--- src/components/modal/modal-header.component.tsx
+++ src/components/modal/modal-header.component.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react'
 
 export const Header = ({ children }: PropsWithChildren) => {
   return (
-    <div className="-m-6 mb-6 flex flex-col gap-1 border-b-1 border-b-border-tertiary bg-background-primary/80 p-6 pb-5 backdrop-blur">
+    <div className="-m-6 mb-6 flex flex-col gap-1 border-b-2 border-b-border-tertiary bg-background-primary/80 p-6 pb-5 backdrop-blur-lg backdrop-brightness-150">
       {children}
     </div>
   )
