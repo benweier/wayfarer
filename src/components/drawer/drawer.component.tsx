bf5fc0f8439780e999e493d9d3e88df291d76797
Ben Weier <ben.weier@gmail.com>
2025-02-01 00:21:21 +1000

    move drawer store creation to new hook

--- src/components/drawer/drawer.component.tsx
+++ src/components/drawer/drawer.component.tsx
@@ -1,32 +1,10 @@
-import { useImperativeHandle, useState } from 'react'
-import { createStore } from 'zustand'
 import { Root } from './drawer-root.component'
 import { DrawerContext } from './drawer.context'
-import type { DrawerProps, DrawerStore } from './drawer.types'
+import { useCreateDrawerStore } from './use-drawer-store.hook'
+import type { DrawerProps } from './drawer.types'
 
 export const Drawer = ({ ref, trigger, defaultOpen = false, children, ...props }: DrawerProps) => {
-  const [store] = useState(() => {
-    return createStore<DrawerStore>((set) => ({
-      isOpen: defaultOpen,
-      actions: {
-        open: () => {
-          set({ isOpen: true })
-        },
-        close: () => {
-          set({ isOpen: false })
-        },
-        toggle: () => {
-          set(({ isOpen }) => ({ isOpen: !isOpen }))
-        },
-      },
-    }))
-  })
-
-  useImperativeHandle(ref, () => {
-    const { actions } = store.getState()
-
-    return actions
-  })
+  const store = useCreateDrawerStore(ref, { defaultOpen })
 
   return (
     <DrawerContext value={store}>
--- src/components/drawer/use-drawer-store.hook.ts
+++ src/components/drawer/use-drawer-store.hook.ts
@@ -1,9 +1,10 @@
-import { use } from 'react'
-import { useStore } from 'zustand'
+import { use, useImperativeHandle, useState } from 'react'
+import { createStore, useStore } from 'zustand'
 import { DrawerContext } from './drawer.context'
-import type { DrawerStore } from './drawer.types'
+import type { DrawerImperativeRef, DrawerStore } from './drawer.types'
+import type { Ref } from 'react'
 
-export const useDrawerContext = <T = DrawerStore>(selector: (state: DrawerStore) => T = (state) => state as T): T => {
+export function useDrawerContext<T = DrawerStore>(selector: (state: DrawerStore) => T = (state) => state as T): T {
   const store = use(DrawerContext)
 
   if (!store) throw new Error('`useDrawerContext` must be used within a `Drawer` Provider')
@@ -11,6 +12,44 @@ export const useDrawerContext = <T = DrawerStore>(selector: (state: DrawerStore)
   return useStore(store, selector)
 }
 
-export const useDrawerActions = () => {
-  return useDrawerContext((state) => state.actions)
+function selector(state: DrawerStore) {
+  return state.actions
+}
+
+export function useDrawerActions() {
+  return useDrawerContext(selector)
+}
+
+function createDrawerStore(defaultOpen = false) {
+  return createStore<DrawerStore>((set) => ({
+    isOpen: defaultOpen,
+    actions: {
+      open: () => {
+        set({ isOpen: true })
+      },
+      close: () => {
+        set({ isOpen: false })
+      },
+      toggle: () => {
+        set(({ isOpen }) => ({ isOpen: !isOpen }))
+      },
+    },
+  }))
+}
+
+export function useCreateDrawerStore(
+  ref?: Ref<DrawerImperativeRef>,
+  { defaultOpen = false }: { defaultOpen?: boolean } = {},
+) {
+  const [store] = useState(() => {
+    return createDrawerStore(defaultOpen)
+  })
+
+  useImperativeHandle(ref, () => {
+    const { actions } = store.getState()
+
+    return actions
+  })
+
+  return store
 }
