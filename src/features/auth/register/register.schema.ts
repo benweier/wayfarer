3da1c5f2d23592c3d8e6de9203727b8911f51789
Ben Weier <ben.weier@gmail.com>
2025-02-02 10:32:40 +1000

    update register schema and types

--- src/features/auth/register/register.schema.ts
+++ src/features/auth/register/register.schema.ts
@@ -1,14 +1,15 @@
-import { i18n } from '@/services/i18n'
 import * as v from 'valibot'
+import { i18n } from '@/services/i18n'
 
 export const RegisterSchema = v.object({
   symbol: v.pipe(v.string(), v.trim(), v.minLength(3), v.maxLength(14)),
-  faction: v.string(),
-  email: v.optional(
-    v.union([v.literal(''), v.pipe(v.string(), v.trim(), v.email())], () => i18n.t('email', { ns: 'validation' })),
+  faction: v.string(() => i18n.t('auth.fields.faction.validation.required')),
+  email: v.union([v.pipe(v.string(), v.trim(), v.literal('')), v.pipe(v.string(), v.trim(), v.email())], () =>
+    i18n.t('email', { ns: 'validation' }),
   ),
 })
 
 export type RegisterSchema = v.InferOutput<typeof RegisterSchema>
 
-export type RegisterFormFieldValues = v.InferInput<typeof RegisterSchema>
+export type RegisterSchemaInput = v.InferInput<typeof RegisterSchema>
+export type RegisterSchemaOutput = v.InferOutput<typeof RegisterSchema>
