6e7e5a8bd71ba6ff25f2201a86d0517320881cfc
Ben Weier <ben.weier@gmail.com>
2025-02-03 18:43:05 +1000

    replace index of with includes

--- src/features/waypoint/utilities/has-trait.helper.ts
+++ src/features/waypoint/utilities/has-trait.helper.ts
@@ -2,5 +2,5 @@ import type { WaypointTraits } from '@/config/spacetraders'
 import type { WaypointTrait } from '@/types/spacetraders'
 
 export const hasTrait = (traits: WaypointTrait[] = [], symbol: WaypointTraits[] = []) => {
-  return traits?.findIndex((trait) => symbol.indexOf(trait.symbol) > -1) > -1
+  return traits?.findIndex((trait) => symbol.includes(trait.symbol)) > -1
 }
