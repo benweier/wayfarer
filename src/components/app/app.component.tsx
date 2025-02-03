4f0b06980b75ffb8fa3e50bdf44e0cef05d4c93f
Ben Weier <ben.weier@gmail.com>
2025-02-03 18:42:39 +1000

    Move sentry initialisation and error boundary

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,9 +1,11 @@
 import { Provider as TooltipProvider } from '@radix-ui/react-tooltip'
-import { QueryClientProvider } from '@tanstack/react-query'
+import * as Sentry from '@sentry/react'
+import { QueryClientProvider, useQueryErrorResetBoundary } from '@tanstack/react-query'
 import { RouterProvider } from '@tanstack/react-router'
 import { enableMapSet } from 'immer'
 import { Suspense } from 'react'
 import { I18nextProvider } from 'react-i18next'
+import { Button } from '@/components/button'
 import { KBar } from '@/components/kbar'
 import { ThemeStyleSheet } from '@/components/theme-style-sheet'
 import { router } from '@/routes/router.conf'
@@ -12,27 +14,56 @@ import { client } from '@/services/query-client'
 import { useAppActions } from './use-app-actions.hook'
 
 import '@/services/valibot'
-import '@/services/sentry'
 import '@/styles/main.css'
 
 enableMapSet()
 
+Sentry.init({
+  dsn: import.meta.env.SENTRY_DSN,
+  enabled: import.meta.env.PROD,
+  integrations: [Sentry.tanstackRouterBrowserTracingIntegration(router)],
+  tracesSampleRate: 0.1,
+})
+
 export const App = () => {
+  const { reset } = useQueryErrorResetBoundary()
   const actions = useAppActions()
 
   return (
-    <Suspense fallback={null}>
-      <ThemeStyleSheet />
+    <Sentry.ErrorBoundary
+      onReset={reset}
+      fallback={({ resetError, error }) => (
+        <div className="flex min-h-screen flex-col items-center justify-center">
+          <div className="flex w-full flex-col items-center gap-4 p-3">
+            <div className="text-h4 text-foreground-error-primary font-mono">{error.message}</div>
+            <Button
+              intent="info"
+              className="w-full max-w-xs"
+              onClick={() => {
+                resetError()
+              }}
+            >
+              Try again
+            </Button>
+          </div>
+        </div>
+      )}
+    >
+      <div className="flex h-dvh w-screen flex-col">
+        <Suspense fallback={null}>
+          <ThemeStyleSheet />
 
-      <I18nextProvider i18n={i18n}>
-        <QueryClientProvider client={client}>
-          <KBar actions={actions}>
-            <TooltipProvider delayDuration={100}>
-              <RouterProvider router={router} />
-            </TooltipProvider>
-          </KBar>
-        </QueryClientProvider>
-      </I18nextProvider>
-    </Suspense>
+          <I18nextProvider i18n={i18n}>
+            <QueryClientProvider client={client}>
+              <KBar actions={actions}>
+                <TooltipProvider delayDuration={100}>
+                  <RouterProvider router={router} />
+                </TooltipProvider>
+              </KBar>
+            </QueryClientProvider>
+          </I18nextProvider>
+        </Suspense>
+      </div>
+    </Sentry.ErrorBoundary>
   )
 }
