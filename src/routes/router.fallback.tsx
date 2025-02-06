1895c12b99ac625364c76e9399ffa8a004867cf7
Ben Weier <ben.weier@gmail.com>
2025-02-06 18:59:41 +1000

    fix tailwind class order

--- src/routes/router.fallback.tsx
+++ src/routes/router.fallback.tsx
@@ -1,5 +1,5 @@
 export const Fallback = () => (
-  <div className="text-h1 flex h-screen w-full animate-pulse items-center justify-center font-black text-foreground-quinary">
+  <div className="text-h1 text-foreground-quinary flex w-full flex-1 animate-pulse items-center justify-center font-black">
     <div>Wayfarer</div>
   </div>
 )
