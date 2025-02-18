4b704dad17fba0c729a1cf5d520fcb132a83ebc1
Ben Weier <ben.weier@gmail.com>
2025-02-18 17:16:46 +1000

    fix type linting

--- src/utilities/dynamic.helper.ts
+++ src/utilities/dynamic.helper.ts
@@ -1,11 +1,12 @@
-import { type ComponentType, lazy } from 'react'
+import { lazy } from 'react'
+import type { ComponentType } from 'react'
 
 // https://github.com/facebook/react/issues/14603#issuecomment-726551598
-export function dynamic<T extends ComponentType<any>, I extends { [K in N]: T }, N extends keyof I>(
+export function dynamic<T extends ComponentType<any>, I extends Record<N, T>, N extends keyof I>(
   factory: () => Promise<I>,
   name: N,
 ): I {
   return Object.create({
     [name]: lazy(() => factory().then((mod) => ({ default: mod[name] }))),
-  })
+  }) as I
 }
